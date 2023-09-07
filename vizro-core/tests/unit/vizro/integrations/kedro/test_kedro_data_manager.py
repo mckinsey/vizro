f952fd52668dd1e652157db4951f67f3fbf4dd8c
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2023-09-07 14:30:37 +0200

    Consolidate conftest.py approach (#18)

--- vizro-core/tests/unit/vizro/integrations/kedro/test_kedro_data_manager.py
+++ vizro-core/tests/unit/vizro/integrations/kedro/test_kedro_data_manager.py
@@ -1,13 +1,20 @@
 """Unit tests for vizro.integrations.kedro."""
 import types
+from pathlib import Path
 
 import pytest
 import yaml
 
 kedro = pytest.importorskip("kedro")
+
 from kedro.io import DataCatalog  # noqa: E402
 
 from vizro.integrations.kedro import datasets_from_catalog  # noqa: E402
+
+
+@pytest.fixture
+def catalog_path():
+    return Path(__file__).parent / "fixtures/test_catalog.yaml"
 
 
 def test_datasets_from_catalog(catalog_path):
