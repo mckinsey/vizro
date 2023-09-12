b79dee40a632003274160175f9d6e4a8ea760dd9
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-09-12 13:32:44 +0200

    Clean up Alert model

--- vizro-core/src/vizro/models/_components/form/_alert.py
+++ vizro-core/src/vizro/models/_components/form/_alert.py
@@ -1,4 +1,4 @@
-from typing import List, Literal
+from typing import List, Literal, Optional
 
 import dash_bootstrap_components as dbc
 from dash import html
@@ -7,36 +7,35 @@ from pydantic import Field
 from vizro.models import Action, VizroBaseModel
 from vizro.models._models_utils import _log_call
 
-ICON_MAPPING = {
-    "warning": "\u26A0          - ",
-    "default": "",
-    "information": "\u24D8          - ",
-    "success": "\u2713          - ",
-}
-
 
 class Alert(VizroBaseModel):
-    """Component provided to `Form` to provide user feedback."""
+    """Component `Alert` to provide user feedback.
+
+    Args:
+        type (Literal["alert"]): Defaults to `"alert"`.
+        text (str): Text to be displayed in the alert.
+        is_open (bool): Flag indicating whether alert should be open by default. Defaults to `True`.
+        duration (Optional[int]): Duration in milliseconds for the alert to appear. Defaults to `None`.
+        actions (List[Action]): See [`Action`][vizro.models.Action]. Defaults to `[]`.
+    """
 
     type: Literal["alert"] = "alert"
-    icon: Literal["warning", "default", "information", "success"] = Field(
-        "default", description="Icon to be displayed next to text in the alert"
-    )
-    text: str = Field(..., description="Text to be displayed in the alert")
-    is_open: bool = Field(False, description="Flag indicating whether alert should open on dashboard start")
-    duration: int = Field(500, description="Duration in milliseconds for the alert to appear", ge=0)
+    text: str = Field(..., description="Text to be displayed in the alert.")
+    is_open: bool = Field(True, description="Flag indicating whether alert should be open by default.")
+    duration: Optional[int] = Field(None, description="Duration in milliseconds for the alert to appear.", ge=0)
     actions: List[Action] = []
 
     @_log_call
     def build(self):
-        alert_text_full = ICON_MAPPING[self.icon] + self.text
-
-        return [
-            dbc.Alert(
-                [html.P(alert_text_full, className="alert_text")],
-                id=self.id,
-                duration=self.duration,
-                is_open=self.is_open,
-                className="alert",
-            ),
-        ]
+        return html.Div(
+            [
+                dbc.Alert(
+                    id=self.id,
+                    children=[html.P(self.text)],
+                    duration=self.duration,
+                    is_open=self.is_open,
+                    className="alert",
+                ),
+            ],
+            className="alert_container",
+        )
