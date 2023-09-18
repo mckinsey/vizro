58aa705d18c0a2ed8179ae970d1c507641d57fb1
petar-qb <petar_pejovic@external.mckinsey.com>
2023-09-18 13:40:15 +0200

    Improved logs

--- vizro-core/src/vizro/models/_action/_action.py
+++ vizro-core/src/vizro/models/_action/_action.py
@@ -74,7 +74,10 @@ class Action(VizroBaseModel):
 
         @callback(output=callback_outputs, inputs=callback_inputs, prevent_initial_call=True)
         def callback_wrapper(trigger: None, **inputs: Dict[str, Any]):
-            logger.debug(f"Inputs to Action: {inputs}")
+            logger.debug("=============== ACTION ===============")
+            logger.debug(f'Action ID: "{self.id}"')
+            logger.debug(f'Action name: "{action_functions[self.function._function]}"')
+            logger.debug(f"Action inputs: {inputs}")
             return_value = self.function(**inputs) or {}
             if isinstance(return_value, dict):
                 return {"action_finished": None, **return_value}
