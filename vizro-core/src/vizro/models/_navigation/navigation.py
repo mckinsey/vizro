4bdca796a569ead44663aeee3bc631ad16ddcbf2
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-03 16:19:42 +0200

    Refactor code
    - Rename to active_page_id
    - Compare with page["module"]
    - Optimise for loop

--- vizro-core/src/vizro/models/_navigation/navigation.py
+++ vizro-core/src/vizro/models/_navigation/navigation.py
@@ -77,5 +77,5 @@ class Navigation(VizroBaseModel):
         self._selector = Accordion(pages=self.pages)
 
     @_log_call
-    def build(self, page_id):
-        return self._selector.build(page_id=page_id)
+    def build(self, active_page_id):
+        return self._selector.build(active_page_id=active_page_id)
