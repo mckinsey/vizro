40ec167b55f4d0a865a26f9896ca33ef3eb967e3
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-04 10:02:16 +0200

    Update utils

--- vizro-core/src/vizro/models/_navigation/_navigation_utils.py
+++ vizro-core/src/vizro/models/_navigation/_navigation_utils.py
@@ -10,35 +10,32 @@ from vizro.managers import model_manager
 # TODO: Adjust validator to take into account pages on different icons
 def _validate_pages(pages):
     from vizro.models import Page
-
-    if pages is not None and not pages:
-        raise ValueError("Ensure this value has at least 1 item.")
-
-    if pages:
-        registered_pages = [page[0] for page in model_manager._items_with_type(Page)]
-
-        if isinstance(pages, dict):
-            missing_pages = [
-                page
-                for page in registered_pages
-                if page not in {page for nav_pages in pages.values() for page in nav_pages}
-            ]
-            unknown_pages = [page for nav_pages in pages.values() for page in nav_pages if page not in registered_pages]
-        else:
-            missing_pages = [page for page in registered_pages if page not in pages]
-            unknown_pages = [page for page in pages if page not in registered_pages]
-
-        if missing_pages:
-            warnings.warn(
-                f"Not all registered pages used in Navigation 'pages'. Missing pages {missing_pages}!", UserWarning
-            )
-
-        if unknown_pages:
-            raise ValueError(
-                f"Unknown page ID or page title provided to Navigation 'pages'. " f"Unknown pages: {unknown_pages}"
-            )
+    registered_pages = [page[0] for page in model_manager._items_with_type(Page)]
 
     if pages is None:
-        return [page for page in dash.page_registry.keys() if page != MODULE_PAGE_404]
+        return registered_pages
 
+    if not pages:
+        raise ValueError("Ensure this value has at least 1 item.")
+
+    if isinstance(pages, dict):
+        missing_pages = [
+            page
+            for page in registered_pages
+            if page not in {page for nav_pages in pages.values() for page in nav_pages}
+        ]
+        unknown_pages = [page for nav_pages in pages.values() for page in nav_pages if page not in registered_pages]
+    else:
+        missing_pages = [page for page in registered_pages if page not in pages]
+        unknown_pages = [page for page in pages if page not in registered_pages]
+
+    if missing_pages:
+        warnings.warn(
+            f"Not all registered pages used in Navigation 'pages'. Missing pages {missing_pages}!", UserWarning
+        )
+
+    if unknown_pages:
+        raise ValueError(
+            f"Unknown page ID or page title provided to Navigation 'pages'. " f"Unknown pages: {unknown_pages}"
+        )
     return pages
