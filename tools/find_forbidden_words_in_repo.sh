608f180f09a953e22c659af72da086fc2ade7e1a
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2023-10-11 10:51:48 +0200

    Stop entering api base into main

--- tools/find_forbidden_words_in_repo.sh
+++ tools/find_forbidden_words_in_repo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-words=$"colour\|visualisation"
+words=$"colour\|visualisation\|openai.api_base"
 words_finder=$(grep -Irwno --exclude=find_forbidden_words_in_repo.sh --exclude-dir={.git,*cache*,*node_modules*,venv} . -e "$words")
 
 if [[ $words_finder ]]
 then
-  echo "Incorrect spelling for $words:
+  echo "Incorrect spelling or forbidden word for $words:
 $words_finder"
   exit 1
 else
