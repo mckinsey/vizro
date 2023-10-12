bdcbbe07d6a88d8dfc6c47b9790bd2bbcc8a7fce
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-12 22:07:08 +0200

    Try out different git command

--- tools/check_branch_name.sh
+++ tools/check_branch_name.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-BRANCH=$(git name-rev --name-only HEAD)
+BRANCH=$(git symbolic-ref --short HEAD)
 REGEX="^(main|(release|feat|bug|docs|qa|dev|demo|ci|tidy)\/[^/]+(/[^/]+)*)$"
 
-
 if ! [[ $BRANCH =~ $REGEX ]]; then
-  echo "Invalid branch name - please rename your branch following this $REGEX syntax."
+  echo "Branch name '$BRANCH' is invalid - please rename your branch following this regex syntax: $REGEX"
   exit 1
 fi
