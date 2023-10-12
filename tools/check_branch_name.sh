3cfcf0224b6f0a9a09c9ec944dabb8b3ee96e913
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-12 15:48:49 +0200

    Add branch name check

---
+++ tools/check_branch_name.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+REGEX="^(main|(release|feat|bug|docs|qa|dev|demo|ci)\/[^/]+(/[^/]+)*)$"
+
+
+if ! [[ $BRANCH =~ $REGEX ]]; then
+  echo "Invalid branch name - please rename your branch following this $REGEX syntax."
+  exit 1
+fi
