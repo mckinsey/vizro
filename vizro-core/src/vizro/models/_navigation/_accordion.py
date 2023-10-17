d5786dcd2887f9bf91a1694f6741346092b133b5
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-17 21:37:09 +0200

    Fix unit tests after latest master merge

--- vizro-core/src/vizro/models/_navigation/_accordion.py
+++ vizro-core/src/vizro/models/_navigation/_accordion.py
@@ -61,7 +61,7 @@ class Accordion(VizroBaseModel):
                 html.Hr(),
             ],
             className="nav_panel",
-            id=f"{self.id}_outer",
+            id="nav_panel_outer",
         )
 
     def _create_accordion_buttons(self, pages, active_page_id):
