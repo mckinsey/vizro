c998ccf28eaf79f117b76d4b7ca3b109de6540ad
Antony Milne <antony.milne@quantumblack.com>
2023-10-17 15:56:31 +0100

    Refactor accordion model

--- vizro-core/src/vizro/models/_navigation/_accordion.py
+++ vizro-core/src/vizro/models/_navigation/_accordion.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Optional
 
 import dash
@@ -40,7 +41,46 @@ class Accordion(VizroBaseModel):
     def build(self, *, active_page_id=None):
         # Could now be None in return - probably not good. Traces through to page._arrange_containers
         # Remember union types awkward and lead to bugs, especially None type
-        return self._create_accordion(active_page_id=active_page_id)
+        accordion_items = []
+
+        # self.pages = {"section 1": ["Page 1", "Page 2"], "section 2": ["Page 3"]}
+
+        # self.pages = ["Page 1", "Page 2"]  -> ACCORDION_DEFAULT_TITLE
+        if isinstance(self.pages, list):
+            self.pages = {ACCORDION_DEFAULT_TITLE: self.pages}
+        # Turn into top case either here or in validator.
+
+        # assume for now that self.pages has at least one item.
+        # Check if "not accordion_buttons" check also needed
+        if len(list(itertools.chain(*self.pages.values()))) == 1:
+            return html.Div(className="hidden")
+
+        for page_group, page_members in self.pages.items():
+            accordion_buttons = self._create_accordion_buttons(pages=page_members, active_page_id=active_page_id)
+            accordion_items.append(
+                dbc.AccordionItem(
+                    children=accordion_buttons,
+                    title=page_group.upper(),
+                    class_name="accordion_item",
+                )
+            )
+
+        return html.Div(
+            children=[
+                dbc.Accordion(
+                    id=self.id,
+                    children=accordion_items,
+                    class_name="accordion",
+                    persistence=True,
+                    persistence_type="session",
+                    always_open=True,
+                ),
+                html.Hr(),
+            ],
+            className="nav_panel",
+            id=f"{self.id}_outer",
+        )
+
 
     def _create_accordion_buttons(self, pages, active_page_id):
         """Creates a button for each provided page that is registered."""
@@ -63,51 +103,3 @@ class Accordion(VizroBaseModel):
             )
         return accordion_buttons
 
-    def _create_accordion_item(self, accordion_buttons, title=ACCORDION_DEFAULT_TITLE):
-        # Defaulting behaviour in wrong place here as makes code in _create_accordion different
-        """Creates an accordion item for each sub-group of pages."""
-        return dbc.AccordionItem(
-            children=accordion_buttons,
-            title=title.upper(),
-            class_name="accordion_item",
-        )
-
-    def _get_accordion_container(self, accordion_items, accordion_buttons):
-        # Return no container if there is only one page in the dashboard or no pages exist
-        # Only need accordion_buttons for this one line -> good sign not needed really or should go elsewhere
-        # Do earlier on in flow so don't need to computer accordion_buttons etc. in this case.
-        if (len(accordion_buttons) == len(accordion_items) == 1) or not accordion_buttons:
-            return None
-
-        return html.Div(
-            children=[
-                dbc.Accordion(
-                    id=self.id,
-                    children=accordion_items,
-                    class_name="accordion",
-                    persistence=True,
-                    persistence_type="session",
-                    always_open=True,
-                ),
-                html.Hr(),
-            ],
-            className="nav_panel",
-            id=f"{self.id}_outer",
-        )
-
-    def _create_accordion(self, active_page_id):
-        """Creates a custom accordion only with user-provided pages."""
-        accordion_items = []
-        if isinstance(self.pages, dict):
-            for page_group, page_members in self.pages.items():
-                accordion_buttons = self._create_accordion_buttons(pages=page_members, active_page_id=active_page_id)
-                accordion_items.append(
-                    self._create_accordion_item(accordion_buttons=accordion_buttons, title=page_group)
-                )
-
-        if isinstance(self.pages, list):
-            # Turn into top case either here or in validator.
-            # GENERAL RULE: do conversion early so don't need to handle different cases.
-            accordion_buttons = self._create_accordion_buttons(pages=self.pages, active_page_id=active_page_id)
-            accordion_items.append(self._create_accordion_item(accordion_buttons=accordion_buttons))
-        return self._get_accordion_container(accordion_items=accordion_items, accordion_buttons=accordion_buttons)
