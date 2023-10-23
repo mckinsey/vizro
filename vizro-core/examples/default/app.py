95448f7ef5328a77ea8137b8e9ce5e9ec379de04
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2023-10-23 12:36:14 +0200

    Move table to correct import path

--- vizro-core/examples/default/app.py
+++ vizro-core/examples/default/app.py
@@ -11,7 +11,7 @@ import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.actions import filter_interaction
-from vizro.charts.tables import dash_data_table
+from vizro.tables import dash_data_table
 from vizro.managers import data_manager
 from vizro.models.types import capture
 
--- vizro-core/src/vizro/tables/__init__.py
+++ vizro-core/src/vizro/tables/__init__.py
@@ -1,4 +1,4 @@
-from vizro.charts.tables.dash_table import dash_data_table
+from vizro.tables.dash_table import dash_data_table
 
 # Please keep alphabetically ordered
 __all__ = ["dash_data_table"]
