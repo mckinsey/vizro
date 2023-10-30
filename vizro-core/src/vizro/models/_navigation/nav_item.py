222b583617a448a7162ac4339e3bae5f89a6e89d
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-10-30 10:19:42 +0100

    Implement root_validator for text and tooltip

--- vizro-core/src/vizro/models/_navigation/nav_item.py
+++ vizro-core/src/vizro/models/_navigation/nav_item.py
@@ -6,7 +6,7 @@ from typing import Optional
 import dash
 import dash_bootstrap_components as dbc
 from dash import html
-from pydantic import validator
+from pydantic import Field, root_validator, validator
 
 from vizro.models import VizroBaseModel
 from vizro.models._models_utils import _log_call
@@ -20,19 +20,19 @@ class NavItem(VizroBaseModel):
 
     Args:
         pages (NavPagesType): See [NavPagesType][vizro.models.types.NavPagesType].
+        icon (str): URI (relative or absolute) of the embeddable content. Defaults to "home".
+        max_text_length (int): Character limit for `text` argument. Defaults to 8.
+        text (Optional[str]): Text to be displayed below the icon. Defaults to `None`.
+        tooltip (Optional[str]): Text to be displayed in the icon tooltip on hover.
         selector (Optional[Accordion]): See [`Accordion`][vizro.models.Accordion]. Defaults to `None`.
-        icon (Optional[str]): URI (relative or absolute) of the embeddable content.
-        max_text_length (int): Character limit for `text` argument. Defaults to 9.
-        text (str): Text to be display below the icon. Defaults to `""`.
-        tooltip (Optional[str]): Text to be displayed in the tooltip on icon hover.
     """
 
-    icon: str = "home"
     pages: NavPagesType
+    icon: str = Field("home", description="URI (relative or absolute) of the embeddable content.")
+    max_text_length: int = Field(8, description="Character limit for `text` argument.")
+    text: Optional[str] = Field(None, description="Text to be displayed below the icon.")
+    tooltip: Optional[str] = Field(None, description="Text to be displayed in the icon tooltip on hover.")
     selector: Optional[Accordion] = None
-    max_text_length: int = 9
-    text: str = ""
-    tooltip: Optional[str] = None
 
     # Re-used validators
     _validate_pages = validator("pages", allow_reuse=True, always=True)(_validate_pages)
@@ -43,25 +43,13 @@ class NavItem(VizroBaseModel):
             return Accordion(pages=values.get("pages"))
         return selector
 
-    @_log_call
-    def pre_build(self):
-        if self.tooltip is None:
-            if self.text and len(self.text) > self.max_text_length:
-                self.tooltip = self.text
-                self.text = self.text[: self.max_text_length]
-
-    @validator("text", always=True)
-    def set_text(cls, text, values):
-        if text and len(text) > values.get("max_text_length"):
-            return text[: values.get("max_text_length")]
-        return text
-
-    @validator("tooltip", always=True, pre=True)
-    def set_tooltip(cls, tooltip, values):
-        if tooltip is None:
-            if values.get("text") and len(values.get("text")) > values.get("max_text_length"):
-                return values.get("text")
-        return tooltip
+    @root_validator()
+    def set_text_and_tooltip(cls, values):
+        if values["text"] and (len(values["text"]) > values["max_text_length"]):
+            if values["tooltip"] is None:
+                values["tooltip"] = values["text"]
+            values["text"] = values["text"][: values["max_text_length"]]
+        return values
 
     @_log_call
     def build(self, active_page_id):
