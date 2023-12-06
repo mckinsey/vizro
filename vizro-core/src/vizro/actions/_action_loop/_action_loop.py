26c6773f8d0e2f51e11c3b9aa4c7b6a3d1c009e3
Antony Milne <49395058+antonymilne@users.noreply.github.com>
2023-12-06 16:06:38 +0000

    Fix integration tests to work in a single pytest session (#192)

--- vizro-core/src/vizro/actions/_action_loop/_action_loop.py
+++ vizro-core/src/vizro/actions/_action_loop/_action_loop.py
@@ -16,7 +16,7 @@ class ActionLoop:
         Returns:
             List of required components for the action loop and for each `Action` in the `Dashboard`.
         """
-        return html.Div([cls._build_action_loop(), cls._build_actions_models()], id="app_components_div")
+        return html.Div([cls._build_action_loop(), cls._build_actions_models()], id="app_components_div", hidden=True)
 
     @staticmethod
     def _build_action_loop():
@@ -36,4 +36,4 @@ class ActionLoop:
             List of required components for each `Action` in the `Dashboard` e.g. List[dcc.Download]
         """
         actions = _get_actions_on_registered_pages()
-        return html.Div([action.build() for action in actions], id="app_action_models_components_div")
+        return html.Div([action.build() for action in actions], id="app_action_models_components_div", hidden=True)
