c2962c9eb1486e0207a6c22e9744659a89bf5de6
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2023-12-12 18:30:12 +0100

    Replace custom component with post app.dash.layout update

--- vizro-core/examples/default/app.py
+++ vizro-core/examples/default/app.py
@@ -1,5 +1,7 @@
 """Example to show dashboard configuration."""
 
+
+import dash_mantine_components as dmc
 import pandas as pd
 
 import vizro.models as vm
@@ -7,43 +9,6 @@ import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.actions import export_data, filter_interaction
 from vizro.tables import dash_data_table
-import dash_mantine_components as dmc
-from typing import Literal
-import dash
-import dash_bootstrap_components as dbc
-from dash import ClientsideFunction, Input, Output, clientside_callback, get_relative_path, html
-import vizro
-from vizro.actions._action_loop._action_loop import ActionLoop
-
-class DashboardWithAffix(vm.Dashboard):
-    type: Literal["dashboard_affix"] = "dashboard_affix"
-
-    def build(self):
-        for page in self.pages:
-            page.build()  # TODO: ideally remove, but necessary to register slider callbacks
-
-        clientside_callback(
-            ClientsideFunction(namespace="clientside", function_name="update_dashboard_theme"),
-            Output("dashboard_container_outer", "className"),
-            Input("theme_selector", "on"),
-        )
-
-        return dbc.Container(
-            id="dashboard_container_outer",
-            children=[
-                html.Div(vizro.__version__, id="vizro_version", hidden=True),
-                ActionLoop._create_app_callbacks(),
-                dash.page_container,
-                dmc.Affix(dmc.Anchor("Any questions? Contact us.",
-                                     href="mailto:vizro@mckinsey.com",
-                                     className="anchor-text"),
-                          className="affix-button",
-                          position={"bottom": 20, "right": 20}
-                          )
-            ],
-            className=self.theme,
-            fluid=True,
-        )
 
 
 def retrieve_avg_continent_data():
@@ -551,7 +516,7 @@ def create_home_page():
     return page_home
 
 
-dashboard = DashboardWithAffix(
+dashboard = vm.Dashboard(
     pages=[
         create_home_page(),
         create_variable_analysis(),
@@ -569,4 +534,13 @@ dashboard = DashboardWithAffix(
 )
 
 if __name__ == "__main__":
-    Vizro(assets_folder="../assets").build(dashboard).run()
+    app = Vizro(assets_folder="../assets").build(dashboard)
+
+    app.dash.layout.children.append(
+        dmc.Affix(
+            dmc.Anchor("Any questions? Contact us.", href="mailto:vizro@mckinsey.com", className="anchor-text"),
+            className="affix-button",
+            position={"bottom": 20, "right": 20},
+        )
+    )
+    app.run()
