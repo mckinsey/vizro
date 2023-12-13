a9bda9e0134f0d8a219336fadd1a0b0ba39fb38d
Antony Milne <antony.milne@quantumblack.com>
2023-12-13 06:08:39 +0000

    Tidy CapturedCallable

--- vizro-core/examples/default/app.py
+++ vizro-core/examples/default/app.py
@@ -1,30 +1,53 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.actions import export_data
+from vizro.actions import filter_interaction
 from vizro.models.types import capture
-from time import sleep
 
 
 @capture("action")
-def my_custom_action(t: int):
+def my_custom_action(show_species: bool, scatter_chart_clickData: dict):
     """Custom action."""
-    sleep(t)
+    clicked_point = scatter_chart_clickData["points"][0]
+    x, y = clicked_point["x"], clicked_point["y"]
+    text = f"Clicked point has sepal length {x}, petal width {y}"
+
+    if show_species:
+        species = clicked_point["customdata"][0]
+        text += f" and species {species}"
+    return text
 
 
 df = px.data.iris()
 
 page = vm.Page(
-    title="Example of a simple custom action",
+    title="Example of a custom action with UI inputs and outputs",
+    layout=vm.Layout(
+        grid=[
+            [0, 2],
+            [0, 2],
+            [0, 2],
+            [1, -1],
+        ],
+        row_gap="25px",
+    ),
     components=[
-        vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
-        vm.Button(
-            text="Export data",
+        vm.Graph(
+            id="scatter_chart",
+            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", custom_data=["species"]),
             actions=[
-                vm.Action(function=export_data()),
-                vm.Action(function=my_custom_action(t=2)),
-                vm.Action(function=export_data(file_format="xlsx")),
+                vm.Action(function=filter_interaction(targets=["scatter_chart_2"])),
+                vm.Action(
+                    function=my_custom_action(show_species=True),
+                    inputs=["scatter_chart.clickData"],
+                    outputs=["my_card.children"],
+                ),
             ],
+        ),
+        vm.Card(id="my_card", text="Click on a point on the above graph."),
+        vm.Graph(
+            id="scatter_chart_2",
+            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species"),
         ),
     ],
     controls=[vm.Filter(column="species", selector=vm.Dropdown(title="Species"))],
