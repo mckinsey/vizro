f7f5377bf1c2c24d74300757068279325d191378
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-05 16:38:12 +0100

    Update dev example

--- vizro-core/examples/default/app.py
+++ vizro-core/examples/default/app.py
@@ -5,7 +5,88 @@ import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.actions import export_data
 
-df_gapminder = px.data.gapminder()
+df = px.data.gapminder()
+
+
+page_variable = vm.Page(
+    title="Variable Analysis",
+    description="Analyzing population, GDP per capita and life expectancy on country and continent level",
+    components=[
+        vm.Card(
+            text="""
+                ### Overview
+                The world map provides initial insights into the variations of metrics across countries and
+                continents. Click on Play to see the animation and explore the development over time.
+
+                #### Observation
+                A global trend of increasing life expectancy emerges, with some exceptions in specific African
+                countries. Additionally, despite similar population growth rates across continents, the overall
+                global population continues to expand, with India and China leading the way.  Meanwhile, GDP per
+                capita experiences growth in most regions.
+
+            """
+        ),
+        vm.Graph(
+            id="variable_map",
+            figure=px.choropleth(
+                df,
+                locations="iso_alpha",
+                color="lifeExp",
+                hover_name="country",
+                animation_frame="year",
+                labels={
+                    "year": "year",
+                    "lifeExp": "Life expectancy",
+                    "pop": "Population",
+                    "gdpPercap": "GDP per capita",
+                },
+                title="Global development over time",
+            ),
+        ),
+        vm.Card(
+            text="""
+                ### Distribution
+                The boxplot illustrates the distribution of each metric across continents, facilitating comparisons
+                of life expectancy, GDP per capita, and population statistics.
+
+                Observations reveal that Europe and Oceania have the highest life expectancy and GDP per capita,
+                likely influenced by their smaller population growth. Additionally, Asia and America exhibit
+                notable GDP per capita outliers, indicating variations among countries within these continents or
+                large growth over the observed years.
+            """
+        ),
+        vm.Graph(
+            id="variable_boxplot",
+            figure=px.box(
+                df,
+                x="continent",
+                y="lifeExp",
+                color="continent",
+                labels={
+                    "year": "year",
+                    "lifeExp": "Life expectancy",
+                    "pop": "Population",
+                    "gdpPercap": "GDP per capita",
+                    "continent": "Continent",
+                },
+                title="Distribution per continent",
+                color_discrete_map={
+                    "Africa": "#00b4ff",
+                    "Americas": "#ff9222",
+                    "Asia": "#3949ab",
+                    "Europe": "#ff5267",
+                    "Oceania": "#08bdba",
+                },
+            ),
+        ),
+    ],
+    controls=[
+        vm.Parameter(
+            targets=["variable_map.color", "variable_boxplot.y"],
+            selector=vm.RadioItems(options=["lifeExp", "pop", "gdpPercap"], title="Select variable"),
+        )
+    ],
+)
 
 
 single_container_default_layout = vm.Page(
@@ -17,7 +98,7 @@ single_container_default_layout = vm.Page(
                 vm.Graph(
                     id="graph_1",
                     figure=px.line(
-                        df_gapminder,
+                        df,
                         title="Graph_1",
                         x="year",
                         y="lifeExp",
@@ -29,7 +110,7 @@ single_container_default_layout = vm.Page(
                 vm.Graph(
                     id="graph_2",
                     figure=px.scatter(
-                        df_gapminder,
+                        df,
                         title="Graph_2",
                         x="gdpPercap",
                         y="lifeExp",
@@ -40,7 +121,7 @@ single_container_default_layout = vm.Page(
                 vm.Graph(
                     id="graph_3",
                     figure=px.box(
-                        df_gapminder,
+                        df,
                         title="Graph_3",
                         x="continent",
                         y="lifeExp",
@@ -80,7 +161,7 @@ multiple_containers_custom_layout = vm.Page(
                 vm.Graph(
                     id="graph_11",
                     figure=px.line(
-                        df_gapminder,
+                        df,
                         title="Graph_11",
                         x="year",
                         y="lifeExp",
@@ -92,7 +173,7 @@ multiple_containers_custom_layout = vm.Page(
                 vm.Graph(
                     id="graph_12",
                     figure=px.scatter(
-                        df_gapminder,
+                        df,
                         title="Graph_12",
                         x="gdpPercap",
                         y="lifeExp",
@@ -109,7 +190,7 @@ multiple_containers_custom_layout = vm.Page(
                 vm.Graph(
                     id="graph_13",
                     figure=px.box(
-                        df_gapminder,
+                        df,
                         title="Graph_13",
                         x="continent",
                         y="lifeExp",
@@ -160,7 +241,7 @@ multiple_containers_nested = vm.Page(
                         vm.Graph(
                             id="graph_1rn",
                             figure=px.line(
-                                df_gapminder,
+                                df,
                                 title="Graph_1",
                                 x="year",
                                 y="lifeExp",
@@ -172,7 +253,7 @@ multiple_containers_nested = vm.Page(
                         vm.Graph(
                             id="graph_2rn",
                             figure=px.scatter(
-                                df_gapminder,
+                                df,
                                 title="Graph_2",
                                 x="gdpPercap",
                                 y="lifeExp",
@@ -183,7 +264,7 @@ multiple_containers_nested = vm.Page(
                         vm.Graph(
                             id="graph_3rn",
                             figure=px.box(
-                                df_gapminder,
+                                df,
                                 title="Graph_3",
                                 x="continent",
                                 y="lifeExp",
@@ -193,7 +274,7 @@ multiple_containers_nested = vm.Page(
                         vm.Graph(
                             id="graph_2rnn",
                             figure=px.scatter(
-                                df_gapminder,
+                                df,
                                 title="Graph_2",
                                 x="gdpPercap",
                                 y="lifeExp",
@@ -209,7 +290,7 @@ multiple_containers_nested = vm.Page(
                         vm.Graph(
                             id="graph_6rn",
                             figure=px.line(
-                                df_gapminder,
+                                df,
                                 title="Graph_6",
                                 x="year",
                                 y="lifeExp",
@@ -228,7 +309,7 @@ multiple_containers_nested = vm.Page(
                 vm.Graph(
                     id="graph_6rnn",
                     figure=px.line(
-                        df_gapminder,
+                        df,
                         title="Graph_6",
                         x="year",
                         y="lifeExp",
@@ -244,7 +325,12 @@ multiple_containers_nested = vm.Page(
 
 
 dashboard = vm.Dashboard(
-    pages=[single_container_default_layout, multiple_containers_custom_layout, multiple_containers_nested]
+    pages=[
+        page_variable,
+        single_container_default_layout,
+        multiple_containers_custom_layout,
+        multiple_containers_nested,
+    ]
 )
 
 if __name__ == "__main__":
