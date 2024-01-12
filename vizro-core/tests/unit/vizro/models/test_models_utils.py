e1345063edeee6fa3048c84f677eb1c8a0ffb917
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-12 12:02:18 +0100

    Optimise tests

--- vizro-core/tests/unit/vizro/models/test_models_utils.py
+++ vizro-core/tests/unit/vizro/models/test_models_utils.py
@@ -11,41 +11,48 @@ except ImportError:  # pragma: no cov
     from pydantic import ValidationError
 
 import vizro.models as vm
-from vizro.models._models_utils import _assign_component_grid_area, get_unique_grid_component_ids
+from vizro.models._models_utils import (
+    _assign_component_grid_area,
+    _create_component_container,
+    get_unique_grid_component_ids,
+)
 
 
-@pytest.mark.parametrize("Model", [vm.Container, vm.Page])
-class TestReusedValidators:
-    def test_set_components_validator(self, Model):
+@pytest.fixture(params=[vm.Container, vm.Page])
+def model(request):
+    return request.param
+
+
+class TestSharedValidators:
+    def test_set_components_validator(self, model):
         with pytest.raises(ValidationError, match="Ensure this value has at least 1 item."):
-            Model(title="Title", components=[])
+            model(title="Title", components=[])
 
-    def test_set_layout_valid(self, Model):
-        Model(title="Title", components=[vm.Button(), vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
+    def test_set_layout_valid(self, model):
+        model(title="Title", components=[vm.Button(), vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
 
-    def test_set_layout_invalid(self, Model):
+    def test_set_layout_invalid(self, model):
         with pytest.raises(ValidationError, match="Number of page and grid components need to be the same."):
-            Model(title="Title", components=[vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
+            model(title="Title", components=[vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
 
-    def test_check_for_valid_component_types(self, Model):
+    def test_check_for_valid_component_types(self, model):
         with pytest.raises(
             ValidationError, match=re.escape("(allowed values: 'button', 'card', 'container', 'graph', 'table')")
         ):
-            Model(title="Page Title", components=[vm.Checklist()])
+            model(title="Page Title", components=[vm.Checklist()])
 
 
-class TestReusedLayoutFunctions:
+class TestSharedLayoutFunctions:
     @pytest.mark.parametrize("grid", [[[0, -1], [1, 2]], [[0, -1, 1, 2]], [[-1, -1, -1], [0, 1, 2]]])
     def test_get_unique_grid_component_ids(self, grid):
-        result = get_unique_grid_component_ids(self, grid)
+        result = get_unique_grid_component_ids(grid)
         expected = np.array([0, 1, 2])
 
         assert isinstance(result, np.ndarray)
         assert (result == expected).all()
 
-    @pytest.mark.parametrize("Model", [vm.Container, vm.Page])
-    def test_assign_component_grid_area_default(self, Model):
-        model = Model(title="Title", components=[vm.Button(), vm.Button()])
+    def test_assign_component_grid_area_default(self, model):
+        model = model(title="Title", components=[vm.Button(), vm.Button()])
         result = _assign_component_grid_area(model)
         assert_component_equal(
             result,
@@ -56,9 +63,8 @@ class TestReusedLayoutFunctions:
             keys_to_strip={"children"},
         )
 
-    @pytest.mark.parametrize("Model", [vm.Container, vm.Page])
-    def test_assign_component_grid_area_with_layout(self, Model):
-        model = Model(title="Title", components=[vm.Button(), vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
+    def test_assign_component_grid_area_with_layout(self, model):
+        model = model(title="Title", components=[vm.Button(), vm.Button()], layout=vm.Layout(grid=[[0, 1]]))
         result = _assign_component_grid_area(model)
         assert_component_equal(
             result,
