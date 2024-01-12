4b67cdfee3aadbe7fe198491ad73b0f039d734a4
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-12 13:57:49 +0100

    Add assets to dev app

--- vizro-core/examples/dev/app.py
+++ vizro-core/examples/dev/app.py
@@ -1,8 +1,8 @@
 """Example to show simple dashboard configuration."""
 
+import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-import vizro.models as vm
 
 df = px.data.iris()
 
@@ -10,7 +10,8 @@ page = vm.Page(
     title="My first dashboard",
     components=[
         vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
-        vm.Graph(id="hist_chart", figure=px.histogram(df, x="sepal_width", color="species"))],
+        vm.Graph(id="hist_chart", figure=px.histogram(df, x="sepal_width", color="species")),
+    ],
     controls=[
         vm.Filter(column="species"),
     ],
