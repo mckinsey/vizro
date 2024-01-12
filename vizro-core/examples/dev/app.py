03b187d860e3c6926e64eed64121709c9b661913
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-12 13:11:24 +0100

    Add dev example app

---
+++ vizro-core/examples/dev/app.py
@@ -0,0 +1,22 @@
+"""Example to show simple dashboard configuration."""
+
+import vizro.plotly.express as px
+from vizro import Vizro
+import vizro.models as vm
+
+df = px.data.iris()
+
+page = vm.Page(
+    title="My first dashboard",
+    components=[
+        vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
+        vm.Graph(id="hist_chart", figure=px.histogram(df, x="sepal_width", color="species"))],
+    controls=[
+        vm.Filter(column="species"),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[page])
+
+if __name__ == "__main__":
+    Vizro().build(dashboard).run()
