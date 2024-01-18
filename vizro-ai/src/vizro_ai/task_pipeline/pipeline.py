151fb7e771e62cc95fe7d21894dd07efcefeffc3
AnnaXiong <anna_xiong@mckinsey.com>
2024-01-18 14:16:17 -0500

    lint

--- vizro-ai/src/vizro_ai/task_pipeline/pipeline.py
+++ vizro-ai/src/vizro_ai/task_pipeline/pipeline.py
@@ -16,7 +16,7 @@ class Pipeline:
         self.components = []
         self.components_instances = {}
 
-    def add(self, component_class, input_keys: List = None, output_key: str = None):
+    def add(self, component_class, input_keys: List | None = None, output_key: str | None = None):
         """Add a component class to the pipeline along with its input and output specifications.
 
         Args:
