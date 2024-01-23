22a9cc4e918286e07654dcfcf684deef0428728c
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-23 14:21:21 +0100

    Add dev example

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,51 +3,229 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.actions import export_data
 from vizro.tables import dash_data_table
 
-df = px.data.gapminder()
+df_gapminder = px.data.gapminder()
 
-table_and_container = vm.Page(
-    title="Table and Container",
+
+single_tabs = vm.Page(
+    title="Single Tabs",
     components=[
-        vm.Container(
-            title="Container w/ Table",
-            components=[
-                vm.Table(
-                    title="Table Title",
-                    figure=dash_data_table(
-                        id="dash_data_table_country",
-                        data_frame=df,
-                        page_size=30,
-                    ),
-                )
-            ],
-        ),
-        vm.Container(
-            title="Another Container",
-            components=[
-                vm.Graph(
-                    figure=px.scatter(
-                        df,
-                        title="Graph_2",
-                        x="gdpPercap",
-                        y="lifeExp",
-                        size="pop",
-                        color="continent",
-                    ),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab I",
+                    components=[
+                        vm.Graph(
+                            id="graph_1",
+                            figure=px.line(
+                                df_gapminder,
+                                title="Graph_1",
+                                x="year",
+                                y="lifeExp",
+                                color="continent",
+                                line_group="country",
+                                hover_name="country",
+                            ),
+                        ),
+                        vm.Graph(
+                            id="graph_2",
+                            figure=px.box(
+                                df_gapminder,
+                                title="Graph_3",
+                                x="continent",
+                                y="lifeExp",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab II",
+                    components=[
+                        vm.Graph(
+                            id="graph_3",
+                            figure=px.scatter(
+                                df_gapminder,
+                                title="Graph_4",
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                        vm.Graph(
+                            id="graph_4",
+                            figure=px.box(
+                                df_gapminder,
+                                title="Graph_5",
+                                x="continent",
+                                y="lifeExp",
+                                color="continent",
+                            ),
+                        ),
+                    ],
                 ),
             ],
         ),
     ],
-    controls=[vm.Filter(column="continent")],
+    controls=[
+        vm.Parameter(
+            targets=[
+                "graph_1.y",
+                "graph_2.y",
+                "graph_3.y",
+                "graph_4.y",
+            ],
+            selector=vm.RadioItems(options=["lifeExp", "pop", "gdpPercap"], title="Select variable"),
+        ),
+        vm.Filter(column="continent"),
+    ],
+)
+
+
+single_tabs_action = vm.Page(
+    title="Single Tabs - with action",
+    components=[
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab I Table",
+                    components=[
+                        vm.Table(
+                            figure=dash_data_table(
+                                id="dash_datatable-1",
+                                data_frame=df_gapminder,
+                            ),
+                        ),
+                        vm.Button(
+                            text="Export data",
+                            actions=[
+                                vm.Action(function=export_data()),
+                            ],
+                        ),
+                    ],
+                )
+            ]
+        ),
+    ],
+    controls=[
+        vm.Filter(column="continent"),
+    ],
+)
+
+tabs_and_component = vm.Page(
+    title="Tabs and Component",
+    components=[
+        vm.Graph(
+            figure=px.box(
+                df_gapminder,
+                title="Graph_5",
+                x="continent",
+                y="lifeExp",
+                color="continent",
+            ),
+        ),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab I Table",
+                    components=[
+                        vm.Table(
+                            figure=dash_data_table(
+                                id="dash_datatable-1",
+                                data_frame=df_gapminder,
+                            ),
+                        ),
+                    ],
+                )
+            ]
+        ),
+    ],
+    controls=[
+        vm.Filter(column="continent"),
+    ],
+)
+
+
+multiple_tabs = vm.Page(
+    title="Multiple Tabs",
+    components=[
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab 1 container 1",
+                    components=[
+                        vm.Graph(
+                            figure=px.scatter(
+                                df_gapminder,
+                                title="Graph_44",
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab 1 container 2",
+                    components=[
+                        vm.Graph(
+                            figure=px.scatter(
+                                df_gapminder,
+                                title="Graph_441",
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        ),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab 2 container",
+                    components=[
+                        vm.Graph(
+                            figure=px.scatter(
+                                df_gapminder,
+                                title="Graph_45",
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab 2 container 2",
+                    components=[
+                        vm.Graph(
+                            figure=px.scatter(
+                                df_gapminder,
+                                title="Graph_451",
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ],
 )
 
 
 dashboard = vm.Dashboard(
-    title="Dashboard Title",
-    pages=[
-        table_and_container,
-    ],
+    title="Dashboard Title", pages=[single_tabs, single_tabs_action, tabs_and_component, multiple_tabs]
 )
 
 if __name__ == "__main__":
