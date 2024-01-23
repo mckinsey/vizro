dea0d8e987ab6bfc199505da5b5fb92ab7e5cdc5
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-01-23 19:05:46 +0100

    Lint

--- vizro-core/tests/unit/vizro/models/test_page.py
+++ vizro-core/tests/unit/vizro/models/test_page.py
@@ -95,7 +95,7 @@ class TestPageInstantiation:
     def test_check_for_valid_component_types(self):
         with pytest.raises(
             ValidationError,
-            match=re.escape("(allowed values: 'button', 'card', 'container', 'graph', 'table')"),
+            match=re.escape("(allowed values: 'button', 'card', 'container', 'graph', 'table', 'tabs')"),
         ):
             vm.Page(title="Page Title", components=[vm.Checklist()])
 
