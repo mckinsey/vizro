069bc32eff96d788753d0bd9563c7ac0ac339e3a
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-02-07 13:43:16 +0100

    Add js code

--- vizro-core/src/vizro/static/js/models/dashboard.js
+++ vizro-core/src/vizro/static/js/models/dashboard.js
@@ -1,9 +1,26 @@
+var maxWidth = screen.availWidth;
+var maxHeight = screen.availHeight;
+
 export function _update_dashboard_theme(checked) {
   return checked ? "vizro_light" : "vizro_dark";
 }
 
 export function _collapse_nav_panel(n_clicks, is_open) {
-  if (!n_clicks) return dash_clientside.no_update;
+  if (!n_clicks) {
+    if (maxWidth === 600 && maxHeight === 600) {
+      return [
+        false,
+        {
+          transform: "rotate(180deg)",
+          left: "12px",
+          transition: "transform 0.35s ease-in-out",
+        },
+        "Show Menu",
+        36,
+      ];
+    }
+    return dash_clientside.no_update;
+  }
   if (is_open) {
     return [
       false,
