dcd3d571f9e5958f59cc907cc75a2c1136451f67
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-02-20 14:52:15 +0100

    Add flex and minWidth

--- vizro-core/src/vizro/tables/dash_aggrid.py
+++ vizro-core/src/vizro/tables/dash_aggrid.py
@@ -52,7 +52,7 @@ def dash_ag_grid(data_frame=None, **kwargs):
     defaults = {
         # "className": "ag-theme-alpine vizro",
         "className": "ag-theme-quartz vizro",
-        "columnDefs": [{"field": col} for col in data_frame.columns],
+        "columnDefs": [{"field": col, "flex":1, "minWidth": 70} for col in data_frame.columns],
         "rowData": data_frame.apply(
             lambda x: (
                 x.dt.strftime("%Y-%m-%d")  # set date columns to `dateString` for AGGrid filtering to function
