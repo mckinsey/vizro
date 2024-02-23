01bf55e2c62e9ac92c5b51e4013f0af8964a0150
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-02-23 19:18:35 +0100

    Reduce height for selectors

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -10,29 +10,11 @@ from vizro.models._components.form._user_input import UserInput
 iris = px.data.iris()
 
 # Only added to container.components directly for dev example
-vm.Container.add_type("components", UserInput)
-vm.Container.add_type("components", TextArea)
-
-inputs = vm.Page(
-    title="User Text Inputs",
-    layout=vm.Layout(grid=[[0, 1]], col_gap="40px"),
-    components=[
-        vm.Container(
-            title="Input Components",
-            components=[
-                UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
-                TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
-            ],
-        ),
-        vm.Graph(
-            id="for_custom_chart",
-            figure=px.scatter(iris, title="Iris Dataset", x="sepal_length", y="petal_width", color="sepal_width"),
-        ),
-    ],
-)
+vm.Page.add_type("controls", UserInput)
+vm.Page.add_type("controls", TextArea)
 
 
-graph = vm.Page(
+selectors = vm.Page(
     title="Graph",
     components=[
         vm.Graph(
@@ -46,25 +28,35 @@ graph = vm.Page(
             selector=vm.Dropdown(title="Dropdown Label"),
         ),
         vm.Filter(
-            column="continent",
-            selector=vm.Checklist(title="Checklist Label"),
-        ),
-        vm.Filter(
             column="year",
             selector=vm.RangeSlider(title="Range Slider Label"),
-        ),
-        vm.Filter(
-            column="continent",
-            selector=vm.RadioItems(title="Radio Items Label"),
         ),
         vm.Filter(
             column="year",
             selector=vm.Slider(title="Slider Label"),
         ),
+        vm.Filter(
+            column="year",
+            selector=vm.RangeSlider(title="Range Slider Label", step=5),
+        ),
+        vm.Filter(
+            column="year",
+            selector=vm.Slider(title="Slider Label", step=5),
+        ),
+        vm.Filter(
+            column="continent",
+            selector=vm.Checklist(title="Checklist Label"),
+        ),
+        vm.Filter(
+            column="continent",
+            selector=vm.RadioItems(title="Radio Items Label"),
+        ),
+        UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
+        TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
     ],
 )
 
-dashboard = vm.Dashboard(pages=[inputs, graph])
+dashboard = vm.Dashboard(pages=[selectors])
 
 if __name__ == "__main__":
-    Vizro(external_stylesheets=[dbc.themes.BOOTSTRAP]).build(dashboard).run()
+    Vizro().build(dashboard).run()
