6d0814ed056c6a5cbb07c36f03030589b3d6cb0d
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-02-27 18:10:11 +0100

    Add yaml example

--- vizro-core/examples/features/yaml_version/app.py
+++ vizro-core/examples/features/yaml_version/app.py
@@ -2,14 +2,20 @@
 
 from pathlib import Path
 
+import numpy as np
 import vizro.plotly.express as px
 import yaml
 from vizro import Vizro
 from vizro.managers import data_manager
 from vizro.models import Dashboard
 
+tips = px.data.tips()
+tips["smoker"] = np.where(tips["smoker"] == "Yes", 1, 0)
 data_manager["iris"] = px.data.iris()
 data_manager["gapminder_2007"] = px.data.gapminder().query("year == 2007")
+data_manager["gapminder"] = px.data.gapminder()
+data_manager["tips"] = tips
+
 
 dashboard = yaml.safe_load(Path("dashboard.yaml").read_text(encoding="utf-8"))
 dashboard = Dashboard(**dashboard)
