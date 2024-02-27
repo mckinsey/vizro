ecf0c996803dc7b12e8326dc831f7f87a0333fee
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-02-27 13:05:10 +0100

    Add dev example for selectors as components

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -11,10 +11,17 @@ iris = px.data.iris()
 # Only added to container.components directly for dev example
 vm.Page.add_type("controls", UserInput)
 vm.Page.add_type("controls", TextArea)
+vm.Container.add_type("components", vm.Dropdown)
+vm.Container.add_type("components", vm.Checklist)
+vm.Container.add_type("components", vm.RadioItems)
+vm.Container.add_type("components", vm.Slider)
+vm.Container.add_type("components", vm.RangeSlider)
+vm.Container.add_type("components", TextArea)
+vm.Container.add_type("components", UserInput)
 
 
 selectors = vm.Page(
-    title="Graph",
+    title="Selectors - Controls",
     components=[
         vm.Graph(
             id="scatter_relation",
@@ -55,7 +62,30 @@ selectors = vm.Page(
     ],
 )
 
-dashboard = vm.Dashboard(pages=[selectors])
+form_components = vm.Page(
+    title="Selectors - Components",
+    components=[
+        vm.Container(
+            id="container-id",
+            title="Form",
+            components=[
+                UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
+                TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
+                vm.Dropdown(title="Dropdown - Single", options=["Option 1", "Option 2", "Option 3"], multi=False),
+                vm.Dropdown(title="Dropdown - Multi", options=["Option 1", "Option 2", "Option 3"], multi=True),
+                vm.Checklist(title="Checklist", options=["Option 1", "Option 2", "Option 3"]),
+                vm.RadioItems(title="Radio Items", options=["Option 1", "Option 2", "Option 3"]),
+                vm.Slider(title="Slider without marks", min=0, max=10),
+                vm.Slider(title="Slider with marks", min=0, max=10, step=1),
+                vm.RangeSlider(title="Range Slider without marks", min=0, max=10),
+                vm.RangeSlider(title="Range Slider with marks", min=0, max=10, step=1),
+                vm.Button()
+            ],
+        ),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[selectors, form_components])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
