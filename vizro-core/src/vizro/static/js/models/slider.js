de6eeaf1b91dc7930261a17f5e2ec39008d68a45
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-02-27 20:25:53 +0100

    Fix js code

--- vizro-core/src/vizro/static/js/models/slider.js
+++ vizro-core/src/vizro/static/js/models/slider.js
@@ -1,26 +1,26 @@
 export function _update_slider_values(start, slider, input_store, self_data) {
-  var text_value, trigger_id;
+  var end_value, trigger_id;
 
   trigger_id = dash_clientside.callback_context.triggered;
   if (trigger_id.length != 0) {
     trigger_id =
       dash_clientside.callback_context.triggered[0]["prop_id"].split(".")[0];
   }
-  if (trigger_id === `${self_data["id"]}_text_value`) {
+  if (trigger_id === `${self_data["id"]}_end_value`) {
     if (isNaN(start)) {
       return dash_clientside.no_update;
     }
-    text_value = start;
+    end_value = start;
   } else if (trigger_id === self_data["id"]) {
-    text_value = slider;
+    end_value = slider;
   } else {
-    text_value = input_store !== null ? input_store : self_data["min"];
+    end_value = input_store !== null ? input_store : self_data["min"];
   }
 
-  text_value = Math.min(
-    Math.max(self_data["min"], text_value),
+  end_value = Math.min(
+    Math.max(self_data["min"], end_value),
     self_data["max"],
   );
 
-  return [text_value, text_value, text_value];
+  return [end_value, end_value, end_value];
 }
