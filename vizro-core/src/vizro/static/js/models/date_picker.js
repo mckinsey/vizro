fb95dbd0d0d6a5dd4788d1f7b201aefc5ba3a9ba
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2024-02-29 12:20:27 +0100

    fixing bug on datepicker

--- vizro-core/src/vizro/static/js/models/date_picker.js
+++ vizro-core/src/vizro/static/js/models/date_picker.js
@@ -1,9 +1,11 @@
 export function _update_date_picker_values(value, input_store, self_data) {
   if (value === null) {
     return [input_store, input_store];
-  } else if (JSON.stringify(value) === JSON.stringify(self_data)) {
-    return [input_store, input_store];
   } else {
-    return [value, value];
+    if (dash_clientside.callback_context.triggered.length === 0) {
+      return [input_store, input_store];
+    } else {
+      return [value, value];
+    }
   }
 }
