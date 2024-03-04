e9a293ba13050d54682983afe665bb800ebfa001
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-03-04 18:36:37 +0100

    Make dev example work, so it passes tests

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,14 +3,14 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.tables import dash_data_table
+from vizro.tables import dash_ag_grid
 
 df = px.data.gapminder()
 
 page = vm.Page(
     title="Example of a Dash AG Grid",
     components=[
-        vm.AgGrid(title="Dash AG Grid", figure=dash_data_table(data_frame=df)),
+        vm.AgGrid(title="Dash AG Grid", figure=dash_ag_grid(data_frame=df)),
     ],
     controls=[vm.Filter(column="continent")],
 )
