36fb0771a9f2a89bd39229dc060702e2bb7283f8
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-03-08 16:28:58 +0100

    Fix bug that would prevent creation of custom AgGrid CapturedCallable

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,57 +1,57 @@
-"""Rough example used by developers."""
+"""Dev example."""
+
+from typing import List
 
 import vizro.models as vm
 import vizro.plotly.express as px
+from dash_ag_grid import AgGrid
 from vizro import Vizro
-from vizro.actions import filter_interaction
-from vizro.tables import dash_ag_grid, dash_data_table
+from vizro.models.types import capture
 
-df_gapminder = px.data.gapminder().query("year == 2007")
+df = px.data.gapminder().query("year == 2007")
 
-dashboard = vm.Dashboard(
-    pages=[
-        vm.Page(
-            title="Ag Grid - Filter interaction",
-            components=[
-                vm.AgGrid(
-                    figure=dash_ag_grid(data_frame=df_gapminder),
-                    actions=[vm.Action(function=filter_interaction(targets=["scatter"]))],
-                ),
-                vm.Graph(
-                    id="scatter",
-                    figure=px.scatter(
-                        df_gapminder,
-                        x="gdpPercap",
-                        y="lifeExp",
-                        size="pop",
-                        color="continent",
-                    ),
-                ),
-            ],
-            controls=[vm.Filter(column="continent")],
+
+@capture("ag_grid")
+def my_custom_aggrid(data_frame=None, chosen_columns: List[str] = None):
+    """Custom ag_grid."""
+    defaults = {
+        "className": "ag-theme-quartz-dark ag-theme-vizro",
+        "defaultColDef": {
+            "resizable": True,
+            "sortable": True,
+            "filter": True,
+            "filterParams": {
+                "buttons": ["apply", "reset"],
+                "closeOnApply": True,
+            },
+            "flex": 1,
+            "minWidth": 70,
+        },
+        "style": {"height": "100%"},
+    }
+    return AgGrid(
+        columnDefs=[{"field": col} for col in chosen_columns], rowData=data_frame.to_dict("records"), **defaults
+    )
+
+
+page = vm.Page(
+    title="Example of a custom Dash AgGrid",
+    components=[
+        vm.AgGrid(
+            id="custom_ag_grid",
+            title="Custom Dash DataTable",
+            figure=my_custom_aggrid(
+                data_frame=df, chosen_columns=["country", "continent", "lifeExp", "pop", "gdpPercap"]
+            ),
         ),
-        vm.Page(
-            title="Data Table - Filter interaction",
-            components=[
-                vm.Table(
-                    figure=dash_data_table(data_frame=df_gapminder),
-                    actions=[vm.Action(function=filter_interaction(targets=["scatter_2"]))],
-                ),
-                vm.Graph(
-                    id="scatter_2",
-                    figure=px.scatter(
-                        df_gapminder,
-                        x="gdpPercap",
-                        y="lifeExp",
-                        size="pop",
-                        color="continent",
-                    ),
-                ),
-            ],
-            controls=[vm.Filter(column="continent")],
-        ),
-    ]
+    ],
+    controls=[
+        vm.Parameter(
+            targets=["custom_ag_grid.chosen_columns"],
+            selector=vm.Dropdown(title="Choose columns", options=df.columns.to_list(), multi=True),
+        )
+    ],
 )
+dashboard = vm.Dashboard(pages=[page])
 
-if __name__ == "__main__":
-    Vizro().build(dashboard).run()
+Vizro().build(dashboard).run()
