82bd758294b4d76efb6150c842e9fa4e1a0016fd
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2024-03-12 18:05:20 +0100

    [Bug] Replace `dcc.Link` with `dbc.NavLink` (#364)

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,55 +3,67 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.actions import filter_interaction
-from vizro.tables import dash_ag_grid, dash_data_table
 
-df_gapminder = px.data.gapminder().query("year == 2007")
-
-dashboard = vm.Dashboard(
-    pages=[
-        vm.Page(
-            title="Ag Grid - Filter interaction",
-            components=[
-                vm.AgGrid(
-                    figure=dash_ag_grid(data_frame=df_gapminder),
-                    actions=[vm.Action(function=filter_interaction(targets=["scatter"]))],
-                ),
-                vm.Graph(
-                    id="scatter",
-                    figure=px.scatter(
-                        df_gapminder,
-                        x="gdpPercap",
-                        y="lifeExp",
-                        size="pop",
-                        color="continent",
-                    ),
-                ),
-            ],
-            controls=[vm.Filter(column="continent")],
+first_page = vm.Page(
+    title="First Page",
+    components=[
+        vm.Graph(
+            id="scatter_chart", figure=px.scatter(px.data.iris(), x="sepal_length", y="petal_width", color="species")
         ),
-        vm.Page(
-            title="Data Table - Filter interaction",
-            components=[
-                vm.Table(
-                    figure=dash_data_table(data_frame=df_gapminder),
-                    actions=[vm.Action(function=filter_interaction(targets=["scatter_2"]))],
-                ),
-                vm.Graph(
-                    id="scatter_2",
-                    figure=px.scatter(
-                        df_gapminder,
-                        x="gdpPercap",
-                        y="lifeExp",
-                        size="pop",
-                        color="continent",
-                    ),
-                ),
-            ],
-            controls=[vm.Filter(column="continent")],
-        ),
-    ]
+        vm.Graph(id="hist_chart", figure=px.histogram(px.data.iris(), x="sepal_width", color="species")),
+    ],
+    controls=[
+        vm.Filter(column="species", selector=vm.Dropdown(value=["ALL"])),
+    ],
 )
+
+home = vm.Page(
+    title="Homepage",
+    components=[
+        vm.Card(
+            text="""
+              ### No Href
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+              option, eaque rerum!
+          """,
+        ),
+        vm.Card(
+            text="""
+            ### /first-page
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            option, eaque rerum!
+        """,
+            href="/first-page",
+        ),
+        vm.Card(
+            text="""
+            ### https
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            option, eaque rerum!
+            """,
+            href="https://www.google.com",
+        ),
+        vm.Card(
+            text="""
+            ### http
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            option, eaque rerum!
+            """,
+            href="http://www.google.com",
+        ),
+    ],
+)
+
+
+dashboard = vm.Dashboard(pages=[home, first_page])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
