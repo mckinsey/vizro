b4e0d3b770b6462bcbcea6ca0476f787a57ad365
Antony Milne <antony.milne@quantumblack.com>
2024-03-13 11:14:39 +0000

    Remove final use of page_registry

--- vizro-core/src/vizro/actions/_action_loop/_action_loop_utils.py
+++ vizro-core/src/vizro/actions/_action_loop/_action_loop_utils.py
@@ -3,29 +3,23 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
-
-from dash import page_registry
-
 from vizro.managers import model_manager
-from vizro.managers._model_manager import ModelID
 
 if TYPE_CHECKING:
-    from vizro.models import Action, Page
+    from vizro.models import Action
     from vizro.models._action._actions_chain import ActionsChain
 
 
-def _get_actions_chains_on_registered_pages() -> List[ActionsChain]:
+def _get_actions_chains_on_all_pages() -> List[ActionsChain]:
     """Gets list of ActionsChain models for registered pages."""
+    from vizro.models import Page
+
     actions_chains: List[ActionsChain] = []
-    for registered_page in page_registry.values():
-        try:
-            page: Page = model_manager[registered_page["module"]]
-        except KeyError:
-            continue
-        actions_chains.extend(model_manager._get_page_actions_chains(page_id=ModelID(str(page.id))))
+    for page_id, _ in model_manager._items_with_type(Page):
+        actions_chains.extend(model_manager._get_page_actions_chains(page_id=page_id))
     return actions_chains
 
 
 def _get_actions_on_registered_pages() -> List[Action]:
     """Gets list of Action models for registered pages."""
-    return [action for action_chain in _get_actions_chains_on_registered_pages() for action in action_chain.actions]
+    return [action for action_chain in _get_actions_chains_on_all_pages() for action in action_chain.actions]
