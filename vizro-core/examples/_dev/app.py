c7298f8fc6ec3f0d7b764868de78a46f42ff78e2
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2024-03-13 13:49:50 +0100

    [Tidy] Replace `dash.page_registry` as page lookup with `model_manager` (#366)
    Co-authored-by: Antony Milne <antony.milne@quantumblack.com>

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,67 +3,150 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.models._components.form._text_area import TextArea
+from vizro.models._components.form._user_input import UserInput
 
-first_page = vm.Page(
-    title="First Page",
+iris = px.data.iris()
+
+# Only added to container.components directly for dev example
+vm.Page.add_type("controls", UserInput)
+vm.Page.add_type("controls", TextArea)
+vm.Container.add_type("components", vm.Dropdown)
+vm.Container.add_type("components", vm.Checklist)
+vm.Container.add_type("components", vm.RadioItems)
+vm.Container.add_type("components", vm.Slider)
+vm.Container.add_type("components", vm.RangeSlider)
+vm.Container.add_type("components", TextArea)
+vm.Container.add_type("components", UserInput)
+
+
+selectors = vm.Page(
+    title="Page 1",
     components=[
         vm.Graph(
-            id="scatter_chart", figure=px.scatter(px.data.iris(), x="sepal_length", y="petal_width", color="species")
+            id="scatter_relation",
+            figure=px.scatter(data_frame=px.data.gapminder(), x="gdpPercap", y="lifeExp", size="pop"),
         ),
-        vm.Graph(id="hist_chart", figure=px.histogram(px.data.iris(), x="sepal_width", color="species")),
     ],
     controls=[
-        vm.Filter(column="species", selector=vm.Dropdown(value=["ALL"])),
+        vm.Filter(
+            column="continent",
+            selector=vm.Dropdown(title="Dropdown Label"),
+        ),
+        vm.Filter(
+            column="year",
+            selector=vm.RangeSlider(title="Range Slider Label", step=1, marks=None),
+        ),
+        vm.Filter(
+            column="year",
+            selector=vm.Slider(title="Slider Label"),
+        ),
+        vm.Filter(
+            column="year",
+            selector=vm.RangeSlider(title="Range Slider Label", step=10),
+        ),
+        vm.Filter(
+            column="year",
+            selector=vm.Slider(title="Slider Label", step=10),
+        ),
+        vm.Filter(
+            column="continent",
+            selector=vm.Checklist(title="Checklist Label"),
+        ),
+        vm.Filter(
+            column="continent",
+            selector=vm.RadioItems(title="Radio Items Label"),
+        ),
+        UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
+        TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
     ],
 )
 
-home = vm.Page(
-    title="Homepage",
+form_components = vm.Page(
+    title="Page 2",
     components=[
-        vm.Card(
-            text="""
-              ### No Href
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-              option, eaque rerum!
-          """,
-        ),
-        vm.Card(
-            text="""
-            ### /first-page
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            option, eaque rerum!
-        """,
-            href="/first-page",
-        ),
-        vm.Card(
-            text="""
-            ### https
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            option, eaque rerum!
-            """,
-            href="https://www.google.com",
-        ),
-        vm.Card(
-            text="""
-            ### http
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            option, eaque rerum!
-            """,
-            href="http://www.google.com",
+        vm.Container(
+            id="container-id",
+            title="Form",
+            components=[
+                UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
+                TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
+                vm.Dropdown(title="Dropdown - Single", options=["Option 1", "Option 2", "Option 3"], multi=False),
+                vm.Dropdown(title="Dropdown - Multi", options=["Option 1", "Option 2", "Option 3"], multi=True),
+                vm.Checklist(title="Checklist", options=["Option 1", "Option 2", "Option 3"]),
+                vm.RadioItems(title="Radio Items", options=["Option 1", "Option 2", "Option 3"]),
+                vm.Slider(title="Slider without marks", min=0, max=10),
+                vm.Slider(title="Slider with marks", min=0, max=10, step=1),
+                vm.RangeSlider(title="Range Slider without marks", min=0, max=10),
+                vm.RangeSlider(title="Range Slider with marks", min=0, max=10, step=1),
+                vm.Button(),
+            ],
         ),
     ],
 )
 
 
-dashboard = vm.Dashboard(pages=[home, first_page])
+slider_marks = vm.Page(
+    title="Page 3",
+    layout=vm.Layout(grid=[[0, 1]], col_gap="100px"),
+    components=[
+        vm.Container(
+            id="container-id-2",
+            title="Range Slider",
+            components=[
+                vm.RangeSlider(title="Range Slider | step = None and marks = {}", min=0, max=10),
+                vm.RangeSlider(
+                    title="Range Slider | step = None and marks = {''}",
+                    min=0,
+                    max=10,
+                    step=None,
+                    marks={0: "0", 5: "5", 10: "10"},
+                ),
+                vm.RangeSlider(
+                    title="Range Slider | step = None and marks = None", min=0, max=10, step=None, marks=None
+                ),
+                vm.RangeSlider(title="Range Slider | step = 1 and marks = {}", min=0, max=10, step=1),
+                vm.RangeSlider(
+                    title="Range Slider | step = 1 and marks = {''}",
+                    min=0,
+                    max=10,
+                    step=1,
+                    marks={0: "0", 5: "5", 10: "10"},
+                ),
+                vm.RangeSlider(title="Range Slider | step = 1 and marks = None", min=0, max=10, step=1, marks=None),
+            ],
+        ),
+        vm.Container(
+            id="container-id-3",
+            title="Slider",
+            components=[
+                vm.Slider(title="Slider | step = None and marks = {}", min=0, max=10),
+                vm.Slider(
+                    title="Slider | step = None and marks = {''}",
+                    min=0,
+                    max=10,
+                    step=None,
+                    marks={0: "0", 5: "5", 10: "10"},
+                ),
+                vm.Slider(title="Slider | step = None and marks = None", min=0, max=10, step=None, marks=None),
+                vm.Slider(title="Slider | step = 1 and marks = {}", min=0, max=10, step=1),
+                vm.Slider(
+                    title="Slider | step = 1 and marks = {''}",
+                    min=0,
+                    max=10,
+                    step=1,
+                    marks={0: "0", 5: "5", 10: "10"},
+                ),
+                vm.Slider(title="Slider | step = 1 and marks = None", min=0, max=10, step=1, marks=None),
+            ],
+        ),
+    ],
+)
+
+dashboard = vm.Dashboard(
+    pages=[selectors, form_components, slider_marks],
+    navigation=vm.Navigation(nav_selector=vm.NavBar(pages=["Page 3", "Page 2", "Page 1"])),
+)
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
