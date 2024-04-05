80d096fc42c5aecfee0dac146167d70e0c51311a
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-04-05 13:56:37 +0200

    Make example pretty

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -12,18 +12,17 @@ gapminder_pos = px.data.gapminder()
 gapminder_neg = px.data.gapminder()
 gapminder_mixed = px.data.gapminder()
 gapminder_neg["lifeExp"] = gapminder_neg["lifeExp"] * (-1)
-gapminder_mixed["lifeExp"][:40] = gapminder_mixed["lifeExp"][:40] * (-1)
+gapminder_mixed["lifeExp"][:200] = gapminder_mixed["lifeExp"][:200] * (-1)
 
 
 @capture("graph")
-def variable_map(data_frame: pd.DataFrame = None, color: Optional[str] = None):
+def variable_map(data_frame: pd.DataFrame = None, color: Optional[str] = None, title: Optional[str]=None):
     """Custom choropleth figure that needs post update calls."""
     fig = px.choropleth(
         data_frame,
         locations="iso_alpha",
         color=color,
         hover_name="country",
-        animation_frame="year",
         labels={
             "year": "year",
             "lifeExp": "Life expectancy",
@@ -32,15 +31,14 @@ def variable_map(data_frame: pd.DataFrame = None, color: Optional[str] = None):
         },
         title="Global development over time",
     )
-    fig.update_layout(showlegend=False)
-    fig.update_yaxes(automargin=True)
-    fig.update_xaxes(automargin=True)
+    fig.update_layout(showlegend=False, title=title)
     fig.update_coloraxes(colorbar={"thickness": 10, "title": {"side": "right"}})
     return fig
 
 
 page = vm.Page(
     title="Autocolorscale",
+    layout=vm.Layout(grid=[[0, 1, 2]]),
     components=[
         vm.Graph(
             figure=variable_map(data_frame=gapminder_pos, color="lifeExp", title="Positive Life Expectancy"),
