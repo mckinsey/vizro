5217758366fb86bac152fa01d45922d38908e1f7
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-04-11 12:57:57 +0200

    Remove custom CSS for types

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -8,46 +8,28 @@ import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.models.types import capture
 
-gapminder_pos = px.data.gapminder()
-gapminder_neg = px.data.gapminder()
-gapminder_mixed = px.data.gapminder()
-gapminder_neg["lifeExp"] = gapminder_neg["lifeExp"] * (-1)
-gapminder_mixed.loc[:200, "lifeExp"] = gapminder_mixed.loc[:200, "lifeExp"] * (-1)
-
-
-@capture("graph")
-def variable_map(data_frame: pd.DataFrame = None, color: Optional[str] = None, title: Optional[str] = None):
-    """Custom choropleth figure that needs post update calls."""
-    fig = px.choropleth(
-        data_frame,
-        locations="iso_alpha",
-        color=color,
-        hover_name="country",
-        labels={
-            "year": "year",
-            "lifeExp": "Life expectancy",
-            "pop": "Population",
-            "gdpPercap": "GDP per capita",
-        },
-        title="Global development over time",
-    )
-    fig.update_layout(showlegend=False, title=title)
-    fig.update_coloraxes(colorbar={"thickness": 10, "title": {"side": "right"}})
-    return fig
-
 
 page = vm.Page(
     title="Autocolorscale",
-    layout=vm.Layout(grid=[[0, 1, 2]]),
     components=[
-        vm.Graph(
-            figure=variable_map(data_frame=gapminder_pos, color="lifeExp", title="Positive Life Expectancy"),
-        ),
-        vm.Graph(
-            figure=variable_map(data_frame=gapminder_neg, color="lifeExp", title="Negative Life Expectancy"),
-        ),
-        vm.Graph(
-            figure=variable_map(data_frame=gapminder_mixed, color="lifeExp", title="Mixed Life Expectancy"),
+vm.Card(
+            text="""
+                # This is an <h1> tag
+                ## This is an <h2> tag
+                ###### This is an <h6> tag
+                \n
+                >
+                > Block quotes are used to highlight text.
+                >
+                \n
+                * Item 1
+                * Item 2
+                \n
+                 *This text will be italic*
+                _This will also be italic_
+                **This text will be bold**
+                _You **can** combine them_
+            """,
         ),
     ],
 )
