ca209bb52b1f3da8ca04aa7f4615dbab686d5e70
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-04-22 15:01:30 +0200

    Fix unit tests and lint

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,14 +1,19 @@
 """Example to show dashboard configuration."""
+
+import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-import vizro.models as vm
 from vizro.tables import dash_ag_grid, dash_data_table
+
 df = px.data.iris()
 
 page = vm.Page(
     title="My first dashboard",
     components=[
-        vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", title="Title - Plotly Chart")),
+        vm.Graph(
+            id="scatter_chart",
+            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", title="Title - Plotly Chart"),
+        ),
         vm.AgGrid(title="Title - AG Grid", figure=dash_ag_grid(data_frame=df)),
         vm.Table(title="Title - DataTable", figure=dash_data_table(data_frame=df)),
     ],
