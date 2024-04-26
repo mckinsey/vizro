7ea2803887778359f536dce128d753aaae8f0976
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-04-26 13:46:31 +0200

    Lint

--- vizro-core/examples/_dev/scratch.py
+++ vizro-core/examples/_dev/scratch.py
@@ -1,16 +1,75 @@
+import numpy as np
 import pandas as pd
 
+region_mapping = {
+    **{state: "North East" for state in ["CT", "ME", "MA", "NH", "RI", "VT", "NJ", "NY", "PA"]},
+    **{state: "Mid West" for state in ["IL", "IN", "MI", "OH", "WI", "IA", "KS", "MN", "MO", "NE", "ND", "SD"]},
+    **{
+        state: "South"
+        for state in [
+            "DE",
+            "FL",
+            "GA",
+            "MD",
+            "NC",
+            "SC",
+            "VA",
+            "WV",
+            "DC",
+            "AL",
+            "KY",
+            "MS",
+            "TN",
+            "AR",
+            "LA",
+            "OK",
+            "TX",
+        ]
+    },
+    **{state: "West" for state in ["AZ", "CO", "ID", "MT", "NV", "NM", "UT", "WY", "AK", "CA", "HI", "OR", "WA"]},
+}
 
-df_complaints = pd.read_csv("data/Financial Consumer Complaints.csv")
+df_complaints = pd.read_csv("data/Financial Consumer Complaints.csv", na_values=['""'])
 
-print(df_complaints["Company response to consumer"].unique())
+# Clean cell values
+df_complaints.fillna("N/A", inplace=True)
 
-df_complaints['Date Received'] = pd.to_datetime(df_complaints['Date Received'], format='%m/%d/%y').dt.strftime('%Y-%m-%d')
-df_complaints['Date Sumbited'] = pd.to_datetime(df_complaints['Date Sumbited'], format='%m/%d/%y').dt.strftime('%Y-%m-%d')
-df_complaints.rename(columns={"Date Sumbited": "Date Submitted"}, inplace=True)
+# Convert to correct type
+df_complaints["Date Received"] = pd.to_datetime(df_complaints["Date Received"], format="%m/%d/%y").dt.strftime(
+    "%Y-%m-%d"
+)
+df_complaints["Date Sumbited"] = pd.to_datetime(df_complaints["Date Sumbited"], format="%m/%d/%y").dt.strftime(
+    "%Y-%m-%d"
+)
 
+# Rename columns
+df_complaints.rename(
+    columns={
+        "Date Sumbited": "Date Submitted",
+        "Submitted via": "Channel",
+        "Company response to consumer": "Company response - detailed",
+    },
+    inplace=True,
+)
 
+# Create additional columns
+df_complaints["Year-Month Received"] = pd.to_datetime(df_complaints["Date Received"], format="%Y-%m-%d").dt.strftime(
+    "%Y-%m"
+)
+df_complaints["Region"] = df_complaints["State"].map(region_mapping)
+df_complaints["Company response"] = np.where(
+    df_complaints["Company response - detailed"].str.contains("Closed"),
+    "Closed",
+    df_complaints["Company response - detailed"],
+)
+df_complaints["Company response - Closed"] = np.where(
+    df_complaints["Company response - detailed"].str.contains("Closed"),
+    df_complaints["Company response - detailed"],
+    "Not closed",
+)
 
-# Group by year and month, and calculate the sum for the numeric column
-df_complaints['Date Received YY-MM'] = pd.to_datetime(df_complaints['Date Received'], format='%Y-%m-%d').dt.strftime('%Y-%m')
-result = df_complaints.groupby(df_complaints['Date Received YY-MM']).aggregate({"Complaint ID": "count"}).reset_index()
+print(df_complaints["Product"].unique())
+print(df_complaints["Issue"].unique())
+print(df_complaints["Sub-issue"].unique())
+print(df_complaints["Company response"].unique())
+print(df_complaints["Company response - Closed"].unique())
