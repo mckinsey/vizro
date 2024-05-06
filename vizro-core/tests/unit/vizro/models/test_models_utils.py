a3d40665ce5d31b7c59268391e9568805cba0ca2
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-05-06 16:18:52 +0200

    Add unit tests for helper function

--- vizro-core/tests/unit/vizro/models/test_models_utils.py
+++ vizro-core/tests/unit/vizro/models/test_models_utils.py
@@ -8,6 +8,7 @@ except ImportError:  # pragma: no cov
     from pydantic import ValidationError
 
 import vizro.models as vm
+from vizro.models._models_utils import _clean_path
 
 
 class TestSharedValidators:
@@ -21,3 +22,37 @@ class TestSharedValidators:
             match=re.escape("(allowed values: 'ag_grid', 'button', 'card', 'container', 'graph', 'table', 'tabs')"),
         ):
             model_with_layout(title="Page Title", components=[vm.Checklist()])
+
+
+@pytest.mark.parametrize(
+    "test_string, expected",
+    [
+        ("Title", "title"),
+        ("Page Title", "page-title"),
+        ("Page Title!", "page-title"),
+        ("Another123Title", "another123title"),
+        ("Some_Example%Title", "some_exampletitle"),
+        ("!@#$%^&*()_+=", "_"),
+        ("", ""),
+    ],
+)
+def test_clean_path_with_url_enabled(test_string, expected):
+    cleaned_string = _clean_path(path=test_string, allowed_characters="-_/", enable_url=True)
+    assert cleaned_string == f"/{expected}"
+
+
+@pytest.mark.parametrize(
+    "test_string, expected",
+    [
+        ("Title", "title"),
+        ("Page Title", "page-title"),
+        ("Page Title!", "page-title"),
+        ("Another123Title", "another123title"),
+        ("Some_Example%Title", "some_exampletitle"),
+        ("!@#$%^&*()_+=", "_"),
+        ("", ""),
+    ],
+)
+def test_clean_path_with_url_disabled(test_string, expected):
+    cleaned_string = _clean_path(path=test_string, allowed_characters="-_/", enable_url=False)
+    assert cleaned_string == expected
