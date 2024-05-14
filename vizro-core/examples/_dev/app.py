df2c5e89ca36330c0257b5f46e90b7a5f8436e6c
petar-qb <petar_pejovic@external.mckinsey.com>
2024-05-14 11:34:54 +0200

    Adding dev app.py example

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -2,73 +2,32 @@
 
 import pandas as pd
 import vizro.models as vm
+import vizro.plotly.express as px
+from vizro.actions import export_data
 from vizro import Vizro
-from vizro.tables import dash_ag_grid
 
-# DATA
-data = {
-    "this_is_a_really_long_column_name_0": [
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-    ],
-    "short_col_name": [4, 5, 6],
-    "this_is_a_really_long_column_name_2": [
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-    ],
-    "short": [10, 11, 12],
-    "this_is_a_really_long_column_name_4": [
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-    ],
-    "this_is_a_really_long_column_name_6": [
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-        "sdfjalskdfnaksnfa;sldknfalksdnfl;kasdnfl;kasndflkasdf",
-    ],
-    "short4": [10, 11, 12],
-    "this_is_a_really_long_column_name_7": [13, 14, 15],
-}
+df = px.data.gapminder()
 
-df = pd.DataFrame(data)
-
-# df = px.data.gapminder()
+vm.Page.add_type("controls", vm.Button)
 
 page = vm.Page(
-    title="Example of a Dash AG Grid",
+    title="Export from control panel",
     components=[
-        vm.AgGrid(
-            title="Dash AG Grid",
-            figure=dash_ag_grid(
-                id="grid1",
-                data_frame=df,
-                defaultColDef={"flex": 1, "minWidth": 200},
-                persistence=True,
-                persisted_props=["filterModel", "columnSize"],  # ,persistence_type = "local" #columnSize="autoSize"#
-            ),
+        vm.Graph(
+            id="scatter_graph",
+            figure=px.scatter(data_frame=df, x="gdpPercap", y="lifeExp", color="continent")
         ),
     ],
-    controls=[vm.Filter(column="this_is_a_really_long_column_name_0")],
-)
-page2 = vm.Page(
-    title="Example of a Dash AG Grid 2",
-    components=[
-        vm.AgGrid(
-            title="Dash AG Grid 2",
-            figure=dash_ag_grid(
-                id="grid2",
-                data_frame=df,
-                columnSize="autoSize",
-                # , persistence=True, persisted_props=["filterModel","columnSize"]#,persistence_type = "local"
-            ),
+    controls=[
+        vm.Filter(column="continent"),
+        vm.Button(
+            text="Export",
+            actions=[vm.Action(function=export_data())]
         ),
     ],
-    # controls=[vm.Filter(column="continent")],
 )
-dashboard = vm.Dashboard(pages=[page, page2])
+
+dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
