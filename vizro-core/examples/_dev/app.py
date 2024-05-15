1cc8ebe1463e076457bf50bf38b5c6250d1b1b14
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2024-05-15 16:50:29 +0200

    [BUG] Fixing dash_data_table dropdown menu to open on click (#481)
    Signed-off-by: Petar Pejovic <108530920+petar-qb@users.noreply.github.com>

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,23 +1,46 @@
 """Dev app to try things out."""
 
+import numpy as np
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.actions import export_data
+from vizro.tables import dash_data_table
 
 df = px.data.gapminder()
 
-vm.Page.add_type("controls", vm.Button)
+dropdown_column = "Label"
+dropdown_options = ["-- A --", "-- B --", "-- C --"]
+
+# Add a 'Label' column to the data where options are randomly selected between 'A', 'B', 'C'
+df[dropdown_column] = np.random.choice(dropdown_options, size=len(df))
+
+# Drop the 'iso_alpha' and 'iso_num' columns
+df.drop(["iso_alpha", "iso_num"], axis=1, inplace=True)
+
 
 page = vm.Page(
-    title="Export from control panel",
+    title="Table Page",
     components=[
-        vm.Graph(id="scatter_graph", figure=px.scatter(data_frame=df, x="gdpPercap", y="lifeExp", color="continent")),
+        vm.Table(
+            title="Table",
+            figure=dash_data_table(
+                id="table",
+                data_frame=df,
+                columns=[
+                    {"name": i, "id": i, "presentation": "dropdown"} if i == dropdown_column else {"name": i, "id": i}
+                    for i in df.columns
+                ],
+                editable=True,
+                dropdown={
+                    dropdown_column: {
+                        "options": [{"label": i, "value": i} for i in dropdown_options],
+                        "clearable": False,
+                    },
+                },
+            ),
+        ),
     ],
-    controls=[
-        vm.Filter(column="continent"),
-        vm.Button(text="Export", actions=[vm.Action(function=export_data())]),
-    ],
+    controls=[vm.Filter(column="continent")],
 )
 
 dashboard = vm.Dashboard(pages=[page])
