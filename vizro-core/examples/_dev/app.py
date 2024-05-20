91f30412909c9b7afac43e946ad9179c523a53e1
Antony Milne <antony.milne@quantumblack.com>
2024-05-20 12:35:14 +0100

    Prepare for merge

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,66 +1,49 @@
 """Dev app to try things out."""
 
+import numpy as np
 import vizro.models as vm
 import vizro.plotly.express as px
-from flask_caching import Cache
 from vizro import Vizro
-from vizro.managers import data_manager
+from vizro.tables import dash_data_table
 
+df = px.data.gapminder()
 
-def conf(title=""):
-    return {
-        "x": "gdpPercap",
-        "y": "lifeExp",
-        "color": "continent",
-        "title": title,
-    }
+dropdown_column = "Label"
+dropdown_options = ["-- A --", "-- B --", "-- C --"]
 
+# Add a 'Label' column to the data where options are randomly selected between 'A', 'B', 'C'
+df[dropdown_column] = np.random.choice(dropdown_options, size=len(df))
 
-def get_data_function():
-    return px.data.gapminder().sample(20)
-
-
-data_manager["static_df"] = px.data.gapminder()
-data_manager["dynamic_df"] = get_data_function
-data_manager["dynamic_df_2"] = get_data_function
-
-# data_manager["dynamic_df"] = lambda: px.data.gapminder().sample(20)
-# data_manager["dynamic_df_2"] = lambda: px.data.gapminder().sample(20)
-
-
-data_manager.cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
-# data_manager.cache = Cache(config={"CACHE_TYPE": "RedisCache", "CACHE_REDIS_HOST": "localhost", "CACHE_REDIS_PORT": 6379})
-# data_manager["dynamic_df"].timeout = 30
-# data_manager["dynamic_df_2"].timeout = 10
+# Drop the 'iso_alpha' and 'iso_num' columns
+df.drop(["iso_alpha", "iso_num"], axis=1, inplace=True)
 
 
 page = vm.Page(
-    title="Data Manager",
-    layout=vm.Layout(
-        grid=[
-            [0, 1],
-            [2, 3],
-            [4, 5],
-        ]
-    ),
+    title="Table Page",
     components=[
-        vm.Graph(
-            id="graph_static_direct_1", figure=px.scatter(data_frame=px.data.gapminder(), **conf("Static direct 1"))
+        vm.Table(
+            title="Table",
+            figure=dash_data_table(
+                id="table",
+                data_frame=df,
+                columns=[
+                    {"name": i, "id": i, "presentation": "dropdown"} if i == dropdown_column else {"name": i, "id": i}
+                    for i in df.columns
+                ],
+                editable=True,
+                dropdown={
+                    dropdown_column: {
+                        "options": [{"label": i, "value": i} for i in dropdown_options],
+                        "clearable": False,
+                    },
+                },
+            ),
         ),
-        vm.Graph(
-            id="graph_static_direct_2", figure=px.scatter(data_frame=px.data.gapminder(), **conf("Static direct 2"))
-        ),
-        vm.Graph(id="graph_static_dm_1", figure=px.scatter(data_frame="static_df", **conf("Static DM 1"))),
-        vm.Graph(id="graph_static_dm_2", figure=px.scatter(data_frame="static_df", **conf("Static DM 2"))),
-        vm.Graph(id="graph_dynamic_dm_1", figure=px.scatter(data_frame="dynamic_df", **conf("Dynamic DM 1"))),
-        vm.Graph(id="graph_dynamic_dm_2", figure=px.scatter(data_frame="dynamic_df_2", **conf("Dynamic DM 2"))),
     ],
     controls=[vm.Filter(column="continent")],
 )
 
 dashboard = vm.Dashboard(pages=[page])
-app = Vizro().build(dashboard)
-server = app.dash.server
 
 if __name__ == "__main__":
-    app.run()
+    Vizro().build(dashboard).run()
