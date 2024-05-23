92120e0ba2d34cb110c43f4e927cfe562295153f
petar-qb <petar_pejovic@external.mckinsey.com>
2024-05-23 09:44:13 +0200

    New _dev/app example

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,29 +1,42 @@
-from vizro import Vizro
-import pandas as pd
-import vizro.plotly.express as px
 import vizro.models as vm
-
+import vizro.plotly.express as px
+from flask_caching import Cache
+from vizro import Vizro
+from vizro.actions import export_data
 from vizro.managers import data_manager
 
 
-def load_iris_data(number_of_points=10):
+# Note need to specify default value if have Filter since that calls data load function
+# Then have problem that filter options don't get updated when data source changes
+def load_iris_data(points=1):
     iris = px.data.iris()
-    return iris.sample(number_of_points)
+    return iris.sample(points)
 
 
 data_manager["iris"] = load_iris_data
 
+# SimpleCache
+data_manager.cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
+
+# RedisCache
+# data_manager.cache = Cache(
+#     config={"CACHE_TYPE": "RedisCache", "CACHE_REDIS_HOST": "localhost", "CACHE_REDIS_PORT": 6379}
+# )
+
+# Timeout
+data_manager["iris"].timeout = 30
+
+
 page = vm.Page(
-    title="Update the chart on page refresh",
+    title="My first page",
     components=[
-        vm.Graph(id="graph", figure=px.box("iris", x="species", y="petal_width", color="species"))
+        vm.Graph(id="graph", figure=px.scatter(data_frame="iris", x="sepal_length", y="petal_width", color="species")),
+        vm.Button(text="Export", actions=[vm.Action(function=export_data())]),
     ],
     controls=[
-        vm.Parameter(
-            targets=["graph.data_frame.number_of_points"],
-            selector=vm.Slider(min=1, max=20, step=1, value=10)
-        ),
-        vm.Filter(column="species"),
+        vm.Parameter(targets=["graph.x"], selector=vm.RadioItems(options=["sepal_length", "sepal_width"])),
+        vm.Parameter(targets=["graph.data_frame.points"], selector=vm.Slider(min=1, max=10, step=1)),
+        vm.Filter(column="species", selector=vm.Dropdown(options=["setosa", "versicolor", "virginica"])),
     ],
 )
 
