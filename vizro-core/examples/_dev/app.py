3d91fd6a35976221a6b3509997c9f5166a37aea9
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-06-04 14:20:07 +0200

    Double-check if titles align

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,17 +3,28 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.tables import dash_ag_grid
+from vizro.tables import dash_ag_grid, dash_data_table
 
 df = px.data.gapminder()
 
-page = vm.Page(
-    title="Example of a Dash AG Grid",
+page_one = vm.Page(
+    title="Dash AG Grid",
+    layout=vm.Layout(grid=[[0, 1]], col_gap="0px"),
     components=[
-        vm.AgGrid(title="Dash AG Grid", figure=dash_ag_grid(data_frame=df)),
+        vm.AgGrid(title="Equal Title One", figure=dash_ag_grid(data_frame=df)),
+        vm.Graph(figure=px.box(df,x="continent", y="lifeExp", title="Equal Title One"))
     ],
 )
-dashboard = vm.Dashboard(pages=[page])
+
+page_two = vm.Page(
+    title="Dash Data Table",
+    layout=vm.Layout(grid=[[0, 1]]),
+    components=[
+        vm.Table(title="Equal Title One", figure=dash_data_table(data_frame=df)),
+        vm.Graph(figure=px.box(df,x="continent", y="lifeExp", title="Equal Title One"))
+    ],
+)
+dashboard = vm.Dashboard(pages=[page_one, page_two])
 
 
 if __name__ == "__main__":
