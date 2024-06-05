b005f51b40210ec6553928bafa369afb30b560a1
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-06-05 14:10:12 +0200

    Revert changes in card

--- vizro-core/src/vizro/models/_components/card.py
+++ vizro-core/src/vizro/models/_components/card.py
@@ -1,89 +1,48 @@
-from typing import Literal, Optional
+from typing import Literal
 
-import pandas as pd
-from dash import dcc
+import dash_bootstrap_components as dbc
+from dash import dcc, get_relative_path
 
 try:
-    from pydantic.v1 import Field, PrivateAttr, validator
+    from pydantic.v1 import Field
 except ImportError:  # pragma: no cov
-    from pydantic import Field, PrivateAttr, validator
+    from pydantic import Field
 
-import vizro.cards as vc
-from vizro.managers import data_manager
 from vizro.models import VizroBaseModel
-from vizro.models._components._components_utils import _callable_mode_validator_factory, _process_callable_data_frame
 from vizro.models._models_utils import _log_call
-from vizro.models.types import CapturedCallable
 
 
 class Card(VizroBaseModel):
-    """Creates a card object to be displayed on dashboard.
+    """Creates a card utilizing `dcc.Markdown` as title and text component.
 
     Args:
         type (Literal["card"]): Defaults to `"card"`.
         text (str): Markdown string to create card title/text that should adhere to the CommonMark Spec.
         href (str): URL (relative or absolute) to navigate to. If not provided the Card serves as a text card
             only. Defaults to `""`.
-        figure (CapturedCallable): Card like object to be displayed. Defaults to `None`. For more information see
-            available card callables in [`vizro.cards`](vizro.cards).
 
     """
 
     type: Literal["card"] = "card"
-    text: str = Field("", description="Markdown string to create card text that should adhere to the CommonMark Spec.")
+    text: str = Field(
+        ..., description="Markdown string to create card title/text that should adhere to the CommonMark Spec."
+    )
     href: str = Field(
         "",
         description="URL (relative or absolute) to navigate to. If not provided the Card serves as a text card only.",
     )
-    figure: Optional[CapturedCallable] = Field(
-        None, import_path=vc, description="Dynamic card function to be visualized on dashboard."
-    )
-
-    _input_component_id: str = PrivateAttr()
-
-    # Component properties for actions and interactions
-    _output_component_property: str = PrivateAttr("children")
-
-    # Validators
-    @validator("figure")
-    def validate_figure(cls, figure, values):
-        if figure is None:
-            return (
-                vc.nav_card(text=values["text"], href=values["href"], data_frame=pd.DataFrame())
-                if values["href"]
-                else vc.text_card(text=values["text"], data_frame=pd.DataFrame())
-            )
-        return figure
-
-    _validate_callable_mode = _callable_mode_validator_factory("card")
-    _validate_callable = validator("figure", allow_reuse=True, always=True)(_process_callable_data_frame)
-
-    def __call__(self, **kwargs):
-        kwargs.setdefault("data_frame", data_manager[self["data_frame"]].load())
-        figure = self.figure(**kwargs) if self.figure else None
-        figure.id = self._input_component_id
-        return figure
-
-    def __getitem__(self, arg_name: str):
-        # pydantic discriminated union validation seems to try Graph["type"], which throws an error unless we
-        # explicitly redirect it to the correct attribute.
-        if self.figure:
-            if arg_name == "type":
-                return self.type
-            return self.figure[arg_name]
-
-    @_log_call
-    def pre_build(self):
-        self._input_component_id = self.figure._arguments.get("id", f"__input_{self.id}")
 
     @_log_call
     def build(self):
-        # LQ: Previously discussed to insert dbc.Card directly here and provide self.id to the outer container.
-        # However, this can affect several custom actions that rely on the id being previously provided to the inner
-        # text component instead of the outer card component. Shall we keep as is or pass the id through the relevant inner
-        # components? The last would require to provide an id argument to each CapturedCallable, which is also not optimal.
-        return dcc.Loading(
-            self.__call__(),
-            color="grey",
-            parent_className="loading-container",
+        text = dcc.Markdown(id=self.id, children=self.text, dangerously_allow_html=False)
+        card_content = (
+            dbc.NavLink(
+                children=text,
+                href=get_relative_path(self.href) if self.href.startswith("/") else self.href,
+            )
+            if self.href
+            else text
         )
+
+        card_class = "card-nav" if self.href else ""
+        return dbc.Card(children=card_content, className=card_class)
