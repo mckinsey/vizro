7fcb6fabcf8d753e06f8c803e3dc006e69e6bcbe
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2024-06-14 10:58:31 +0200

    [Tidy] Update Graph theme using clientside_callback (#523)

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -5,26 +5,46 @@ import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.tables import dash_ag_grid, dash_data_table
 
-df = px.data.gapminder()
+NUMBER_OF_COMPONENTS = 64
+
+
+def squared_layout(N):
+    """Util function."""
+    import math
+
+    size = math.ceil(math.sqrt(N))
+    layout = [[(i * size + j) if (i * size + j) < N else -1 for j in range(size)] for i in range(size)]
+    return layout
+
 
 page_one = vm.Page(
-    title="Dash AG Grid",
-    layout=vm.Layout(grid=[[0, 1]], col_gap="0px"),
+    title="Page 1",
+    layout=vm.Layout(grid=squared_layout(NUMBER_OF_COMPONENTS), col_gap="0px"),
     components=[
-        vm.AgGrid(title="Equal Title One", figure=dash_ag_grid(data_frame=df)),
-        vm.Graph(figure=px.box(df, x="continent", y="lifeExp", title="Equal Title One")),
+        vm.Graph(id=f"{i}_graph", figure=px.box(px.data.gapminder(), x="continent", y="lifeExp", title=f"Graph {i}"))
+        for i in range(NUMBER_OF_COMPONENTS)
     ],
+    controls=[vm.Filter(column="continent")],
 )
 
 page_two = vm.Page(
-    title="Dash Data Table",
-    layout=vm.Layout(grid=[[0, 1]]),
+    title="Page 2",
+    layout=vm.Layout(grid=[[0, 1], [2, 2]]),
     components=[
-        vm.Table(title="Equal Title One", figure=dash_data_table(data_frame=df)),
-        vm.Graph(figure=px.box(df, x="continent", y="lifeExp", title="Equal Title One")),
+        vm.Table(
+            id="P2_table", title="Data Table", figure=dash_data_table(id="P2_UL_table", data_frame=px.data.gapminder())
+        ),
+        vm.AgGrid(
+            id="P2_aggrid", title="AG Grid", figure=dash_ag_grid(id="P2_UL_aggrid", data_frame=px.data.gapminder())
+        ),
+        vm.Graph(id="P2_graph", figure=px.box(px.data.gapminder(), x="continent", y="lifeExp", title="Graph")),
     ],
+    controls=[vm.Filter(column="continent")],
 )
-dashboard = vm.Dashboard(pages=[page_one, page_two])
+dashboard = vm.Dashboard(
+    pages=[page_one, page_two],
+    # theme="vizro_light"
+)
 
 
 if __name__ == "__main__":
