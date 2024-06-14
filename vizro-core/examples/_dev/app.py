4507eb84bc4a16ccaa008234cf983fb1ac646f3b
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2024-06-14 11:54:31 +0200

    [Docs] Clarify what chart data will be exported (#525)

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -3,49 +3,63 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.tables import dash_ag_grid, dash_data_table
+from vizro.actions import export_data
+from vizro.models.types import capture
+from vizro.tables import dash_ag_grid
 
-NUMBER_OF_COMPONENTS = 64
+df = px.data.gapminder()
 
-
-def squared_layout(N):
-    """Util function."""
-    import math
-
-    size = math.ceil(math.sqrt(N))
-    layout = [[(i * size + j) if (i * size + j) < N else -1 for j in range(size)] for i in range(size)]
-    return layout
-
+# Vizro filter exporting Page --------------------------------------------
+# Solution based on https://vizro.readthedocs.io/en/stable/pages/user-guides/actions/#export-data
 
 page_one = vm.Page(
-    title="Page 1",
-    layout=vm.Layout(grid=squared_layout(NUMBER_OF_COMPONENTS), col_gap="0px"),
+    title="Vizro filters exporting",
+    layout=vm.Layout(grid=[[0]] * 5 + [[1]]),
     components=[
-        vm.Graph(id=f"{i}_graph", figure=px.box(px.data.gapminder(), x="continent", y="lifeExp", title=f"Graph {i}"))
-        for i in range(NUMBER_OF_COMPONENTS)
+        vm.AgGrid(id="ag_grid_1", title="Equal Title One", figure=dash_ag_grid(data_frame=df)),
+        vm.Button(text="Export data", actions=[vm.Action(function=export_data())]),
     ],
-    controls=[vm.Filter(column="continent")],
+    controls=[vm.Filter(column="continent"), vm.Filter(column="year")],
 )
+
+# AgGrid filter exporting Page -------------------------------------------
+# Solution based on https://dash.plotly.com/dash-ag-grid/export-data-csv
+
+
+# More about Vizro Custom Actions -> https://vizro.readthedocs.io/en/stable/pages/user-guides/custom-actions/
+@capture("action")
+def ag_grid_data_exporting():
+    """Custom Action."""
+    return True
+
 
 page_two = vm.Page(
-    title="Page 2",
-    layout=vm.Layout(grid=[[0, 1], [2, 2]]),
+    title="AgGrid filters exporting",
+    layout=vm.Layout(grid=[[0]] * 5 + [[1]]),  # grid = [[0], [0], [0], [0], [0], [1]]
     components=[
-        vm.Table(
-            id="P2_table", title="Data Table", figure=dash_data_table(id="P2_UL_table", data_frame=px.data.gapminder())
-        ),
         vm.AgGrid(
-            id="P2_aggrid", title="AG Grid", figure=dash_ag_grid(id="P2_UL_aggrid", data_frame=px.data.gapminder())
+            id="ag_grid_2",
+            title="Equal Title One",
+            figure=dash_ag_grid(
+                # underlying_ag_grid_2 is the id of the AgGrid component on the client-side. It is used to reference
+                # it's `exportDataAsCsv` property with the custom action below
+                id="underlying_ag_grid_2",
+                data_frame=df,
+                csvExportParams={
+                    "fileName": "ag_grid_2.csv",
+                },
+            ),
         ),
-        vm.Graph(id="P2_graph", figure=px.box(px.data.gapminder(), x="continent", y="lifeExp", title="Graph")),
+        vm.Button(
+            id="button_2",
+            text="Export data",
+            actions=[vm.Action(function=ag_grid_data_exporting(), outputs=["underlying_ag_grid_2.exportDataAsCsv"])],
+        ),
     ],
-    controls=[vm.Filter(column="continent")],
-)
-dashboard = vm.Dashboard(
-    pages=[page_one, page_two],
-    # theme="vizro_light"
+    controls=[vm.Filter(column="continent"), vm.Filter(column="year")],
 )
 
+dashboard = vm.Dashboard(pages=[page_one, page_two])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
