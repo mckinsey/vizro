ee606f818be55808e8b42d04592a688e7ed3af8f
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-06-18 11:26:29 +0200

    Incorporate last PR comments

--- vizro-core/src/vizro/figures/kpi_cards.py
+++ vizro-core/src/vizro/figures/kpi_cards.py
@@ -1,6 +1,5 @@
 """Contains default KPI card functions."""
 
-import warnings
 from typing import Optional
 
 import dash_bootstrap_components as dbc
@@ -10,45 +9,42 @@ from dash import html
 
 from vizro.models.types import capture
 
-FORMATTING_WARNING = (
-    "Custom format string detected. Note that the format string is being evaluated, so ensure that "
-    "only trusted user input is provided to prevent potential security risks."
-)
-
-VALUE_FORMAT_DEFAULT = "{value}"
-REFERENCE_FORMAT_DEFAULT = "{delta_relative:.1%} vs. reference ({reference})"
-
 
 @capture("figure")
 def kpi_card(  # noqa: PLR0913
     data_frame: pd.DataFrame,
     value_column: str,
     *,
-    value_format: str = VALUE_FORMAT_DEFAULT,
+    value_format: str = "{value}",
     agg_func: str = "sum",
     title: Optional[str] = None,
     icon: Optional[str] = None,
 ) -> dbc.Card:
     """Creates a styled KPI (Key Performance Indicator) card displaying a value.
 
+    **Warning:** Note that the format string provided to `value_format` is being evaluated, so ensure that only trusted
+    user input is provided to prevent
+    [potential security risks](https://stackoverflow.com/questions/76783239/is-it-safe-to-use-python-str-format-method-with-user-submitted-templates-in-serv).
+
     Args:
-        data_frame: Data frame containing the data.
+        data_frame: DataFrame containing the data.
         value_column: Column name of the value to be shown.
-        value_format: Format string to be applied to the value. It must be a valid Python format string where any of the
-            below placeholders can be used. Defaults to "{value}".
+        value_format: Format string to be applied to the value. It must be a
+            [valid Python format](https://docs.python.org/3/library/string.html#format-specification-mini-language)
+            string where any of the below placeholders can be used. Defaults to "{value}".
+
             - value: `value_column` aggregated by `agg_func`.
 
-            For more details see: https://docs.python.org/3/library/string.html#format-specification-mini-language
+            **Common examples include:**
 
-
-            Common examples include:
              - "{value}": Displays the raw value.
              - "${value:0.2f}": Formats the value as a currency with two decimal places.
              - "{value:.0%}": Formats the value as a percentage without decimal places.
              - "{value:,}": Formats the value with comma as a thousands separator.
 
         agg_func: String function name to be used for aggregating the data. Common options include
-            "sum", "min", "max", "mean" or "median". Default is "sum".
+            "sum", "mean" or "median". Default is "sum". For more information on possible functions, see
+            https://stackoverflow.com/questions/65877567/passing-function-names-as-strings-to-pandas-groupby-aggregrate.
         title: KPI title displayed on top of the card. If not provided, it defaults to the capitalized `value_column`.
         icon: Name of the icon from the [Google Material Icon Library](https://fonts.google.com/icons) to be displayed
             on the left side of the KPI title. If not provided, no icon is displayed.
@@ -61,10 +57,7 @@ def kpi_card(  # noqa: PLR0913
          A Dash Bootstrap Components card (`dbc.Card`) containing the formatted KPI value.
 
     """
-    if value_format != VALUE_FORMAT_DEFAULT:
-        warnings.warn(FORMATTING_WARNING, UserWarning)
-
-    title = title or value_column.title()
+    title = title or f"{agg_func} {value_column}".title()
     value = data_frame[value_column].agg(agg_func)
 
     return dbc.Card(
@@ -87,7 +80,7 @@ def kpi_card_reference(  # noqa: PLR0913
     value_column: str,
     reference_column: str,
     *,
-    value_format: str = VALUE_FORMAT_DEFAULT,
+    value_format: str = "{value}",
     reference_format: str = "{delta_relative:.1%} vs. reference ({reference})",
     agg_func: str = "sum",
     title: Optional[str] = None,
@@ -95,20 +88,25 @@ def kpi_card_reference(  # noqa: PLR0913
 ) -> dbc.Card:
     """Creates a styled KPI (Key Performance Indicator) card displaying a value in comparison to a reference value.
 
+    **Warning:** Note that the format string provided to `value_format` and `reference_format` is being evaluated,
+    so ensure that only trusted user input is provided to prevent
+    [potential security risks](https://stackoverflow.com/questions/76783239/is-it-safe-to-use-python-str-format-method-with-user-submitted-templates-in-serv).
+
     Args:
-        data_frame: Data frame containing the data.
+        data_frame: DataFrame containing the data.
         value_column: Column name of the value to be shown.
         reference_column: Column name of the reference value for comparison.
-        value_format: Format string to be applied to the value. It must be a valid Python format string where any of the
-            below placeholders can be used. Defaults to "{value}".
+        value_format: Format string to be applied to the value. It must be a
+            [valid Python format](https://docs.python.org/3/library/string.html#format-specification-mini-language)
+            string where any of the below placeholders can be used. Defaults to "{value}".
+
             - value: `value_column` aggregated by `agg_func`.
             - reference: `reference_column` aggregated by `agg_func`.
             - delta: Difference between `value` and `reference`.
             - delta_relative: Relative difference between `value` and `reference`.
 
-            For more details see: https://docs.python.org/3/library/string.html#format-specification-mini-language
+            **Common examples include:**
 
-            Common examples include:
              - "{value}": Displays the raw value.
              - "${value:0.2f}": Formats the value as a currency with two decimal places.
              - "{value:.0%}": Formats the value as a percentage without decimal places.
@@ -117,7 +115,8 @@ def kpi_card_reference(  # noqa: PLR0913
         reference_format: Format string to be applied to the reference. For more details on possible placeholders, see
             docstring on `value_format`. Defaults to "{delta_relative:.1%} vs. reference ({reference})".
         agg_func: String function name to be used for aggregating the data. Common options include
-            "sum", "min", "max", "mean" or "median". Default is "sum".
+            "sum", "mean" or "median". Default is "sum". For more information on possible functions, see
+            https://stackoverflow.com/questions/65877567/passing-function-names-as-strings-to-pandas-groupby-aggregrate.
         title: KPI title displayed on top of the card. If not provided, it defaults to the capitalized `value_column`.
         icon: Name of the icon from the [Google Material Icon Library](https://fonts.google.com/icons) to be displayed
             on the left side of the KPI title. If not provided, no icon is displayed.
@@ -130,13 +129,10 @@ def kpi_card_reference(  # noqa: PLR0913
         A Dash Bootstrap Components card (`dbc.Card`) containing the formatted KPI value and reference.
 
     """
-    if value_format != VALUE_FORMAT_DEFAULT or reference_format != REFERENCE_FORMAT_DEFAULT:
-        warnings.warn(FORMATTING_WARNING, UserWarning)
-
-    title = title or value_column.title()
+    title = title or f"{agg_func} {value_column}".title()
     value, reference = data_frame[[value_column, reference_column]].agg(agg_func)
     delta = value - reference
-    delta_relative = np.nan if reference == 0 else delta / reference
+    delta_relative = delta / reference if reference else np.nan
 
     return dbc.Card(
         [
