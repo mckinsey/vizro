639dac7a8d0ea136998491cd366eb598f12fca56
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-06-25 13:06:56 +0200

    Add compendium and rework bar chart

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,70 +1,70 @@
-"""Dev app to try things out."""
+"""Example to show dashboard configuration specified as pydantic models."""
 
-import pandas as pd
+from typing import Literal, Optional
+
+import dash_mantine_components as dmc
 import vizro.models as vm
+import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.figures import kpi_card, kpi_card_reference
 
-df_kpi = pd.DataFrame(
-    {"Actual": [100, 200, 700], "Reference": [100, 300, 500], "Reference Zero": [0, 0, 0], "Category": ["A", "B", "C"]}
+gapminder = px.data.gapminder()
+
+
+# class CodeHighlight(vm.VizroBaseModel):
+#    type: Literal["code_highlight"] = "code_highlight"
+#    language: Optional[str] = "python"
+#    code: str
+
+#    def build(self):
+#        return dmc.CodeHighlight(
+#            language=self.language,
+#            code=self.code,
+#        )
+#vm.Page.add_type("components", CodeHighlight)
+
+
+bar = vm.Page(
+    title="Bar Chart",
+    layout=vm.Layout(grid=[[0, 1, 1]]),
+    components=[
+        vm.Card(
+            text="""
+            
+            ### What is a bar chart?
+            
+            A Bar chart displays bars in lengths proportional to the values they represent. One axis of 
+            the chart shows the categories to compare and the other axis provides a value scale, 
+            starting with zero.
+            
+            &nbsp;
+            
+            ### When to use the bart chart?
+            
+            Select a Bar chart when you want to help your audience draw size comparisons and identify
+            patterns between categorical data, i.e., data that presents `how many?` in each category. You can
+            arrange your bars in any order to fit the message you wish to emphasise. Be mindful of labelling
+            clearly when you have a large number of bars. You may need to include a legend,
+            or use abbreviations in the chart with fuller descriptions below of the terms used.
+            
+            &nbsp;
+            
+            ### Code
+            ```python
+
+            vm.Graph(figure=px.bar(data_frame=gapminder.query('country == 'Germany''), x='year', y='pop')
+
+            ```
+        """
+        ),
+        vm.Graph(figure=px.bar(data_frame=gapminder.query("country == 'Germany'"), x="year", y="pop")),
+    ],
 )
 
-example_cards = [
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with value"),
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with aggregation", agg_func="median"),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with formatting",
-        value_format="${value:.2f}",
-    ),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with icon",
-        icon="shopping_cart",
-    ),
-]
 
-example_reference_cards = [
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="Delta Positive",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        agg_func="median",
-        title="Delta Negative",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Actual",
-        title="Delta Zero",
-        value_format="{value:.2f}$",
-        reference_format="{delta:.2f}$ vs. last year ({reference:.2f}$)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference Zero",
-        title="Reference Zero",
-        icon="shopping_cart",
-    ),
-]
 
-page = vm.Page(
-    title="KPI Indicators",
-    layout=vm.Layout(grid=[[0, 1, 2, 3], [4, 5, 6, 7], [-1, -1, -1, -1], [-1, -1, -1, -1]]),
-    components=[vm.Figure(figure=figure) for figure in example_cards + example_reference_cards],
-    controls=[vm.Filter(column="Category")],
-)
 
-dashboard = vm.Dashboard(pages=[page])
+
+dashboard = vm.Dashboard(pages=[bar])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
