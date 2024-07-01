3f90c1dafae4419822dd45d4930b748462c8fab1
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-01 16:14:40 +0200

    Add POC

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,67 +1,86 @@
 """Example to show dashboard configuration specified as pydantic models."""
 
-from typing import Literal, Optional
-
-import dash_mantine_components as dmc
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.models.types import capture
+from dash import html, dcc
+from typing import Literal, Optional
+import dash_bootstrap_components as dbc
+
+class CodeClipboard(vm.VizroBaseModel):
+    type: Literal["code_clipboard"] = "code_clipboard"
+    title: str = "Code"
+    text: str
+
+    def build(self):
+        return dbc.Card([
+            html.H3(self.title),
+            dcc.Markdown(self.text, id=self.id, className="code-block"),
+            dcc.Clipboard(target_id=self.id, className="code-clipboard")])
+
 
 gapminder = px.data.gapminder()
 
-
-# class CodeHighlight(vm.VizroBaseModel):
-#    type: Literal["code_highlight"] = "code_highlight"
-#    language: Optional[str] = "python"
-#    code: str
-
-#    def build(self):
-#        return dmc.CodeHighlight(
-#            language=self.language,
-#            code=self.code,
-#        )
-#vm.Page.add_type("components", CodeHighlight)
+vm.Page.add_type("components", CodeClipboard)
 
 
 bar = vm.Page(
     title="Bar Chart",
-    layout=vm.Layout(grid=[[0, 1, 1]]),
+    layout=vm.Layout(grid=[[0, 2, 2],
+                            [1, 2, 2],
+                           [1, 2, 2]]),
     components=[
         vm.Card(
             text="""
-            
+
             ### What is a bar chart?
-            
-            A Bar chart displays bars in lengths proportional to the values they represent. One axis of 
-            the chart shows the categories to compare and the other axis provides a value scale, 
+
+            A Bar chart displays bars in lengths proportional to the values they represent. One axis of
+            the chart shows the categories to compare and the other axis provides a value scale,
             starting with zero.
-            
+
             &nbsp;
-            
+
             ### When to use the bart chart?
-            
+
             Select a Bar chart when you want to help your audience draw size comparisons and identify
             patterns between categorical data, i.e., data that presents `how many?` in each category. You can
             arrange your bars in any order to fit the message you wish to emphasise. Be mindful of labelling
             clearly when you have a large number of bars. You may need to include a legend,
             or use abbreviations in the chart with fuller descriptions below of the terms used.
             
-            &nbsp;
-            
-            ### Code
-            ```python
-
-            vm.Graph(figure=px.bar(data_frame=gapminder.query('country == 'Germany''), x='year', y='pop')
-
-            ```
         """
         ),
+        CodeClipboard(text="""                
+                ```python
+                import vizro.models as vm
+                import vizro.plotly.express as px
+                from vizro import Vizro
+                
+                df = px.data.iris()
+                
+                page = vm.Page(
+                    title="My first page",
+                    components=[
+                        vm.Graph(
+                            figure=px.scatter_matrix(
+                                df, dimensions=["sepal_length", "sepal_width", "petal_length", "petal_width"], color="species"
+                            ),
+                        ),
+                    ],
+                    controls=[vm.Filter(column="species", selector=vm.Dropdown(title="Species"))],
+                )
+                
+                dashboard = vm.Dashboard(pages=[page])
+                
+                Vizro().build(dashboard).run()
+                ```
+                
+                """),
         vm.Graph(figure=px.bar(data_frame=gapminder.query("country == 'Germany'"), x="year", y="pop")),
     ],
 )
-
-
-
 
 
 dashboard = vm.Dashboard(pages=[bar])
