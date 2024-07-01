ae73db3cdd9ed36038b96e2bc35f1d6be7f28165
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-01 16:47:53 +0200

    Add intermediate layout

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -14,10 +14,18 @@ class CodeClipboard(vm.VizroBaseModel):
     text: str
 
     def build(self):
-        return dbc.Card([
+        return dbc.Accordion(
+        [
+            dbc.AccordionItem(
+                dbc.Card([
             html.H3(self.title),
             dcc.Markdown(self.text, id=self.id, className="code-block"),
-            dcc.Clipboard(target_id=self.id, className="code-clipboard")])
+            dcc.Clipboard(target_id=self.id, className="code-clipboard")]), title="SHOW CODE"
+            )
+        ],
+        start_collapsed=True,
+    )
+
 
 
 gapminder = px.data.gapminder()
@@ -26,10 +34,9 @@ vm.Page.add_type("components", CodeClipboard)
 
 
 bar = vm.Page(
-    title="Bar Chart",
-    layout=vm.Layout(grid=[[0, 2, 2],
-                            [1, 2, 2],
-                           [1, 2, 2]]),
+    title="Bar",
+    layout=vm.Layout(grid=[[0, 0, 2, 2, 2],
+                            [1, 1,2,2, 2]], col_gap="80px"),
     components=[
         vm.Card(
             text="""
@@ -45,7 +52,7 @@ bar = vm.Page(
             ### When to use the bart chart?
 
             Select a Bar chart when you want to help your audience draw size comparisons and identify
-            patterns between categorical data, i.e., data that presents `how many?` in each category. You can
+            patterns between categorical data, i.e., data that presents **how many?** in each category. You can
             arrange your bars in any order to fit the message you wish to emphasise. Be mindful of labelling
             clearly when you have a large number of bars. You may need to include a legend,
             or use abbreviations in the chart with fuller descriptions below of the terms used.
