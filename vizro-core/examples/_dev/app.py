a3a46239a7d5c2c52b7c3e3cc53890769c01fbaf
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-03 09:24:46 +0200

    Fix inconsistent return types

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,64 +1,49 @@
 """Dev app to try things out."""
 
-from typing import Optional
-
-import dash_bootstrap_components as dbc
 import pandas as pd
 import vizro.models as vm
 import vizro.plotly.express as px
-from dash import html
 from vizro import Vizro
-from vizro.figures import kpi_card
 from vizro.models.types import capture
 
-tips = px.data.tips
+df_stocks = px.data.stocks(datetimes=True)
+df_stocks_long = pd.melt(
+    df_stocks,
+    id_vars="date",
+    value_vars=["GOOG", "AAPL", "AMZN", "FB", "NFLX", "MSFT"],
+    var_name="stocks",
+    value_name="value",
+)
 
 
-@capture("figure")  # (1)!
-def custom_kpi_card(  # noqa: PLR0913
-    data_frame: pd.DataFrame,
-    value_column: str,
-    *,
-    value_format: str = "{value}",
-    agg_func: str = "sum",
-    title: Optional[str] = None,
-    icon: Optional[str] = None,
-) -> dbc.Card:  # (2)!
-    """Creates a custom KPI card."""
-    title = title or f"{agg_func} {value_column}".title()
-    value = data_frame[value_column].agg(agg_func)
+@capture("graph")
+def vizro_plot(data_frame, stocks_selected, **kwargs):
+    """Custom chart function."""
+    return px.line(data_frame[data_frame["stocks"].isin(stocks_selected)], **kwargs)
 
-    header = dbc.CardHeader(
-        [
-            html.H2(title),
-            html.P(icon, className="material-symbols-outlined") if icon else None,  # (3)!
-        ]
-    )
-    body = dbc.CardBody([value_format.format(value=value)])
-    return dbc.Card([header, body], className="card-kpi")
 
+df_stocks_long["value"] = df_stocks_long["value"].round(3)
 
 page = vm.Page(
-    title="Create your own KPI card",
-    layout=vm.Layout(grid=[[0, 1, -1, -1]] + [[-1, -1, -1, -1]] * 3),  # (4)!
+    title="My first page",
     components=[
-        vm.Figure(
-            figure=kpi_card(  # (5)!
-                data_frame=tips,
-                value_column="tip",
-                value_format="${value:.2f}",
-                icon="shopping_cart",
-                title="Default KPI card",
-            )
+        vm.Graph(
+            id="my_graph",
+            figure=vizro_plot(
+                data_frame=df_stocks_long,
+                stocks_selected=list(df_stocks_long["stocks"].unique()),
+                x="date",
+                y="value",
+                color="stocks",
+            ),
         ),
-        vm.Figure(
-            figure=custom_kpi_card(  # (6)!
-                data_frame=tips,
-                value_column="tip",
-                value_format="${value:.2f}",
-                icon="payment",
-                title="Custom KPI card",
-            )
+    ],
+    controls=[
+        vm.Parameter(
+            targets=["my_graph.stocks_selected"],
+            selector=vm.Dropdown(
+                options=[{"label": s, "value": s} for s in df_stocks_long["stocks"].unique()],
+            ),
         ),
     ],
 )
