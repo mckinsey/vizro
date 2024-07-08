cb34493e2458454043f5f72186155f8a0ab2c7fe
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-08 11:43:05 +0200

    Tidy

--- vizro-core/examples/_dev/utils/_pages_charts.py
+++ vizro-core/examples/_dev/utils/_pages_charts.py
@@ -5,6 +5,8 @@ import vizro.plotly.express as px
 
 from ._charts import CodeClipboard, FlexContainer, Markdown
 
+PAGE_GRID = [[0, 0, 0, 0, 0]] * 2 + [[1, 1, 1, 2, 2]] * 5
+
 gapminder = px.data.gapminder()
 iris = px.data.iris()
 stocks = px.data.stocks()
@@ -17,9 +19,7 @@ vm.Container.add_type("components", Markdown)
 # CHART PAGES -------------------------------------------------------------
 line = vm.Page(
     title="Line",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -70,22 +70,24 @@ line = vm.Page(
 
 scatter = vm.Page(
     title="Scatter",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
 
             #### What is a scatter?
 
-            A scatter plot is a two-dimensional data visualisation using dots to represent the values obtained for two different variables - one plotted along the x-axis and the other plotted along the y-axis.
+            A scatter plot is a two-dimensional data visualisation using dots to represent the values obtained for two 
+            different variables - one plotted along the x-axis and the other plotted along the y-axis.
 
             &nbsp;
 
             #### When to use it?
 
-            Use Scatter Plots when you want to show the relationship between two variables. Scatter plots are sometimes called Correlation plots because they show how two variables are correlated. Scatter plots are ideal when you have paired numerical data and you want to see if one variable impacts the other. However, do remember that correlation is not causation, and another unnoticed variable may be influencing results. Make sure your audience does not draw the wrong conclusions.
+            Use Scatter Plots when you want to show the relationship between two variables. Scatter plots are sometimes 
+            called Correlation plots because they show how two variables are correlated. Scatter plots are ideal when 
+            you have paired numerical data and you want to see if one variable impacts the other. However, do remember 
+            that correlation is not causation. Make sure your audience does not draw the wrong conclusions.
         """
         ),
         vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
@@ -119,9 +121,7 @@ scatter = vm.Page(
 
 bar = vm.Page(
     title="Bar",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -182,9 +182,7 @@ bar = vm.Page(
 
 column = vm.Page(
     title="Column",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -245,9 +243,7 @@ column = vm.Page(
 
 pie = vm.Page(
     title="Pie",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -260,9 +256,13 @@ pie = vm.Page(
 
             #### When to use it?
 
-            Use the Pie chart when you need to show your audience a quick view of how data is distributed proportionately, with percentages highlighted. The different values you present must add up to a total and equal 100%.
-
-            The downsides are that Pie charts tend to occupy more space than many other charts, they don’t work well with more than a few values because labelling small segments is challenging, and it can be difficult to accurately compare the sizes of the segments.
+            Use the Pie chart when you need to show your audience a quick view of how data is distributed
+            proportionately, with percentages highlighted. The different values you present must add up to a total and 
+            equal 100%. 
+            
+            The downsides are that Pie charts tend to occupy more space than other charts, they don’t 
+            work well with more than a few values because labelling small segments is challenging, and it can be 
+            difficult to accurately compare the sizes of the segments.
 
         """
         ),
@@ -306,9 +306,7 @@ pie = vm.Page(
 
 donut = vm.Page(
     title="Donut",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -366,9 +364,7 @@ donut = vm.Page(
 
 boxplot = vm.Page(
     title="Boxplot",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
@@ -428,9 +424,7 @@ boxplot = vm.Page(
 
 violin = vm.Page(
     title="Violin",
-    layout=vm.Layout(
-        grid=[[0, 0, 0, 0, 0]] * 1 + [[1, 1, 1, 2, 2]] * 2,
-    ),
+    layout=vm.Layout(grid=PAGE_GRID),
     components=[
         vm.Card(
             text="""
