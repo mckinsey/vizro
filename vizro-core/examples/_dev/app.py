3d43142afc11af33dc6fa8b7203f27614b78027b
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-10 13:51:53 +0200

    Add local overwrites to test

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -9,16 +9,12 @@ df_kpi = pd.DataFrame({"Actual": [100, 200, 700], "Reference": [100, 300, 500], 
 # Add single CSS file figures.css or
 vizro_bootstrap = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/vizro-bootstrap.min.css"
 vizro_css = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/figures.css"
-vizro_tokens = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/token_names.css"
-vizro_variables = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/variables.css"
 
 # Add entire assets folder from Vizro
 app = Dash(
     external_stylesheets=[
         # dbc.themes.BOOTSTRAP,
         "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
-        vizro_tokens,
-        vizro_variables,
         vizro_bootstrap,
         vizro_css,
     ],
@@ -43,7 +39,7 @@ app.layout = [
                 title="KPI Card II",
             ),
         ],
-        className="vizro_dark"
+        className="vizro_light"
     ),
 ]
 
