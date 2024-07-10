6ab16e3e7ce4e59b4e692a06ae23c95a40a50720
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-10 13:20:52 +0200

    Add raw links

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -8,17 +8,19 @@ from vizro.figures.undecorated import kpi_card, kpi_card_reference
 df_kpi = pd.DataFrame({"Actual": [100, 200, 700], "Reference": [100, 300, 500], "Category": ["A", "B", "C"]})
 
 # Add single CSS file figures.css or
-vizro_bootstrap = "https://github.com/mckinsey/vizro/blob/main/vizro-core/src/vizro/static/css/vizro-bootstrap.min.css"
+vizro_bootstrap = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/vizro-bootstrap.min.css"
 vizro_css = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/figures.css"
-# vizro_tokens = "https://github.com/mckinsey/vizro/blob/main/vizro-core/src/vizro/static/css/token_names.css"
-# vizro_variables = "https://github.com/mckinsey/vizro/blob/main/vizro-core/src/vizro/static/css/vizro-bootstrap.min.css"
+vizro_tokens = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/token_names.css"
+vizro_variables = "https://raw.githubusercontent.com/mckinsey/vizro/main/vizro-core/src/vizro/static/css/variables.css"
 
 # Add entire assets folder from Vizro
 app = Dash(
     external_stylesheets=[
         # dbc.themes.BOOTSTRAP,
-        vizro_bootstrap,
         "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
+        vizro_tokens,
+        vizro_variables,
+        vizro_bootstrap,
         vizro_css,
     ],
 )
@@ -41,7 +43,8 @@ app.layout = [
                 icon="payment",
                 title="KPI Card II",
             ),
-        ]
+        ],
+        className="vizro_dark"
     ),
 ]
 
