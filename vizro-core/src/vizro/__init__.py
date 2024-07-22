883939e1cb4158a528a3f9fa6116f196afa316e9
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-07-22 19:03:35 +0200

    Add changes

--- vizro-core/src/vizro/__init__.py
+++ vizro-core/src/vizro/__init__.py
@@ -1,19 +1,21 @@
 import logging
 import os
+from dash.development.base_component import ComponentRegistry
 
 from ._vizro import Vizro
+
+ComponentRegistry.registry.add("vizro")
 
 _css_dist = [
     {
         "relative_package_path": "static/css/figures.css",
         "namespace": "vizro",
+
     },
 ]
 
 _js_dist = []
-
 __all__ = ["Vizro"]
-
 __version__ = "0.1.20.dev0"
 
 logging.basicConfig(level=os.getenv("VIZRO_LOG_LEVEL", "WARNING"))
