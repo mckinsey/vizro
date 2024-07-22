d4b53056fadce205b52940a0e80b76a6b872d965
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2024-07-22 12:03:39 +0200

    review fixes

---
+++ tools/scan_yaml_for_risky_text.py.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+
+# according to this article: https://nathandavison.com/blog/github-actions-and-the-threat-of-malicious-pull-requests
+# we should avoid using `pull_request_target` for security reasons
+risky_text = "pull_request_target"
+
+
+def find_risky_files(path: str):
+    return {str(file) for file in Path(path).rglob("*.yml") if f"{risky_text}" in file.read_text()}
+
+
+if __name__ == '__main__':
+    risky_files = find_risky_files(sys.argv[1])
+    if risky_files:
+        sys.exit(f"{risky_text} found in files {risky_files}.")
