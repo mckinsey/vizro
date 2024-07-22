3df80d3fac1f3810ecff77565b83937367014bda
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2024-07-22 15:02:04 +0200

    unit tests for dashboard creation

--- vizro-ai/tests/unit/vizro-ai/dashboard/graph/conftest.py
+++ vizro-ai/tests/unit/vizro-ai/dashboard/graph/conftest.py
@@ -1,15 +1,28 @@
+import pandas as pd
 import pytest
-import vizro.plotly.express as px
-from vizro_ai.dashboard.graph.code_generation import GraphState
-from vizro_ai.dashboard.utils import DfMetadata
+from langchain_core.messages import HumanMessage
+from vizro_ai.dashboard.graph.dashboard_creation import GraphState
+from vizro_ai.dashboard.utils import DfMetadata, MetadataContent
 
 
 @pytest.fixture
-def graph_state():
-    state = GraphState(messages="", dfs=[px.data.medals_long()], df_metadata=DfMetadata(), pages=[])
+def dataframes():
+    return [pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [4, 5, 6, 7, 8]})]
 
-    # dfs: List[pd.DataFrame]
-    # df_metadata: DfMetadata
-    # dashboard_plan: DashboardPlanner = None
-    # pages: Annotated[List, operator.add]
-    # dashboard: vm.Dashboard = None
+
+@pytest.fixture
+def df_metadata():
+    df_metadata = DfMetadata({})
+    df_metadata.metadata["gdp_chart"] = MetadataContent(
+        df_schema={"a": "int64", "b": "int64"},
+        df=pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [4, 5, 6, 7, 8]}),
+        df_sample=pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [4, 5, 6, 7, 8]}),
+    )
+    return df_metadata
+
+
+@pytest.fixture
+def graph_state(dataframes, df_metadata):
+    return GraphState(
+        messages=[HumanMessage(content="contents of the message")], dfs=dataframes, df_metadata=df_metadata, pages=[]
+    )
--- vizro-ai/tests/unit/vizro-ai/dashboard/graph/test_dashboard_creation.py
+++ vizro-ai/tests/unit/vizro-ai/dashboard/graph/test_dashboard_creation.py
@@ -6,17 +6,33 @@ try:
 except ImportError:  # pragma: no cov
     from pydantic import ValidationError
 
+from langchain_core.messages import HumanMessage
+from vizro_ai.dashboard.graph.dashboard_creation import GraphState
+from vizro_ai.dashboard.utils import DfMetadata
+
 
 class TestConfig:
     """Test GraphState config creation"""
 
+    def test_graph_state_instantiation(self, graph_state, dataframes):
+        assert isinstance(graph_state, GraphState)
+        assert graph_state.messages[0].content == "contents of the message"
+        assert graph_state.dfs == dataframes
+        assert "gdp_chart" in graph_state.df_metadata.metadata
+        assert graph_state.pages == []
+
     @pytest.mark.parametrize(
         "dataframes, output_error",
         [
-            (pd.DataFrame(), "dfs must be a list"),
-            ([pd.DataFrame(), {}], "Each element in dfs must be a Pandas DataFrame"),
+            (pd.DataFrame(), "value is not a valid list"),
+            ([pd.DataFrame(), {}], "instance of DataFrame expected"),
         ],
     )
     def test_check_dataframes(self, dataframes, output_error):
         with pytest.raises(ValidationError, match=output_error):
-            pass
+            GraphState(
+                messages=[HumanMessage(content="contents of the message")],
+                dfs=dataframes,
+                df_metadata=DfMetadata(),
+                pages=[],
+            )
