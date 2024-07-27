022aca696a2d44c9857fd0c63205aafc7eb1b54e
Lingyi Zhang <lingyi_zhang@mckinsey.com>
2024-07-27 03:09:19 -0400

    Fix typo

--- vizro-ai/src/vizro_ai/plot/components/chart_selection.py
+++ vizro-ai/src/vizro_ai/plot/components/chart_selection.py
@@ -12,7 +12,7 @@ except ImportError:  # pragma: no cov
 from langchain_core.language_models.chat_models import BaseChatModel
 
 from vizro_ai.chains._chain_utils import _log_time
-from vizro_ai.plot.components import VizroAiComponentBase
+from vizro_ai.plot.components import VizroAIComponentBase
 from vizro_ai.plot.schema_manager import SchemaManager
 
 # initialization of schema manager, and register schema needed
@@ -39,7 +39,7 @@ chart_type_prompt = "choose a best chart types for this df info:{df_schema}, {df
 
 
 # 3. Define Component
-class GetChartSelection(VizroAiComponentBase):
+class GetChartSelection(VizroAIComponentBase):
     """Get Chart Types.
 
     Attributes
