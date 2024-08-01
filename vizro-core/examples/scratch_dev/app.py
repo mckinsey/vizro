5e5d082e2ca6dccadca8c729bdcd19b5e6ca6dc7
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-08-01 17:16:03 +0200

    Add user example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -27,6 +27,9 @@ b = dict(min=0, max=2, step=0.1, marks={0: "a", 1: "x", 2: "y"})  # noqa: C408
 # Mixed float and int: This works in Vizro, while it only partially works in Dash
 c = dict(min=0, max=1, step=0.1, marks={0: "a", 0.5: "x", 1.0: "y"})  # noqa: C408
 
+# User example
+e = dict(min=0, max=1, step=0.01, marks={0: "0%", 0.21: "MARKET", 1.0: "100%"})  # noqa: C408
+
 # Other test example: https://github.com/mckinsey/vizro/pull/266
 d = dict(min=2, max=5, step=1, value=3)  # noqa: C408
 
@@ -35,17 +38,18 @@ page = vm.Page(
     components=[
         vm.Container(
             title="vm.Sliders",
-            layout=vm.Layout(grid=[[0, 1, 2, 3]]),
-            components=[vm.Slider(**a), vm.Slider(**b), vm.Slider(**c), vm.Slider(**d)],
+            layout=vm.Layout(grid=[[0, 1, 2, 3, 4]]),
+            components=[vm.Slider(**a), vm.Slider(**b), vm.Slider(**c), vm.Slider(**d), vm.Slider(**e)],
         ),
         vm.Container(
             title="dcc.Sliders",
-            layout=vm.Layout(grid=[[0, 1, 2, 3]]),
+            layout=vm.Layout(grid=[[0, 1, 2, 3, 4]]),
             components=[
                 PureDashSlider(kwargs=a),
                 PureDashSlider(kwargs=b),
                 PureDashSlider(kwargs=c),
                 PureDashSlider(kwargs=d),
+                PureDashSlider(kwargs=e),
             ],
         ),
     ],
