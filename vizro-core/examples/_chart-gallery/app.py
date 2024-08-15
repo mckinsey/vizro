bd8db16416199ce5428d26a2fa9010eec6d49ba4
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-08-15 17:30:21 +0200

    Improve layout

--- vizro-core/examples/_chart-gallery/app.py
+++ vizro-core/examples/_chart-gallery/app.py
@@ -44,7 +44,7 @@ def make_homepage_container(chart_group: ChartGroup) -> vm.Container:
     # deviation-butterfly, which both have title "Butterfly", correspond to a single card.
     return vm.Container(
         title=chart_group.name,
-        layout=vm.Layout(grid=[[0, 1, 1, 1]], col_gap="40px"),
+        layout=vm.Layout(grid=[[0, 1, 1]], col_gap="40px"),
         components=[
             Markdown(text=chart_group.intro_text, classname="intro-text"),
             FlexContainer(
