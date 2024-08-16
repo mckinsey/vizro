cafa7073ea5d8323df38d36261619f5af087fb51
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-08-16 10:36:33 +0200

    Push dev example for demo

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -4,9 +4,18 @@ import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
 from vizro.tables import dash_data_table
+from vizro.models.types import capture
 
 df = px.data.iris()
 
+@capture("graph")
+def my_graph(data_frame):
+    fig = px.scatter(data_frame, x="sepal_width", y="sepal_length")
+    print(fig.layout.margin.t)  # This is None
+    print(fig.layout.template.layout.margin.t)  # This is 64 our default
+    fig.update_layout(margin_t=0)
+    print(fig.layout.margin.t) # This is 0 now
+    return fig
 
 page = vm.Page(
     title="Test",
@@ -15,7 +24,7 @@ page = vm.Page(
     ),
     components=[
         # Graph
-        vm.Graph(figure=px.scatter(df, x="sepal_width", y="sepal_length", title="My Graph")),
+        vm.Graph(figure=my_graph(df)),
         vm.Table(figure=dash_data_table(df), title="My Table"),
         vm.Graph(
             figure=px.scatter(
