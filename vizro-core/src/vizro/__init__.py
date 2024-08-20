2d15a84700261f3a4605f614deb3c62bbab318cb
Alexey Snigir <35569332+l0uden@users.noreply.github.com>
2024-08-20 15:16:21 +0200

    [Release] Release of vizro 0.1.20 (#639)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
    Co-authored-by: huong-li-nguyen <huong_li_nguyen@mckinsey.com>

--- vizro-core/src/vizro/__init__.py
+++ vizro-core/src/vizro/__init__.py
@@ -13,7 +13,7 @@ pio.templates["vizro_light"] = light
 
 __all__ = ["Vizro"]
 
-__version__ = "0.1.20.dev0"
+__version__ = "0.1.20"
 
 
 # For the below _css_dist to be used by Dash, it must be retrieved by dash.resources.Css.get_all_css(). This means it
