4454b18b82486f14b4fefcbd7015ce9587dd89c7
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-09-02 10:33:08 +0200

    Add example app to check

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -3,13 +3,15 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.tables import dash_data_table
+from vizro.tables import dash_data_table, dash_ag_grid
 
 df = px.data.iris()
 
+HEADER = """This is a subtitle"""
+FOOTER = """Data source: [Iris dataset](https://plotly.com/python/plotly-express/)"""
 
 page = vm.Page(
-    title="Testasfsadfsadf",
+    title="Fig Title",
     layout=vm.Layout(
         grid=[[0, 1, 2, 3]],
     ),
@@ -24,8 +26,49 @@ page = vm.Page(
         ),
     ],
 )
+page_two = vm.Page(
+    title="Graph Title",
+    layout=vm.Layout(
+        grid=[[0, 1, 2, 3]],
+    ),
+    components=[
+        vm.Graph(figure=px.scatter(df, x="sepal_width", y="sepal_length")),
+        vm.Graph(figure=px.scatter(df, x="sepal_width", y="sepal_length"), title="Blah blah"),
+        vm.Table(figure=dash_data_table(df), title="My Table"),
+        vm.Graph(
+            figure=px.scatter(
+                df, x="sepal_width", y="sepal_length",
+            ),
+            title="My Graph",
+            header=HEADER
+        ),
+    ],
+)
 
-dashboard = vm.Dashboard(pages=[page])
+page_three = vm.Page(
+    title="Styling Header/Footer",
+    layout=vm.Layout(
+        grid=[[0, 1, 2]],
+    ),
+    components=[
+        vm.AgGrid(figure=dash_ag_grid(df), title="My AgGrid", header=HEADER,
+            footer=FOOTER),
+        vm.Table(figure=dash_data_table(df), title="My Table", header=HEADER,
+            footer=FOOTER),
+        vm.Graph(
+            figure=px.scatter(
+                df, x="sepal_width", y="sepal_length",
+            ),
+            title="My Graph",
+            header=HEADER,
+            footer=FOOTER
+        ),
+    ],
+)
+
+
+
+dashboard = vm.Dashboard(pages=[page, page_two, page_three])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run(debug=True)
