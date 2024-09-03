cfcbb05bca27328a1ce352347cc3345c8ae19eb7
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-09-03 15:50:28 +0200

    Add docs

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -3,106 +3,55 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models.types import capture
 from vizro.tables import dash_ag_grid, dash_data_table
 
-df = px.data.iris()
+iris = px.data.iris()
+gapminder_2007 = px.data.gapminder().query("year == 2007")
 
-HEADER = """
-Each point in the scatter plot represents one of the 150 iris flowers, with colors indicating their types. The Setosa
-type is easily identifiable by its short and wide sepals.
-
-However, there is still overlap between the Versicolor and Virginica types when considering only sepal width and length.
-"""
-
-FOOTER = """SOURCE: **Plotly Iris, 2024**"""
-
-
-@capture("graph")
-def scatter(data_frame, **kwargs):
-    """Scatter plot."""
-    fig = px.scatter(data_frame, **kwargs)
-    fig.update_layout(margin_b=24, legend_y=-0.2, legend_title="")
-    return fig
-
-
-page = vm.Page(
-    title="Fig Title",
-    layout=vm.Layout(
-        grid=[[0, 1, 2, 3]],
-    ),
-    components=[
-        vm.Graph(figure=scatter(df, x="sepal_width", y="sepal_length")),
-        vm.Graph(figure=scatter(df, x="sepal_width", y="sepal_length", title="Blah blah")),
-        vm.Table(figure=dash_data_table(df), title="My Table"),
-        vm.Graph(
-            figure=scatter(df, x="sepal_width", y="sepal_length", title="My Graph <br><span>This is a subtitle</span>")
-        ),
-    ],
-)
-page_two = vm.Page(
-    title="Graph Title",
-    layout=vm.Layout(
-        grid=[[0, 1, 2, 3]],
-    ),
-    components=[
-        vm.Graph(figure=scatter(df, x="sepal_width", y="sepal_length")),
-        vm.Graph(figure=scatter(df, x="sepal_width", y="sepal_length"), title="Blah blah"),
-        vm.Table(figure=dash_data_table(df), title="My Table"),
-        vm.Graph(
-            figure=scatter(df, x="sepal_width", y="sepal_length", color="species"),
-            title="My Graph",
-            header=HEADER,
-        ),
-    ],
-)
-
-page_three = vm.Page(
-    title="Styling Header/Footer",
-    layout=vm.Layout(
-        grid=[[0, 1, 2]],
-    ),
-    components=[
-        vm.AgGrid(figure=dash_ag_grid(df), title="My AgGrid", header=HEADER, footer=FOOTER),
-        vm.Table(figure=dash_data_table(df), title="My Table", header=HEADER, footer=FOOTER),
-        vm.Graph(
-            figure=scatter(df, x="sepal_width", y="sepal_length", color="species"),
-            title="My Graph",
-            header=HEADER,
-            footer=FOOTER,
-        ),
-    ],
-)
-
-page_four = vm.Page(
-    title="Styling Header/Footer - AgGrid",
-    components=[
-        vm.AgGrid(figure=dash_ag_grid(df), title="My AgGrid", header=HEADER, footer=FOOTER),
-    ],
-)
-
-page_five = vm.Page(
-    title="Styling Header/Footer - DataTable",
-    components=[
-        vm.Table(figure=dash_data_table(df), title="My AgGrid", header=HEADER, footer=FOOTER),
-    ],
-)
-
-
-page_six = vm.Page(
-    title="Styling Header/Footer - Graph",
+page_graph = vm.Page(
+    title="Formatted Graph",
     components=[
         vm.Graph(
-            figure=scatter(df, x="sepal_width", y="sepal_length", color="species"),
+            figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"),
             title="Relationships between Sepal Width and Sepal Length",
-            header=HEADER,
-            footer=FOOTER,
+            header="""
+                Each point in the scatter plot represents one of the 150 iris flowers,  with colors indicating their
+                types. The Setosa type is easily identifiable by its short and wide sepals.
+
+                However, there is still overlap between the Versicolor and Virginica types when considering only sepal
+                width and length.
+                """,
+            footer="""SOURCE: **Plotly iris data set, 2024**""",
         ),
     ],
 )
 
+page_grid = vm.Page(
+    title="Formatted AgGrid",
+    components=[
+        vm.AgGrid(
+            figure=dash_ag_grid(data_frame=gapminder_2007, dashGridOptions={"pagination": True}),
+            title="Gapminder Data Insights",
+            header="""#### An Interactive Exploration of Global Health, Wealth, and Population""",
+            footer="""SOURCE: **Plotly gapminder data set, 2024**""",
+        )
+    ],
+)
 
-dashboard = vm.Dashboard(pages=[page, page_two, page_three, page_four, page_five, page_six])
+page_table = vm.Page(
+    title="Formatted DataTable",
+    components=[
+        vm.Table(
+            figure=dash_data_table(data_frame=gapminder_2007),
+            title="Gapminder Data Insights",
+            header="""#### An Interactive Exploration of Global Health, Wealth, and Population""",
+            footer="""SOURCE: **Plotly gapminder data set, 2024**""",
+        )
+    ],
+)
+
+
+dashboard = vm.Dashboard(pages=[page_graph, page_grid, page_table])
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run(debug=True)
+    Vizro().build(dashboard).run(debug=False)
