0869ad97531d872c83dfd87e2bb11f47af28bd07
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-09-18 18:48:33 +0200

    Fix bug that prevented font overwrites to be passed through

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,36 +1,31 @@
-"""Dev app to try things out."""
+"""DDev app to try things out."""
 
-import pandas as pd
+import plotly.io as pio
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models.types import capture
-
-animals = pd.DataFrame(
-    {"animals": ["giraffes", "orangutans", "monkeys"], "value": [20, 14, 23], "color": ["Male", "Female", "Female"]}
-)
 
 iris = px.data.iris()
 
-
-@capture("graph")
-def bar(data_frame):
-    """LA LA LA LA."""
-    fig = px.bar(data_frame, x="sepal_length", y="sepal_width", color="species")
-    return fig
-
-
-@capture("graph")
-def px_bar(data_frame):
-    """LA LA LA LA."""
-    fig = px.bar(data_frame, x="animals", y="value", color="color")
-    return fig
+# This previously did not work, because we defined a font family for subcategories of the charts
+# e.g. xaxis_title_font_size, xaxis_tickfont_size, etc. However, if we only define the font family in our template
+# it will be passed on to the chart's subcomponents and this way, users only have to do the change below
+# if they want to change the font globally, instead of overwriting the font for every single subcomponent.
+pio.templates["vizro_dark"]["layout"]["font_family"] = "PlayfairDisplay, Inter, sans-serif, Arial, serif"
+pio.templates["vizro_light"]["layout"]["font_family"] = "PlayfairDisplay, Inter, sans-serif, Arial, serif"
 
 
 page = vm.Page(
-    title="My first page",
+    title="Page with different font",
     components=[
-        vm.Graph(figure=bar(iris)),
+        vm.Card(
+            text="""
+        # Lorem ipsum
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nunc nec libero ultricies ultricies.
+        """
+        ),
+        vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
     ],
 )
 
--- vizro-core/src/vizro/_themes/_templates/common_values.py
+++ vizro-core/src/vizro/_themes/_templates/common_values.py
@@ -13,15 +13,12 @@ def create_template_common():
         https://plotly.com/python/reference/layout/
 
     """
-    primary_font = "Inter, sans-serif, Arial"
-
     template_common = go.layout.Template()
 
     template_common.layout = go.Layout(
         # LAYOUT
-        font_family=primary_font,
+        font_family="Inter, sans-serif, Arial",
         font_size=14,
-        title_font_family=primary_font,
         title_font_size=20,
         title_xref="container",
         title_yref="container",
@@ -34,11 +31,9 @@ def create_template_common():
         title_pad_t=24,
         title_pad_b=0,
         showlegend=True,
-        legend_font_family=primary_font,
         legend_font_size=14,
         legend_orientation="h",
         legend_y=-0.20,
-        legend_title_font_family=primary_font,
         legend_title_font_size=14,
         legend_bgcolor="rgba(0,0,0,0)",
         margin_l=80,
@@ -54,17 +49,14 @@ def create_template_common():
         coloraxis_colorbar_ticks="outside",
         coloraxis_colorbar_tickwidth=1,
         coloraxis_colorbar_ticklen=8,
-        coloraxis_colorbar_tickfont_family=primary_font,
         coloraxis_colorbar_tickfont_size=14,
         coloraxis_colorbar_ticklabelposition="outside",
-        coloraxis_colorbar_title_font_family=primary_font,
         coloraxis_colorbar_title_font_size=14,
         bargroupgap=0.1,
         uniformtext_minsize=12,
         uniformtext_mode="hide",
         # X AXIS
         xaxis_visible=True,
-        xaxis_title_font_family=primary_font,
         xaxis_title_font_size=16,
         xaxis_title_standoff=8,
         xaxis_ticklabelposition="outside",
@@ -73,7 +65,6 @@ def create_template_common():
         xaxis_tickwidth=1,
         xaxis_showticklabels=True,
         xaxis_automargin=True,
-        xaxis_tickfont_family=primary_font,
         xaxis_tickfont_size=14,
         xaxis_showline=True,
         xaxis_layer="below traces",
@@ -81,7 +72,6 @@ def create_template_common():
         xaxis_zeroline=False,
         # Y AXIS
         yaxis_visible=True,
-        yaxis_title_font_family=primary_font,
         yaxis_title_font_size=16,
         yaxis_title_standoff=8,
         yaxis_ticklabelposition="outside",
@@ -90,7 +80,6 @@ def create_template_common():
         yaxis_tickwidth=1,
         yaxis_showticklabels=True,
         yaxis_automargin=True,
-        yaxis_tickfont_family=primary_font,
         yaxis_tickfont_size=14,
         yaxis_showline=False,
         yaxis_layer="below traces",
