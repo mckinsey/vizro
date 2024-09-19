2aa7e7432fe68b2006c1e8323e56f559c23a6f6a
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-09-19 11:28:56 +0200

    Tidy example with existing fonts

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -6,13 +6,8 @@ import vizro.plotly.express as px
 from vizro import Vizro
 
 iris = px.data.iris()
-
-# This previously did not work, because we defined a font family for subcategories of the charts
-# e.g. xaxis_title_font_size, xaxis_tickfont_size, etc. However, if we only define the font family in our template
-# it will be passed on to the chart's subcomponents and this way, users only have to do the change below
-# if they want to change the font globally, instead of overwriting the font for every single subcomponent.
-pio.templates["vizro_dark"]["layout"]["font_family"] = "PlayfairDisplay, Inter, sans-serif, Arial, serif"
-pio.templates["vizro_light"]["layout"]["font_family"] = "PlayfairDisplay, Inter, sans-serif, Arial, serif"
+pio.templates["vizro_dark"]["layout"]["font_family"] = "Times New Roman, Inter, sans-serif, Arial, serif"
+pio.templates["vizro_light"]["layout"]["font_family"] = "Times New Roman, Inter, sans-serif, Arial, serif"
 
 
 page = vm.Page(
