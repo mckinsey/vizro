e8b6c2d234cacf627bcb21d8ad34b615ac3cd934
petar-qb <petar_pejovic@external.mckinsey.com>
2024-09-19 14:15:44 +0200

    Other files

---
+++ vizro-core/examples/scratch_dev/app_ag_grid_vs_filter.py
@@ -0,0 +1,170 @@
+# """Dev app to try things out."""
+#
+# import plotly.io as pio
+# import vizro.models as vm
+# import vizro.plotly.express as px
+# from vizro import Vizro
+# from vizro.tables import dash_ag_grid
+#
+# iris = px.data.iris()
+#
+#
+# page = vm.Page(
+#     title="Title",
+#     components=[
+#         vm.AgGrid(
+#             id="ag-grid-container",
+#             figure=dash_ag_grid(
+#                 id="ag-grid",
+#                 data_frame=iris,
+#                 persistence=True,
+#                 persistence_type="session",
+#                 persisted_props=["filterModel"],
+#             )
+#         ),
+#     ],
+#     controls=[
+#         vm.Filter(
+#             id="filter_container",
+#             column="sepal_length",
+#             selector=vm.RangeSlider(
+#                 id="filter"
+#             )
+#         ),
+#     ]
+# )
+#
+# dashboard = vm.Dashboard(pages=[page])
+#
+# if __name__ == "__main__":
+#     Vizro().build(dashboard).run()
+
+
+import random
+import dash
+import datetime
+import time
+import plotly.express as px
+import plotly.graph_objs as go
+from dash_ag_grid import AgGrid
+
+from dash import Dash, html, dcc, Output, callback, clientside_callback, Input, State, set_props
+
+
+# like dynamic data
+def slow_load():
+    print("running slow_load")
+    time.sleep(0.1)
+    return px.data.iris()#.sample(6)
+
+
+common = [
+    html.H1(id="dashboard_title", children="Dashboard"),
+    html.Div(dcc.Link("Homepage", href="/")),
+    html.Div(dcc.Link("Another page", href="/another-page")),
+]
+
+
+def make_page(content):
+    return html.Div(
+        [
+            *common,
+            html.P(datetime.datetime.now()),
+            *content,
+        ]
+    )
+
+
+# like build
+def homepage(**kwargs):
+    return make_page([html.H2("Homepage")])
+
+
+# like page build
+def another_page(**kwargs):
+    return make_page(
+        [
+            dcc.Store(id="on_page_load_trigger_another_page", data="123"),
+            html.H2("Another page"),
+
+            # Filter
+            html.Div(
+                id="filter_container",
+                children=[html.Div(id="filter")],
+            ),
+
+            # Grid
+            html.Div(
+                id="ag_grid_container",
+                children=[html.Div(id="ag_grid")],
+            ),
+        ]
+    )
+
+
+def get_data(species):
+    df = slow_load()
+    return df[df["species"].isin(species)]
+
+
+# This mechanism is not actually necessary in this example since can just let OPL run without this trigger removing
+# prevent_initial_call=True from it.
+clientside_callback(
+    """
+    function trigger_to_global_store(data) {
+        return data;
+    }
+    """,
+    Output("global_on_page_load_another_page_action_trigger", "data"),
+    Input("on_page_load_trigger_another_page", "data"),
+    prevent_initial_call=True,  # doesn't do anything - callback still runs
+)
+
+
+@callback(
+    Output("ag_grid_container", "children"),
+    Output("filter_container", "children"),
+    Input("global_on_page_load_another_page_action_trigger", "data"),
+    State("filter", "value"),
+    prevent_initial_call=True
+)
+# TODO - ANSWER -> It can't propagate persisted value to the on_page_load input
+#  as it's an empty html.Div object created by page.build()...
+def on_page_load(trigger, persisted_filter_value):
+    print("running on_page_load")
+    df = slow_load()
+
+    categorical_filter_options = sorted(df["species"].unique().tolist())
+    # categorical_filter_value = [value for value in persisted_filter_value if value in categorical_filter_options]
+    categorical_filter_value = categorical_filter_options
+    categorical_filter_obj = dcc.Dropdown(
+        id="filter",
+        options=categorical_filter_options,
+        value=categorical_filter_value,
+        persistence=True,
+        persistence_type="session",
+        multi=True,
+    )
+
+    # set_props(component_id="categorical_filter_container", props={"children": categorical_filter_obj})
+    # df = df[df["species"].isin(categorical_filter_value)]
+
+    dash_ag_grid_obj = AgGrid(
+        id="ag_grid",
+        rowData=df.to_dict("records"),
+        persistence=True,
+        persistence_type="session"
+    )
+
+    print("")
+    return dash_ag_grid_obj, categorical_filter_obj
+
+
+app = Dash(use_pages=True, pages_folder="", suppress_callback_exceptions=True)
+dash.register_page("/", layout=homepage)
+dash.register_page("another_page", layout=another_page)
+
+app.layout = html.Div([dcc.Store("global_on_page_load_another_page_action_trigger"), dash.page_container])
+
+if __name__ == "__main__":
+    app.run(dev_tools_hot_reload=False)
