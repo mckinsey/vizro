143eee1c2f8adb29508163482aef23584abc83ea
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2024-09-24 10:21:39 +0200

    [Docs] Add CSS customization guide (#716)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,45 +1,34 @@
 """Dev app to try things out."""
 
+import plotly.io as pio
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models.types import capture
 
-df = px.data.iris()
+pio.templates["vizro_dark"]["layout"]["paper_bgcolor"] = "rgba(0, 0, 0, 0)"
+pio.templates["vizro_light"]["layout"]["paper_bgcolor"] = "rgba(0, 0, 0, 0)"
+pio.templates["vizro_dark"]["layout"]["plot_bgcolor"] = "rgba(0, 0, 0, 0)"
+pio.templates["vizro_light"]["layout"]["plot_bgcolor"] = "rgba(0, 0, 0, 0)"
 
 
-@capture("action")
-def my_custom_action(show_species: bool, points_data: dict):
-    """Custom action."""
-    clicked_point = points_data["points"][0]
-    x, y = clicked_point["x"], clicked_point["y"]
-    text = f"Clicked point has sepal length {x}, petal width {y}"
-
-    if show_species:
-        species = clicked_point["customdata"][0]
-        text += f" and species {species}"
-    return text
-
+iris = px.data.iris()
 
 page = vm.Page(
-    title="Action with clickData as input",
+    title="Page with subsections",
+    layout=vm.Layout(grid=[[0, 1]]),
     components=[
-        vm.Graph(
-            id="scatter_chart",
-            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", custom_data=["species"]),
-            actions=[
-                vm.Action(
-                    function=my_custom_action(show_species=True),
-                    inputs=["scatter_chart.clickData"],
-                    outputs=["my_card.children"],
-                ),
-            ],
+        vm.Container(
+            title="Container I",
+            components=[vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"))],
         ),
-        vm.Card(id="my_card", text="Click on a point on the above graph."),
+        vm.Container(
+            title="Container II",
+            components=[vm.Graph(figure=px.box(iris, x="species", y="sepal_length", color="species"))],
+        ),
     ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run()
+    Vizro().build(dashboard).run(debug=False)
