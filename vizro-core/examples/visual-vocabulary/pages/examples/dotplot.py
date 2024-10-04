9f239c44776b716e4a2ecf8b0fec1b7ec46ab96c
njmcgrat <143455814+njmcgrat@users.noreply.github.com>
2024-10-04 21:09:59 +0000

    Add dot plot to visual vocabulary (#708)

--- vizro-core/examples/visual-vocabulary/pages/examples/dotplot.py
+++ vizro-core/examples/visual-vocabulary/pages/examples/dotplot.py
@@ -6,7 +6,7 @@ iris = px.data.iris()
 
 page = vm.Page(
     title="Dot Plot",
-    components=[vm.Graph(px.scatter(iris, x="sepal_length"))],
+    components=[vm.Graph(px.scatter(iris, x="sepal_length", y="species"))],
 )
 
 dashboard = vm.Dashboard(pages=[page])
