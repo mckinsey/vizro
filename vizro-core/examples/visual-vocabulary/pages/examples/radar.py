a654f9728aa9450910db7eeab6b1752d564bac85
Rosheen C. <rc678@users.noreply.github.com>
2024-10-09 15:19:26 -0400

    Update vizro-core/examples/visual-vocabulary/pages/examples/radar.py
    Co-authored-by: Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>

--- vizro-core/examples/visual-vocabulary/pages/examples/radar.py
+++ vizro-core/examples/visual-vocabulary/pages/examples/radar.py
@@ -1,13 +1,30 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.models.types import capture
 
 wind = px.data.wind()
+
+
+@capture("graph")
+def radar(data_frame, **kwargs):
+    """
+    Add docstrings here (see butterfly example)
+    
+    """
+    fig = px.line_polar(data_frame, **kwargs)
+    fig.update_traces(fill="toself")
+    return fig
+
 
 page = vm.Page(
     title="Radar chart",
     components=[
-        vm.Graph(figure=px.line_polar(wind, r="frequency", theta="direction", color="strength", line_close=True))
+        vm.Graph(
+            figure=radar_chart(
+                wind.query("strength =='1-2'"), r="frequency", theta="direction", color="strength", line_close=True
+            )
+        )
     ],
 )
 
