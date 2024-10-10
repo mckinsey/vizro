6ff7bd1dea5321442c6cdde101f8d09012eea45d
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2024-10-10 12:52:08 +0000

    Add simplified example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,24 +1,49 @@
 """Dev app to try things out."""
 
+import pandas as pd
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro._themes._color_values import COLORS
 
-df = px.data.iris()
+pastry = pd.DataFrame(
+    {
+        "pastry": [
+            "Scones",
+            "Bagels",
+            "Muffins",
+            "Cakes",
+            "Donuts",
+            "Cookies",
+            "Croissants",
+            "Eclairs",
+            "Brownies",
+            "Tarts",
+            "Macarons",
+            "Pies",
+        ],
+        "Profit Ratio": [-0.10, -0.15, -0.05, 0.10, 0.05, 0.20, 0.15, -0.08, 0.08, -0.12, 0.02, -0.07],
+    }
+)
+
 
 page = vm.Page(
-    title="Page with asfsadfsadf",
-    layout=vm.Layout(grid=[[0, 1]]),
+    title="Diverging bar",
     components=[
-        vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
-        vm.Graph(id="hist_chart", figure=px.histogram(df, x="sepal_width", color="species")),
-    ],
-    controls=[
-        vm.Filter(column="species", selector=vm.Dropdown(value=["ALL"])),
+        vm.Graph(
+            figure=px.bar(
+                pastry.sort_values("Profit Ratio"),
+                orientation="h",
+                x="Profit Ratio",
+                y="pastry",
+                color="Profit Ratio",
+                color_continuous_scale=COLORS["DIVERGING_RED_CYAN"],
+            ),
+        ),
     ],
 )
 
-dashboard = vm.Dashboard(pages=[page], navigation=vm.Navigation(nav_selector=vm.NavBar()))
+dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
-    Vizro(serve_locally=False).build(dashboard).run()
+    Vizro().build(dashboard).run()
