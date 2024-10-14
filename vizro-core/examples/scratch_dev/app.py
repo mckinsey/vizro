6a189d5d74dcc37446ad2a7a4929dd9e0da2b38a
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-10-14 16:35:39 +0200

    Revert changes in scratch app

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,67 +1,49 @@
 """Dev app to try things out."""
 
+import pandas as pd
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models.types import capture
+from vizro._themes._color_values import COLORS
 
-tips = px.data.tips()
-
-
-@capture("graph")
-def dumbbell(data_frame, x, y, color):
-    """Creates a waterfall chart using Plotly's `go.Waterfall`.
-
-    A Waterfall chart visually breaks down the cumulative effect of sequential positive and negative values,
-    showing how each value contributes to the total.
-
-    Args:
-        data_frame (pd.DataFrame): The data source for the chart.
-        x (str): Column name in `data_frame` for x-axis values.
-        y (str): Column name in `data_frame` for y-axis values.
-        measure (List[str]): List specifying the type of each bar, can be "relative", "total", or "absolute".
-
-    Returns:
-        go.Figure: A Plotly Figure object representing the Waterfall chart.
-
-    For additional parameters and customization options, see the Plotly documentation:
-    https://plotly.com/python/reference/waterfall/
-
-    """
-    # Add two dots to plot
-    fig = px.scatter(data_frame, y=y, x=x, color=color)
-
-    # Add lines between dots
-    for i in data_frame[y].unique():
-        df_sub = data_frame[data_frame[y] == i]
-        fig.add_shape(
-            type="line",
-            layer="below",
-            y0=df_sub[y].values[0],
-            x0=df_sub[x].values[0],
-            y1=df_sub[y].values[1],
-            x1=df_sub[x].values[1],
-            line_color="grey",
-        )
-
-    # Increase size of dots
-    fig.update_traces(marker_size=12)
-    return fig
+pastry = pd.DataFrame(
+    {
+        "pastry": [
+            "Scones",
+            "Bagels",
+            "Muffins",
+            "Cakes",
+            "Donuts",
+            "Cookies",
+            "Croissants",
+            "Eclairs",
+            "Brownies",
+            "Tarts",
+            "Macarons",
+            "Pies",
+        ],
+        "Profit Ratio": [-0.10, -0.15, -0.05, 0.10, 0.05, 0.20, 0.15, -0.08, 0.08, -0.12, 0.02, -0.07],
+    }
+)
 
 
 page = vm.Page(
-    title="Dumbbell",
+    title="Diverging bar",
     components=[
         vm.Graph(
-            figure=dumbbell(
-                tips.groupby(["day", "sex"]).agg({"tip": "sum"}).reset_index(), y="day", x="tip", color="sex"
+            figure=px.bar(
+                pastry.sort_values("Profit Ratio"),
+                orientation="h",
+                x="Profit Ratio",
+                y="pastry",
+                color="Profit Ratio",
+                color_continuous_scale=COLORS["DIVERGING_RED_CYAN"],
             ),
         ),
     ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
-Vizro().build(dashboard).run()
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
