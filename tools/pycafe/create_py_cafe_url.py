8c6e898260d638205a549a3d3305fa5cfe621275
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-10-16 10:51:40 +0200

    append

--- tools/pycafe/create_py_cafe_url.py
+++ tools/pycafe/create_py_cafe_url.py
@@ -34,7 +34,7 @@ def generate_link(directory):
             relative_path = os.path.relpath(file_path, directory)
             file_url = f"{base_url}{relative_path.replace(os.sep, '/')}"
             json_object["files"].append({"name": relative_path, "url": file_url})
-    print(json_object)
+            print(json_object)
     json_text = json.dumps(json_object)
     compressed_json_text = gzip.compress(json_text.encode("utf8"))
     base64_text = base64.b64encode(compressed_json_text).decode("utf8")
