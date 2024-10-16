8be0bef35e5164407e9bc5556a658d1e6680e40f
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-10-16 11:53:27 +0200

    asdf

--- tools/pycafe/create_py_cafe_url.py
+++ tools/pycafe/create_py_cafe_url.py
@@ -22,7 +22,25 @@ def generate_link(directory):
         # print("=====")
 
     json_object = {
-        "code": str(app_content),
+        "code": """import vizro.plotly.express as px
+from vizro import Vizro
+import vizro.models as vm
+
+df = px.data.iris()
+
+page = vm.Page(
+    title="My first dashboard",
+    components=[
+        vm.Graph(id="scatter_chart", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
+        vm.Graph(id="hist_chart", figure=px.histogram(df, x="sepal_width", color="species")),
+    ],
+    controls=[
+        vm.Filter(column="species", selector=vm.Dropdown(value=["ALL"])),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[page])
+Vizro().build(dashboard).run()""",#str(app_content),
         "requirements": "https://py.cafe/gh/artifact/mckinsey/vizro/2054307112/vizro-0.1.25.dev0-py3-none-any.whl",
         "files": [],
     }
