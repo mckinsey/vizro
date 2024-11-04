b37f38f2c2994ca5823b7241bb96759db684dde6
Antony Milne <antony.milne@quantumblack.com>
2024-11-04 16:18:02 +0000

    Refactor set methods and add in __call__

--- vizro-core/src/vizro/models/_controls/filter.py
+++ vizro-core/src/vizro/models/_controls/filter.py
@@ -87,7 +87,6 @@ class Filter(VizroBaseModel):
     )
     selector: SelectorType = None
     _column_type: Literal["numerical", "categorical", "temporal"] = PrivateAttr()
-    _data_frames = PrivateAttr()
 
     @validator("targets", each_item=True)
     def check_target_present(cls, target):
@@ -97,96 +96,41 @@ class Filter(VizroBaseModel):
 
     @_log_call
     def pre_build(self):
-        self._set_targets()
-        self._set_column_type()
-        self._set_selector()
-        self._validate_disallowed_selector()
-        self._set_numerical_and_temporal_selectors_values()
-        self._set_categorical_selectors_options()
-        self._set_actions()
-
-    @_log_call
-    def build(self):
-        return self.selector.build()
-
-    def _set_targets(self):
-        # TODO: consider moving to data_manager, depending on Petar's work
-        # TODO: write tests
-        potential_targets = self.targets or model_manager._get_page_model_ids_with_figure(
-            page_id=model_manager._get_model_page_id(model_id=ModelID(str(self.id)))
-        )
-
-        potential_target_to_data_source_name = {
-            target: model_manager[target]["data_frame"] for target in potential_targets
-        }
-        # Using set() here ensures we only load each data source once rather than repeating the operation for each
-        # target.
-        data_source_name_to_data = {
-            data_source_name: data_manager[data_source_name].load()
-            for data_source_name in set(potential_target_to_data_source_name.values())
-        }
-        target_to_series = dict()
-
-        for target, data_source_name in potential_target_to_data_source_name.items():
-            data_frame = data_source_name_to_data[data_source_name]
-
-            if self.column in data_frame.columns:
-                target_to_series[target] = data_frame
-            elif self.targets:
-                # targets were manually specified so it's not ok the column isn't there. If targets were not specified
-                # then it's fine, we just skip this target, and error is not raised.
-                raise ValueError(f"Selected column {self.column} not found in dataframe for {target}.")
-
-        if not target_to_series:
-            raise ValueError(f"Selected column {self.column} not found in any dataframe on this page.")
-
-        self.targets = list(target_to_series)
-        # COMMEnt. will have repeats
-        self._targeted_data = pd.DataFrame.from_dict(target_to_series)
-
-    def _set_column_type(self):
-        # TODO: check
-        is_numerical = self._targeted_data.apply(is_numeric_dtype)
-        is_temporal = self._targeted_data.apply(is_datetime64_any_dtype)
-        is_categorical = ~is_numerical & ~is_temporal
-
-        if is_numerical.all():
-            self._column_type = "numerical"
-        elif is_temporal.all():
-            self._column_type = "temporal"
-        elif is_categorical.all():
-            self._column_type = "categorical"
+        if self.targets:
+            targeted_data = self._validate_targeted_data(targets=self.targets)
         else:
-            raise ValueError(
-                f"Inconsistent types detected in the shared data column {self.column}. This column must "
-                "have the same type for all targets."
+            # If targets aren't explicitly provided then try to target all figures on the page. In this case we don't
+            # want to raise an error if the column is not found in a figure's data_frame, it will just be ignored.
+            # Possibly in future this will change (which would be breaking change).
+            targeted_data = self._validate_targeted_data(
+                targets=model_manager._get_page_model_ids_with_figure(
+                    page_id=model_manager._get_model_page_id(model_id=ModelID(str(self.id)))
+                ),
+                raise_column_not_found_error=False,
             )
+            if targeted_data.empty:
+                raise ValueError(f"Selected column {self.column} not found in any dataframe on this page.")
+            self.targets = targeted_data.columns
 
-    def _set_selector(self):
+        # Set default selector according to column type.
+        self._column_type = self._validate_column_type(targeted_data)
         self.selector = self.selector or SELECTORS[self._column_type][0]()
         self.selector.title = self.selector.title or self.column.title()
 
-    def _validate_disallowed_selector(self):
         if isinstance(self.selector, DISALLOWED_SELECTORS.get(self._column_type, ())):
             raise ValueError(
-                f"Chosen selector {self.selector.type} is not compatible "
-                f"with {self._column_type} column '{self.column}'. "
+                f"Chosen selector {type(self.selector)} is not compatible with ({self._column_type}) column '"
+                f"{self.column}'."
             )
 
-    def _set_numerical_and_temporal_selectors_values(self):
-        # If the selector is a numerical or temporal selector, and the min and max values are not set, then set them
-        # N.B. All custom selectors inherit from numerical or temporal selector should also pass this check
-        if isinstance(self.selector, SELECTORS["numerical"] + SELECTORS["temporal"]):
-            self.selector.min = self.selector.min or self._targeted_data.to_numpy().min()
-            self.selector.max = self.selector.max or self._targeted_data.to_numpy().max()
+        # Set appropriate properties for the selector.
+        if self._column_type == "categorical":
+            self.selector.options = self.selector.options or self._get_options(targeted_data)
+        elif self._column_type in ("temporal", "numerical"):
+            _min, _max = self._get_min_max(targeted_data)
+            self.selector.min = self.selector.min or _min
+            self.selector.max = self.selector.max or _max
 
-    def _set_categorical_selectors_options(self):
-        # If the selector is a categorical selector, and the options are not set, then set them
-        # N.B. All custom selectors inherit from categorical selector should also pass this check
-        if isinstance(self.selector, SELECTORS["categorical"]):
-            self.selector.options = self.selector.options or sorted(np.unique(self._targeted_data.to_numpy()))
-
-    def _set_actions(self):
         if not self.selector.actions:
             if isinstance(self.selector, RangeSlider) or (
                 isinstance(self.selector, DatePicker) and self.selector.range
@@ -201,3 +145,91 @@ class Filter(VizroBaseModel):
                     function=_filter(filter_column=self.column, targets=self.targets, filter_function=filter_function),
                 )
             ]
+
+    def __call__(self, **kwargs):
+        # Only relevant for a dynamic filter.
+        # TODO: this will need to pass parametrised data_frame arguments through to _validate_targeted_data.
+        # Although targets are fixed at build time, the validation logic is repeated during runtime, so if a column
+        # is missing then it will raise an error. We could change this if we wanted.
+        targeted_data = self._validate_targeted_data(targets=self.targets)
+
+        if (column_type := self._validate_column_type(targeted_data)) != self._column_type:
+            raise ValueError(
+                f"{self.column} has changed type from {self._column_type} to {column_type}. A filtered column cannot "
+                "change type while the dashboard is running."
+            )
+
+        if self._column_type == "categorical":
+            options = self._get_options(targeted_data)
+            return options
+            # TODO: when implement dynamic, will need to do something with this e.g. pass to selector.__call__.
+        elif self._column_type in ("temporal", "numerical"):
+            _min, _max = self._get_min_max(targeted_data)
+            return _min, _max
+            # TODO: when implement dynamic, will need to do something with this e.g. pass to selector.__call__.
+
+    @_log_call
+    def build(self):
+        return self.selector.build()
+
+    def _validate_targeted_data(self, targets: list[ModelID], raise_column_not_found_error=True) -> pd.DataFrame:
+        # TODO: consider moving some of this logic to data_manager when implement dynamic filter. Make sure
+        #  get_modified_figures is as efficient as code here.
+
+        # When loading data_frame there are possible keys:
+        #  1. target. In worst case scenario this is needed but can lead to unnecessary repeated data loading.
+        #  2. data_source_name. No repeated data loading but won't work when applying data_frame parameters at runtime.
+        #  3. target + data_frame parameters keyword-argument pairs. This is the correct key to use at runtime.
+        # For now we follow scheme 2 for data loading (due to set() below) and 1 for the returned targeted_data
+        # pd.DataFrame, i.e. a separate column for each target even if some data is repeated.
+        # TODO: when this works with data_frame parameters load() will need to take arguments and the structures here
+        #  might change a bit.
+        target_to_data_source_name = {target: model_manager[target]["data_frame"] for target in targets}
+        data_source_name_to_data = {
+            data_source_name: data_manager[data_source_name].load()
+            for data_source_name in set(target_to_data_source_name.values())
+        }
+        target_to_series = dict()
+
+        for target, data_source_name in target_to_data_source_name.items():
+            data_frame = data_source_name_to_data[data_source_name]
+
+            if self.column in data_frame.columns:
+                # reset_index so that when we make a DataFrame out of all these pd.Series pandas doesn't try to align
+                # the columns by index.
+                target_to_series[target] = data_frame[self.column].reset_index(drop=True)
+            elif raise_column_not_found_error:
+                raise ValueError(f"Selected column {self.column} not found in dataframe for {target}.")
+
+        return pd.DataFrame(target_to_series)
+
+    def _validate_column_type(self, targeted_data: pd.DataFrame) -> Literal["numerical", "categorical", "temporal"]:
+        is_numerical = targeted_data.apply(is_numeric_dtype)
+        is_temporal = targeted_data.apply(is_datetime64_any_dtype)
+        is_categorical = ~is_numerical & ~is_temporal
+
+        if is_numerical.all():
+            return "numerical"
+        elif is_temporal.all():
+            return "temporal"
+        elif is_categorical.all():
+            return "categorical"
+        else:
+            raise ValueError(
+                f"Inconsistent types detected in the shared data column {self.column}. This column must "
+                "have the same type for all targets."
+            )
+
+    # TODO: write tests. Include N/A
+    # TODO: block all update of models during runtime
+    def _get_min_max(self, targeted_data: pd.DataFrame) -> tuple(float, float):
+        # Use item() to convert to convert scalar from numpy to Python type. This isn't needed during pre_build because
+        # pydantic will coerce the type, but it is necessary in __call__ where we don't update model field values
+        # and instead just pass straight to the Dash component.
+        return targeted_data.min(axis=None).item(), targeted_data.max(axis=None).item()
+
+    def _get_options(self, targeted_data: pd.DataFrame) -> list:
+        # Use tolist() to convert to convert scalar from numpy to Python type. This isn't needed during pre_build
+        # because pydantic will coerce the type, but it is necessary in __call__ where we don't update model field values
+        # and instead just pass straight to the Dash component.
+        return np.unique(targeted_data.stack().dropna()).tolist()
