f2ab354f9d241c97539108f3326ef0d309ac160b
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-11-04 23:05:28 +0100

    Mock greenlet

--- tools/pycafe/create_pycafe_links.py
+++ tools/pycafe/create_pycafe_links.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             "black",
             "jupyter",
             "openpyxl",
-            "greenlet",  # mock
+            "greenlet # mock",
             "tiktoken @ https://py.cafe/files/maartenbreddels/tiktoken-demo/tiktoken-0.7.0-cp312-cp312-pyodide_2024_0_wasm32.whl",
             "https://py.cafe/files/maartenbreddels/jiter-demo/jiter-0.6.1-cp312-cp312-pyodide_2024_0_wasm32.whl",
         ],
