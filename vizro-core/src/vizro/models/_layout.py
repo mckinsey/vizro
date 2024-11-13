ffef6dc2872b0271b4ca6c25547b8589fa35e910
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-11-13 16:12:10 +0100

    Refactor layout model to use field_validator and update regex to pattern for Pydantic v2 compatibility

--- vizro-core/src/vizro/models/_layout.py
+++ vizro-core/src/vizro/models/_layout.py
@@ -3,13 +3,12 @@ from typing import NamedTuple, Optional
 import numpy as np
 from dash import html
 from numpy import ma
-from pydantic import field_validator
+from pydantic import Field, PrivateAttr, field_validator
 
-try:
-    from pydantic.v1 import Field, PrivateAttr, validator
-except ImportError:  # pragma: no cov
-    from pydantic import Field, PrivateAttr
-
+# try:
+#     from pydantic.v1 import Field, PrivateAttr, validator
+# except ImportError:  # pragma: no cov
+#     from pydantic import Field, PrivateAttr
 from vizro._constants import EMPTY_SPACE_CONST
 from vizro.models import VizroBaseModel
 from vizro.models._models_utils import _log_call
@@ -158,11 +157,13 @@ class Layout(VizroBaseModel):
     """
 
     grid: list[list[int]] = Field(..., description="Grid specification to arrange components on screen.")
-    row_gap: str = Field(GAP_DEFAULT, description="Gap between rows in px. Defaults to 12px.", regex="[0-9]+px")
-    col_gap: str = Field(GAP_DEFAULT, description="Gap between columns in px. Defaults to 12px.", regex="[0-9]+px")
-    row_min_height: str = Field(MIN_DEFAULT, description="Minimum row height in px. Defaults to 0px.", regex="[0-9]+px")
+    row_gap: str = Field(GAP_DEFAULT, description="Gap between rows in px. Defaults to 12px.", pattern="[0-9]+px")
+    col_gap: str = Field(GAP_DEFAULT, description="Gap between columns in px. Defaults to 12px.", pattern="[0-9]+px")
+    row_min_height: str = Field(
+        MIN_DEFAULT, description="Minimum row height in px. Defaults to 0px.", pattern="[0-9]+px"
+    )
     col_min_width: str = Field(
-        MIN_DEFAULT, description="Minimum column width in px. Defaults to 0px.", regex="[0-9]+px"
+        MIN_DEFAULT, description="Minimum column width in px. Defaults to 0px.", pattern="[0-9]+px"
     )
     _component_grid_lines: Optional[list[ColRowGridLines]] = PrivateAttr()
 
