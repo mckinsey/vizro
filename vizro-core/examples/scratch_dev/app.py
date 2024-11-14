f28ca8c6dba2425f52f60033d5f20d9dc39febca
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-11-14 12:32:36 +0100

    Add original example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,45 +1,59 @@
 """Dev app to try things out."""
 
 import pandas as pd
+import plotly.graph_objects as go
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro._themes._color_values import COLORS
+from vizro.models.types import capture
 
-pastry = pd.DataFrame(
-    {
-        "pastry": [
-            "Scones",
-            "Bagels",
-            "Muffins",
-            "Cakes",
-            "Donuts",
-            "Cookies",
-            "Croissants",
-            "Eclairs",
-            "Brownies",
-            "Tarts",
-            "Macarons",
-            "Pies",
-        ],
-        "Profit Ratio": [-0.10, -0.15, -0.05, 0.10, 0.05, 0.20, 0.15, -0.08, 0.08, -0.12, 0.02, -0.07],
-    }
-)
+
+@capture("graph")
+def lollipop(data_frame: pd.DataFrame, x: str, y: str):
+    """Creates a lollipop chart using Plotly.
+
+    This function generates a scatter chart and then draws lines extending from each point to the x-axis.
+
+    Args:
+        data_frame (pd.DataFrame): The data source for the chart.
+        x (str): The column name to be used for the x-axis.
+        y (str): The column name to be used for the y-axis.
+
+    Returns:
+        go.Figure: : A Plotly Figure object representing the lollipop chart.
+    """
+    fig = go.Figure()
+
+    # Draw points
+    fig.add_trace(
+        go.Scatter(
+            x=data_frame[x],
+            y=data_frame[y],
+            mode="markers",
+            marker=dict(color="#00b4ff", size=12),
+        )
+    )
+
+    for i in range(len(data_frame)):
+        fig.add_trace(
+            go.Scatter(
+                x=[0, data_frame[x].iloc[i]],
+                y=[data_frame[y].iloc[i], data_frame[y].iloc[i]],
+                mode="lines",
+                line=dict(color="#00b4ff", width=3),
+            )
+        )
+    fig.update_layout(showlegend=False)
+    return fig
+
+
+gapminder = px.data.gapminder()
 
 
 page = vm.Page(
-    title="Charts UI",
+    title="Lollipop",
     components=[
-        vm.Graph(
-            figure=px.bar(
-                pastry.sort_values("Profit Ratio"),
-                orientation="h",
-                x="Profit Ratio",
-                y="pastry",
-                color="Profit Ratio",
-                color_continuous_scale=COLORS["DIVERGING_RED_CYAN"],
-            ),
-        ),
+        vm.Graph(figure=lollipop(gapminder.query("year == 2007 and gdpPercap > 36000"), y="country", x="gdpPercap"))
     ],
 )
 
