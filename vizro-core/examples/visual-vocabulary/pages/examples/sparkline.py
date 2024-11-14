f479fd12d6dd3df61475b956f7de98614ca2e379
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-11-14 13:25:34 +0100

    Add sparkline

---
+++ vizro-core/examples/visual-vocabulary/pages/examples/sparkline.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from vizro.models.types import capture
+
+stocks = px.data.stocks()
+stocks_melt = stocks.melt(id_vars='date', value_vars=['GOOG', 'AMZN', "AAPL"], var_name='stock', value_name='value')
+
+@capture("graph")
+def sparkline(data_frame, **kwargs):
+    fig = px.line(data_frame, **kwargs)
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(visible=False)
+    fig.update_layout(showlegend=False)
+    return fig
+
+fig = sparkline(stocks_melt, x="date", y='value', facet_row="stock", color="stock")
