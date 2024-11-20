c46383c9ede65ee0aae8a96453632b38e8c665e5
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2024-11-20 14:51:07 +0100

    [Tidy] Remove custom CSS for tooltip (#885)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,15 +1,47 @@
 """Dev app to try things out."""
 
+import pandas as pd
+import vizro.models as vm
+import vizro.plotly.express as px
+from vizro import Vizro
+from vizro._themes._color_values import COLORS
+import dash_bootstrap_components as dbc
+from dash import html
+
+from typing import Literal
+
+from dash import html
+
 import vizro.models as vm
 from vizro import Vizro
 
 
+class Tooltip(vm.VizroBaseModel):
+    type: Literal["tooltip"] = "tooltip"
+    text: str = "This is a tooltip"
+
+    def build(self):
+        return html.Div(
+            [
+                dbc.Button("Button", id=f"{self.id}-tooltip-target"),
+                dbc.Tooltip(
+                    self.text,
+                    target=f"{self.id}-tooltip-target",
+                    is_open=True,
+                ),
+            ]
+        )
+
+
+vm.Page.add_type("components", Tooltip)
+
 page = vm.Page(
-    title="Button Styling",
-    layout=vm.Layout(grid=[[0, 1]]),
+    title="Custom Component",
     components=[
-        vm.Button(),
-        vm.Button(text="Take me home", href="/"),
+        Tooltip(text="Tooltip label"),
+        Tooltip(
+            text="Max. width of tooltip should not exceed 180px - this instance should be used for multiple lines of text for increased legibility."
+        ),
     ],
 )
 
