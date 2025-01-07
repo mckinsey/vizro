1a4bd66b1f23562dbb0e7c6e6745e09118e8af25
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-01-07 09:33:38 +0100

    Check screenshots

--- vizro-core/src/vizro/_themes/_templates/template_dashboard_overrides.py
+++ vizro-core/src/vizro/_themes/_templates/template_dashboard_overrides.py
@@ -3,5 +3,5 @@
 import plotly.graph_objs as go
 
 dashboard_overrides = go.layout.Template(
-    layout={"title": {"pad_l": 0, "pad_r": 0}, "margin_l": 24, "margin_t": 24, "margin_b": 16}
+    layout={"title": {"pad_l": 0, "pad_r": 0}, "margin_l": 36, "margin_t": 40, "margin_b": 24}
 )
