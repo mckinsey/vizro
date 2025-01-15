6633916b089c614c17ee15fcdcb08560dc156b48
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-01-15 14:14:05 +0100

    [Bug] Fix appearance of spinner buttons in number input (#954)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,68 +1,43 @@
 """Dev app to try things out."""
 
-import pandas as pd
+import dash_bootstrap_components as dbc
+
 import vizro.models as vm
 from vizro import Vizro
+import vizro.plotly.express as px
 
-# For more information, refer to the API reference for kpi_card and kpi_card_reference
-from vizro.figures import kpi_card, kpi_card_reference
 
-df_kpi = pd.DataFrame({"Actual": [100, 200, 700], "Reference": [100, 300, 500], "Category": ["A", "B", "C"]})
+from typing import Literal
 
-example_cards = [
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with value"),
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with aggregation", agg_func="median"),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with formatting",
-        value_format="${value:.2f}",
-    ),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with icon",
-        icon="shopping_cart",
-    ),
-]
+gapminder = px.data.gapminder()
 
-example_reference_cards = [
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference (pos)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        agg_func="median",
-        title="KPI reference (neg)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference with formatting",
-        value_format="{value:.2f}€",
-        reference_format="{delta:+.2f}€ vs. last year ({reference:.2f}€)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference with icon",
-        icon="shopping_cart",
-    ),
-]
 
-# Create a layout with four rows and columns. The KPI cards are positioned in the first eight cells, while the remaining cells are empty.
+class NumberInput(vm.VizroBaseModel):
+    type: Literal["number_input"] = "number_input"
+
+    def build(self):
+        return (
+            dbc.Input(
+                id="number-input",
+                type="number",
+                min=0,
+                max=10,
+                step=1,
+                value=5,
+                debounce=True,
+            ),
+        )
+
+
+vm.Page.add_type("components", NumberInput)
+
 page = vm.Page(
-    title="KPI cards",
-    layout=vm.Layout(grid=[[0, 1, 2, 3], [4, 5, 6, 7], [-1, -1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1]]),
-    components=[vm.Figure(figure=figure) for figure in example_cards + example_reference_cards],
-    controls=[vm.Filter(column="Category")],
+    title="Charts UI",
+    components=[
+        NumberInput(),
+        vm.Graph(figure=px.box(gapminder, x="year", y="gdpPercap", color="continent")),
+    ],
+    controls=[vm.Filter(column="year")],
 )
 
 dashboard = vm.Dashboard(pages=[page])
