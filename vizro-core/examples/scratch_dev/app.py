c9c628a8287f59f9c47b2b4a49d00dc52b0c7ec7
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-01-22 16:47:04 +0100

    adding examples to app

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -18,8 +18,8 @@ page = vm.Page(
     ],
     controls=[
         vm.Filter(column="continent", selector=vm.Dropdown(value=["Europe"])),
-        # vm.Filter(column="continent", selector=vm.Dropdown(value="Europe", multi=False)),
-        # vm.Filter(column="continent", selector=vm.Checklist()),
+        vm.Filter(column="continent", selector=vm.Dropdown(value="Europe", multi=False)),
+        vm.Filter(column="continent", selector=vm.Checklist()),
     ],
 )
 
