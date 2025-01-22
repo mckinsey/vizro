c723a9f46ebeea4fc6347056f193b3afe0d31982
petar-qb <petar_pejovic@external.mckinsey.com>
2025-01-22 15:09:00 +0100

    Address PR changes

--- vizro-core/tests/unit/vizro/managers/test_model_manager.py
+++ vizro-core/tests/unit/vizro/managers/test_model_manager.py
@@ -29,7 +29,6 @@ def managers_dashboard_two_pages(vizro_app, page_1, standard_kpi_card):
                     vm.Button(id="page_2_button_id"),
                     vm.Figure(id="page_2_figure_id", figure=standard_kpi_card),
                 ],
-                controls=[vm.Filter(id="page_2_filter", column="year")],
             ),
         ]
     )
@@ -45,7 +44,7 @@ class TestGetModels:
         """model_type is None | page is None -> return all elements."""
         result = [model.id for model in model_manager._get_models()]
 
-        expected_present = {
+        expected = {
             "page_1_id",
             "page_1_button_id",
             "page_1_graph_id",
@@ -54,37 +53,37 @@ class TestGetModels:
             "page_2_figure_id",
         }
 
-        assert expected_present.issubset(result)
+        assert expected.issubset(result)
 
     def test_model_type_page_none(self):
         """model_type is vm.Button | page is None -> return all vm.Button from the dashboard."""
         result = [model.id for model in model_manager._get_models(model_type=vm.Button)]
 
-        expected_present = {"page_1_button_id", "page_2_button_id"}
-        expected_absent = {"page_1_id", "page_1_graph_id", "page_2_id", "page_2_figure_id"}
+        expected = {"page_1_button_id", "page_2_button_id"}
+        excluded = {"page_1_id", "page_1_graph_id", "page_2_id", "page_2_figure_id"}
 
-        assert expected_present.issubset(result)
-        assert expected_absent.isdisjoint(result)
+        assert expected.issubset(result)
+        assert excluded.isdisjoint(result)
 
     def test_model_type_none_page_not_none(self, page_1):
         """model_type is None | page is page_1 -> return all elements from the page_1."""
         result = [model.id for model in model_manager._get_models(page=page_1)]
 
-        expected_present = {"page_1_id", "page_1_button_id", "page_1_graph_id"}
-        expected_absent = {"page_2_id", "page_2_button_id", "page_2_figure_id"}
+        expected = {"page_1_id", "page_1_button_id", "page_1_graph_id"}
+        excluded = {"page_2_id", "page_2_button_id", "page_2_figure_id"}
 
-        assert expected_present.issubset(result)
-        assert expected_absent.isdisjoint(result)
+        assert expected.issubset(result)
+        assert excluded.isdisjoint(result)
 
     def test_model_type_not_none_page_not_none(self, page_1):
         """model_type is vm.Button | page is page_1 -> return all vm.Button from the page_1."""
         result = [model.id for model in model_manager._get_models(model_type=vm.Button, page=page_1)]
 
-        expected_present = {"page_1_button_id"}
-        expected_absent = {"page_1_id", "page_1_graph_id", "page_2_id", "page_2_button_id", "page_2_figure_id"}
+        expected = {"page_1_button_id"}
+        excluded = {"page_1_id", "page_1_graph_id", "page_2_id", "page_2_button_id", "page_2_figure_id"}
 
-        assert expected_present.issubset(result)
-        assert expected_absent.isdisjoint(result)
+        assert expected.issubset(result)
+        assert excluded.isdisjoint(result)
 
     def test_model_type_no_match_page_none(self):
         """model_type matches no type | page is None -> return empty list."""
@@ -104,21 +103,21 @@ class TestGetModels:
         """model_type is tuple of models -> return all elements of the specified types from the dashboard."""
         result = [model.id for model in model_manager._get_models(model_type=(vm.Button, vm.Graph))]
 
-        expected_present = {"page_1_button_id", "page_1_graph_id", "page_2_button_id"}
-        expected_absent = {"page_1_id", "page_2_id", "page_2_figure_id"}
+        expected = {"page_1_button_id", "page_1_graph_id", "page_2_button_id"}
+        excluded = {"page_1_id", "page_2_id", "page_2_figure_id"}
 
-        assert expected_present.issubset(result)
-        assert expected_absent.isdisjoint(result)
+        assert expected.issubset(result)
+        assert excluded.isdisjoint(result)
 
     def test_model_type_figure_models(self):
         """model_type is FIGURE_MODELS | page is None -> return all figure elements from the dashboard."""
         result = [model.id for model in model_manager._get_models(model_type=FIGURE_MODELS)]
 
-        expected_present = {"page_1_graph_id", "page_2_figure_id"}
-        expected_absent = {"page_1_id", "page_1_button_id", "page_2_id", "page_2_button_id"}
+        expected = {"page_1_graph_id", "page_2_figure_id"}
+        excluded = {"page_1_id", "page_1_button_id", "page_2_id", "page_2_button_id"}
 
-        assert expected_present.issubset(result)
-        assert expected_absent.isdisjoint(result)
+        assert expected.issubset(result)
+        assert excluded.isdisjoint(result)
 
     def test_subclass_model_type(self, page_1, standard_px_chart):
         """model_type is subclass of vm.Graph -> return all elements of the specified type and its subclasses."""
@@ -140,35 +139,37 @@ class TestGetModels:
 
     # This test checks if the model manager can find model_type under a nested model.
     @pytest.mark.parametrize(
-        "controls, expected_id",
+        "get_nested_control",
         [
             # model as a property of a custom model
-            (vm.Filter(id="page_1_control_0", column="year"), "page_1_control_0"),
+            (lambda x: x),
             # model inside a list
-            ([vm.Filter(id="page_1_control_1", column="year")], "page_1_control_1"),
+            (lambda x: [x]),
             # model as a value in a dictionary
-            ({"key_1": vm.Filter(id="page_1_control_2", column="year")}, "page_1_control_2"),
+            (lambda x: {"key": x}),
             # model nested within a list of lists
-            ([[vm.Filter(id="page_1_control_3", column="year")]], "page_1_control_3"),
+            (lambda x: [[x]]),
             # model nested in a dictionary of dictionaries
-            ({"key_1": {"key_2": vm.Filter(id="page_1_control_4", column="year")}}, "page_1_control_4"),
+            (lambda x: {"key_1": {"key_2": x}}),
             # model nested in a list of dictionaries
-            ([{"key_1": vm.Filter(id="page_1_control_5", column="year")}], "page_1_control_5"),
+            (lambda x: [{"key": x}]),
             # model nested in a dictionary of lists
-            ({"key_1": [vm.Filter(id="page_1_control_6", column="year")]}, "page_1_control_6"),
+            (lambda x: {"key": [x]}),
         ],
     )
-    def test_nested_models(self, page_1, controls, expected_id):
+    def test_nested_models(self, page_1, get_nested_control):
         """Model is nested under another model and known property in different ways -> return the model."""
 
         class ControlGroup(vm.VizroBaseModel):
             controls: Any
 
-        page_1.controls.append(ControlGroup(controls=controls))
+        page_1.controls.append(
+            ControlGroup(controls=get_nested_control(vm.Filter(id="page_1_control_1", column="year")))
+        )
 
         result = [model.id for model in model_manager._get_models(model_type=vm.Filter, page=page_1)]
 
-        assert expected_id in result
+        assert "page_1_control_1" in result
 
     def test_model_under_unknown_field(self, page_1):
         """Model is nested under another model but under an unknown field -> don't return the model."""
@@ -176,7 +177,7 @@ class TestGetModels:
         class ControlGroup(vm.VizroBaseModel):
             unknown_field: Any
 
-        page_1.controls.append(ControlGroup(unknown_field=[vm.Filter(id="page_1_control_1", column="year")]))
+        page_1.controls.append(ControlGroup(unknown_field=vm.Filter(id="page_1_control_1", column="year")))
 
         result = [model.id for model in model_manager._get_models(model_type=vm.Filter, page=page_1)]
 
