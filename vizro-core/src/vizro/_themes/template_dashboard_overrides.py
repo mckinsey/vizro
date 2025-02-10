2fea31f93be3808607c9dfc76475517c50d445d7
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-02-10 18:50:20 +0100

    Make charts transparent when used inside dashboard

--- vizro-core/src/vizro/_themes/template_dashboard_overrides.py
+++ vizro-core/src/vizro/_themes/template_dashboard_overrides.py
@@ -2,6 +2,20 @@
 
 import plotly.graph_objs as go
 
-dashboard_overrides = go.layout.Template(
-    layout={"title": {"pad_l": 0, "pad_r": 0}, "margin_l": 24, "margin_t": 24, "margin_b": 16}
+BG_TRANSPARENT = "rgba(0, 0, 0, 0)"
+
+dashboard_overrides = go.layout.Template()
+dashboard_overrides.layout = go.Layout(
+    title_pad_l=0,
+    title_pad_r=0,
+    margin_l=24,
+    margin_t=24,
+    margin_b=16,
+    paper_bgcolor=BG_TRANSPARENT,
+    plot_bgcolor=BG_TRANSPARENT,
+    geo_bgcolor=BG_TRANSPARENT,
+    geo_lakecolor=BG_TRANSPARENT,
+    geo_landcolor=BG_TRANSPARENT,
+    polar_bgcolor=BG_TRANSPARENT,
+    ternary_bgcolor=BG_TRANSPARENT,
 )
