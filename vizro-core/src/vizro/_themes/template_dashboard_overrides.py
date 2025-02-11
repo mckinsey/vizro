81d4559f67bf9299d1c25f4cab3afdb0f9729091
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-02-11 10:42:47 +0100

    Sort alphabetically

--- vizro-core/src/vizro/_themes/template_dashboard_overrides.py
+++ vizro-core/src/vizro/_themes/template_dashboard_overrides.py
@@ -6,16 +6,16 @@ BG_TRANSPARENT = "rgba(0, 0, 0, 0)"
 
 dashboard_overrides = go.layout.Template()
 dashboard_overrides.layout = go.Layout(
-    title_pad_l=0,
-    title_pad_r=0,
-    margin_l=24,
-    margin_t=24,
-    margin_b=16,
-    paper_bgcolor=BG_TRANSPARENT,
-    plot_bgcolor=BG_TRANSPARENT,
     geo_bgcolor=BG_TRANSPARENT,
     geo_lakecolor=BG_TRANSPARENT,
     geo_landcolor=BG_TRANSPARENT,
+    margin_b=16,
+    margin_l=24,
+    margin_t=24,
+    paper_bgcolor=BG_TRANSPARENT,
+    plot_bgcolor=BG_TRANSPARENT,
     polar_bgcolor=BG_TRANSPARENT,
     ternary_bgcolor=BG_TRANSPARENT,
+    title_pad_l=0,
+    title_pad_r=0,
 )
