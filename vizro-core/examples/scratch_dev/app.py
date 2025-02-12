e1d8a1511279c51ba8a3eef106ec99c24a39c786
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-02-12 10:38:38 +0100

    Update bootstrap file with fixes

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,14 +1,38 @@
 """Test app"""
 
-import vizro.models as vm
-import vizro.plotly.express as px
-from vizro import Vizro
+from dash import Dash, html, clientside_callback, Output, Input
+import dash_bootstrap_components as dbc
+import vizro
 
-page = vm.Page(
-    title="foo", components=[vm.Graph(figure=px.scatter(data_frame=px.data.iris(), x="sepal_width", y="sepal_length"))]
+app = Dash(__name__, external_stylesheets=[dbc.icons.FONT_AWESOME, vizro.bootstrap])
+
+color_mode_switch = html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="switch"),
+        dbc.Switch(id="switch", value=False, className="d-inline-block ms-1"),
+        dbc.Label(className="fa fa-sun", html_for="switch"),
+    ]
 )
 
-dashboard = vm.Dashboard(pages=[page])
+app.layout = dbc.Container(
+    [
+        html.H3(["Bootstrap Light Dark Color Modes Demo"], className="bg-primary p-2"),
+        color_mode_switch,
+    ]
+)
+
+clientside_callback(
+    """
+    (switchOn) => {
+       switchOn
+         ? document.documentElement.setAttribute('data-bs-theme', 'light')
+         : document.documentElement.setAttribute('data-bs-theme', 'dark')
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("switch", "id"),
+    Input("switch", "value"),
+)
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run()
+    app.run_server(debug=True)
