2b897b2d803ac00f994f2b831c23011fd5bab4e2
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-02-14 13:40:38 +0100

    Add scratch example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -7,17 +7,69 @@ from vizro import Vizro
 iris = px.data.iris()
 
 page = vm.Page(
-    title="Page with subsections",
-    layout=vm.Layout(grid=[[0, 1]]),
+    title="New Line Height",
+    layout=vm.Layout(grid=[[0,1]]),
     components=[
-        vm.Container(
-            title="Container I",
-            components=[vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"))],
+        vm.Card(
+            id="new-line-height",
+            text="""
+            # New
+            
+            ## What is Vizro?
+            
+            Vizro is an open-source dashboarding framework developed by McKinsey. Built with Plotly and Dash, Vizro 
+            provides a high-level API for creating interactive, production-ready dashboards with minimal code. 
+            It includes pre-configured layouts, themes, and components, making it easier to build data-driven 
+            applications.
+
+            Even if you're not creating a Vizro app, you can still use its styling and design system in your Dash 
+            applications.
+
+            ## Vizro Features Available for Dash Apps
+            
+            * Vizro Bootstrap-themed figure templates are available in the dash-bootstrap-templates library starting 
+            from version 2.1.0. Both dark and light-themed templates are included.
+
+            * Vizro Bootstrap theme provides styling for Bootstrap components, allowing them to match the Vizro light 
+            or dark theme.
+
+            * Vizro theme for other Dash components extends styling beyond Bootstrap. Vizro includes custom CSS to 
+            theme additional Dash components that are not part of Bootstrap. You can explore all the custom CSS files 
+            in their GitHub repository.
+
+            * Vizro KPI cards like the ones shown in the image above can be added to a regular Dash app, bringing a 
+            visually consistent way to display key performance indicators. For more details, see this Plotly forum post."""
         ),
-        vm.Container(
-            title="Container II",
-            components=[vm.Graph(figure=px.box(iris, x="species", y="sepal_length", color="species"))],
-        ),
+        vm.Card(
+            id="old-line-height",
+            text="""
+            # Old
+            
+            ## What is Vizro?
+        
+            Vizro is an open-source dashboarding framework developed by McKinsey. Built with Plotly and Dash, Vizro 
+            provides a high-level API for creating interactive, production-ready dashboards with minimal code. 
+            It includes pre-configured layouts, themes, and components, making it easier to build data-driven 
+            applications.
+        
+            Even if you're not creating a Vizro app, you can still use its styling and design system in your Dash 
+            applications.
+        
+            ## Vizro Features Available for Dash Apps
+        
+            * Vizro Bootstrap-themed figure templates are available in the dash-bootstrap-templates library starting 
+            from version 2.1.0. Both dark and light-themed templates are included.
+        
+            * Vizro Bootstrap theme provides styling for Bootstrap components, allowing them to match the Vizro light 
+            or dark theme.
+        
+            * Vizro theme for other Dash components extends styling beyond Bootstrap. Vizro includes custom CSS to 
+            theme additional Dash components that are not part of Bootstrap. You can explore all the custom CSS files 
+            in their GitHub repository.
+        
+            * Vizro KPI cards like the ones shown in the image above can be added to a regular Dash app, bringing a 
+            visually consistent way to display key performance indicators. For more details, see this Plotly forum post."""
+                ),
     ],
 )
 
