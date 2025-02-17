0e5b6d75c463237737eb6d69088636aff56098dc
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-02-17 10:42:03 +0100

    Add unstyled container to example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -8,24 +8,41 @@ iris = px.data.iris()
 
 page = vm.Page(
     title="Page with subsections",
-    layout=vm.Layout(grid=[[0, 1]]),
+    layout=vm.Layout(grid=[[0, 3, 3, 3, 4, 4], [1, 3, 3, 3, 4, 4], [2, 3, 3, 3, 4, 4]]),
     components=[
+        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
+        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
+        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
         vm.Container(
             title="Container I",
             components=[
-                vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
                 vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
             ],
             background=True,
         ),
         vm.Container(
             title="Container II",
-            components=[vm.Graph(figure=px.box(iris, x="species", y="sepal_length", color="species"))],
+            components=[
+                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
+            ],
+            background=True,
         ),
     ],
 )
 
-dashboard = vm.Dashboard(pages=[page])
+page_two = vm.Page(
+    title="Container",
+    components=[
+        vm.Container(
+            title="Container III",
+            components=[
+                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
+            ],
+        ),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[page, page_two])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
