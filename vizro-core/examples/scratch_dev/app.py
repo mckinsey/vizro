ca17343bb32a6d5e10a781f65dc1ceaeeee38ec4
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2025-02-26 10:17:48 +0100

    Clean up app.py

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,26 +1,16 @@
 """Dev app to try things out."""
 
 from datetime import date
-from os import chown
 
-import pandas as pd
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models.types import capture
-from vizro.tables import dash_ag_grid
 
 iris = px.data.iris()
 
 
-@capture("ag_grid")
-def my_custom_ag_grid(data_frame, chosen_columns, **kwargs):
-    print(f"\nChosen column: {chosen_columns}\n")
-    return dash_ag_grid(data_frame=data_frame[chosen_columns], **kwargs)()  # type: ignore
-
-
 page = vm.Page(
-    title="Page with subsections",
+    title="Container",
     layout=vm.Layout(grid=[[0, 3, 3, 3, 4, 4], [1, 3, 3, 3, 4, 4], [2, 3, 3, 3, 4, 4]]),
     components=[
         vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),  # type: ignore
@@ -39,18 +29,6 @@ page = vm.Page(
                 vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),  # type: ignore
             ],
             extra={"class_name": "bg-container"},
-        ),
-    ],
-)
-
-page_two = vm.Page(
-    title="Container",
-    components=[
-        vm.Container(
-            title="Container III",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),  # type: ignore
-            ],
         ),
     ],
 )
@@ -261,7 +239,6 @@ page_date_picker = vm.Page(
 dashboard = vm.Dashboard(
     pages=[
         page,
-        page_two,
         page_card,
         page_button,
         page_dropdown,
