232d4cdf68fb0d528ca45d66862fd056fb792acf
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-02-28 10:40:01 +0100

    poc collapsible container
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    more clientside callback to component build

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -3,65 +3,45 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from components import CollapsibleContainer, FlexContainer
+
+vm.Page.add_type("components", FlexContainer)
+FlexContainer.add_type("components", CollapsibleContainer)
+vm.Page.add_type("components", CollapsibleContainer)
 
 iris = px.data.iris()
 
 page = vm.Page(
-    title="Page with subsections",
-    layout=vm.Layout(grid=[[0, 0, 1, 1, 2, 2], [3, 3, 3, 4, 4, 4], [3, 3, 3, 4, 4, 4]]),
+    title="Collapse containers with flex container",
     components=[
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Container(
-            title="Container I",
+        FlexContainer(
             components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            theme="outlined",
-        ),
-        vm.Container(
-            title="Container II",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            theme="filled",
-        ),
+                CollapsibleContainer(
+                    id="collapsible-container",
+                    title="Collapsible container",
+                    components=[
+                        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+                        vm.Graph(figure=px.histogram(iris, x="sepal_width", color="species")),
+                        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+                    ],
+                    layout=vm.Layout(grid=[[0, 0, 0, 0], [1, 1, 2, 2]]),
+                ),
+                CollapsibleContainer(
+                    title="Collapsible container 2",
+                    components=[
+                        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+                        vm.Graph(figure=px.histogram(iris, x="sepal_width", color="species")),
+                    ],
+                    layout=vm.Layout(grid=[[0, 0, 0, 0], [1, 1, 1, 1]]),
+                ),
+            ]
+        )
     ],
 )
 
-page_two = vm.Page(
-    title="Container",
-    components=[
-        vm.Container(
-            title="Container III",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-        ),
-    ],
-)
 
-page_three = vm.Page(
-    title="Container Style",
-    components=[
-        vm.Container(
-            title="Container I",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            theme="outlined",
-        ),
-        vm.Container(
-            title="Container II",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            theme="filled",
-        ),
-    ],
-)
-dashboard = vm.Dashboard(pages=[page, page_two, page_three])
+dashboard = vm.Dashboard(pages=[page])
+
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
