8f49524e561c461b2828478b9c6789007a3079d0
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-02-28 10:40:01 +0100

    poc collapsible container

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -4,43 +4,57 @@ import pandas as pd
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.tables import dash_ag_grid
-from vizro.models.types import capture
+from components import CollapsibleContainer, FlexContainer
+from dash import Input, Output, clientside_callback
+
+vm.Page.add_type("components", FlexContainer)
+FlexContainer.add_type("components", CollapsibleContainer)
+
+vm.Page.add_type("components", CollapsibleContainer)
 
 
-df = px.data.iris()
-
-
-@capture("ag_grid")
-def my_custom_ag_grid(data_frame, chosen_columns, **kwargs):
-    print(f"\nChosen column: {chosen_columns}\n")
-    return dash_ag_grid(data_frame=data_frame[chosen_columns], **kwargs)()
+iris = px.data.iris()
 
 
 page = vm.Page(
-    title="Fix empty dropdown as parameter",
+    title="Collapse containers with flex container",
     components=[
-        vm.AgGrid(
-            id="my_custom_ag_grid",
-            figure=my_custom_ag_grid(
-                data_frame=df,
-                chosen_columns=df.columns.to_list(),
-            ),
+        FlexContainer(
+            components=[
+                CollapsibleContainer(
+                    id="collapsible-container",
+                    title="Collapsible container",
+                    components=[
+                        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+                        vm.Graph(figure=px.histogram(iris, x="sepal_width", color="species")),
+                        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+                    ],
+                    layout=vm.Layout(grid=[[0, 0, 0, 0], [1, 1, 2, 2]]),
+                ),
+                vm.Container(
+                    title="Regular container",
+                    components=[vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species"))],
+                ),
+            ]
         )
-    ],
-    controls=[
-        vm.Parameter(
-            targets=["my_custom_ag_grid.chosen_columns"],
-            selector=vm.Dropdown(
-                title="Choose columns",
-                options=df.columns.to_list(),
-                multi=True,
-            ),
-        ),
     ],
 )
 
+
 dashboard = vm.Dashboard(pages=[page])
+
+clientside_callback(
+    """
+    (n_clicks) => {
+       if (n_clicks % 2 === 1) {
+        return [true, {transform: "rotate(180deg)", transition: "transform 0.35s ease-in-out"}];
+    } else return [false, {transform: "rotate(0deg)", transition: "transform 0.35s ease-in-out"}];
+    }
+    """,
+    [Output("collapsible-container", "is_open"), Output("collapsible-container_icon", "style")],
+    Input("collapsible-container_title", "n_clicks"),
+)
+
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
