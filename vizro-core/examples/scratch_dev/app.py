b96f6bc715e92b27eafc11ce8e38288c15f55416
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-03-04 20:13:59 +0100

    Make Table and AgGrid transparent

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -3,60 +3,38 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.tables import dash_ag_grid, dash_data_table
 
 iris = px.data.iris()
 
-page = vm.Page(
-    title="Page with subsections",
-    layout=vm.Layout(grid=[[0, 0, 1, 1, 2, 2], [3, 3, 3, 4, 4, 4], [3, 3, 3, 4, 4, 4]]),
+page= vm.Page(
+    title="AgGrid",
     components=[
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Card(text="""Hello, this is a card with a [link](https://www.google.com)"""),
-        vm.Container(
-            title="Container I",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            variant="outlined",
-        ),
         vm.Container(
             title="Container II",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
+            components=[vm.AgGrid(figure=dash_ag_grid(iris))],
             variant="filled",
         ),
     ],
 )
 
-page_two = vm.Page(
-    title="Container",
+page_two= vm.Page(
+    title="Data Table",
     components=[
         vm.Container(
-            title="Container III",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
+            title="Container II",
+            components=[vm.Table(figure=dash_data_table(iris))],
+            variant="filled",
         ),
     ],
 )
 
-page_three = vm.Page(
-    title="Container Style",
+page_three= vm.Page(
+    title="Graph",
     components=[
         vm.Container(
-            title="Container I",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
-            variant="outlined",
-        ),
-        vm.Container(
             title="Container II",
-            components=[
-                vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species")),
-            ],
+            components=[vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"))],
             variant="filled",
         ),
     ],
