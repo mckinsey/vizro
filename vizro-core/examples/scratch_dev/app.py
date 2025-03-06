e97c0f7b06e7170e4769ef2e6024f27e4ef65353
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-03-06 12:07:30 +0100

    Add tables and buttons in example config

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -6,7 +6,8 @@ See: https://vizro.readthedocs.io/en/stable/pages/tutorials/explore-components/
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-
+from vizro.figures import kpi_card
+from vizro.tables import dash_ag_grid, dash_data_table
 
 tips = px.data.tips()
 
@@ -56,13 +57,17 @@ page3 = vm.Page(
     title="Flex - default",
     layout=vm.Flex(),
     components=[
-        vm.Card(text="""
+        vm.Card(
+            text="""
                # Lorem Ipsum
 
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit. 
-               In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum. 
-               Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum. 
-           """) for i in range(6)],
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit.
+               In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum.
+               Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum.
+           """
+        )
+        for i in range(6)
+    ],
 )
 
 
@@ -70,26 +75,34 @@ page4 = vm.Page(
     title="Flex - gap",
     layout=vm.Flex(gap="40px"),
     components=[
-        vm.Card(text="""
+        vm.Card(
+            text="""
             # Lorem Ipsum
 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit. 
-            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum. 
-            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum. 
-        """) for i in range(6)],
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit.
+            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum.
+            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum.
+        """
+        )
+        for i in range(6)
+    ],
 )
 
 page5 = vm.Page(
     title="Flex - row",
     layout=vm.Flex(direction="row"),
     components=[
-        vm.Card(text="""
+        vm.Card(
+            text="""
             # Lorem Ipsum
 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit. 
-            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum. 
-            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum. 
-        """) for i in range(6)],
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit.
+            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum.
+            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum.
+        """
+        )
+        for i in range(6)
+    ],
 )
 
 page6 = vm.Page(
@@ -97,13 +110,17 @@ page6 = vm.Page(
     title="Flex - row/wrap",
     layout=vm.Flex(direction="row", wrap=True),
     components=[
-        vm.Card(text="""
+        vm.Card(
+            text="""
             # Lorem Ipsum
 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit. 
-            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum. 
-            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum. 
-        """) for i in range(6)],
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit.
+            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum.
+            Nam ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum.
+        """
+        )
+        for i in range(6)
+    ],
 )
 
 page7 = vm.Page(
@@ -132,8 +149,65 @@ page10 = vm.Page(
     components=[vm.Graph(figure=px.violin(tips, y="tip", x="day", color="day", box=True)) for i in range(6)],
 )
 
-
 page11 = vm.Page(
+    title="Flex - default - aggrid",
+    layout=vm.Flex(),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+
+page12 = vm.Page(
+    title="Flex - gap - aggrid",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+page13 = vm.Page(
+    title="Flex - row - aggrid",
+    layout=vm.Flex(direction="row"),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+page14 = vm.Page(
+    title="Flex - default - table",
+    layout=vm.Flex(),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+
+page15 = vm.Page(
+    title="Flex - gap - table",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+page16 = vm.Page(
+    title="Flex - row - table",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+page17 = vm.Page(
+    title="Flex - default - button",
+    layout=vm.Flex(),
+    components=[vm.Button() for i in range(9)],
+)
+
+
+page18 = vm.Page(
+    title="Flex - gap - button",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.Button() for i in range(9)],
+)
+
+page19 = vm.Page(
+    title="Flex - row - button",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Button() for i in range(9)],
+)
+
+
+page20 = vm.Page(
     title="Flex - Graphs with Card",
     layout=vm.Flex(),
     components=[
@@ -154,7 +228,7 @@ page11 = vm.Page(
     controls=[vm.Filter(column="day")],
 )
 
-page12 = vm.Page(
+page21 = vm.Page(
     title="Flex - Container",
     components=[
         vm.Container(
@@ -179,7 +253,7 @@ page12 = vm.Page(
     controls=[vm.Filter(column="day")],
 )
 
-page13 = vm.Page(
+page22 = vm.Page(
     title="Flex - Container with card",
     components=[
         vm.Container(
@@ -205,13 +279,53 @@ page13 = vm.Page(
     controls=[vm.Filter(column="day")],
 )
 
-dashboard = vm.Dashboard(
-    pages=[page1, page2, page3, page4, page5, page6, page7, page8, page9, page10,
-           page11, page12, page13],
-    title="Test out Flex/Grid",
+page23 = vm.Page(
+    id="page-flex-inside-flex",
+    title="Flex inside flex",
+    layout=vm.Flex(),
+    components=[
+        vm.Container(
+            title="KPI Banner",
+            layout=vm.Flex(direction="row"),
+            components=[
+                vm.Figure(
+                    figure=kpi_card(
+                        data_frame=tips,
+                        value_column="total_bill",
+                        agg_func="mean",
+                        value_format="${value:.2f}",
+                        title="Average Bill",
+                    )
+                )
+                for i in range(4)
+            ],
+        ),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Total Bill ($)",
+                    components=[
+                        vm.Graph(figure=px.histogram(tips, x="total_bill")),
+                    ],
+                ),
+                vm.Container(
+                    title="Total Tips ($)",
+                    components=[
+                        vm.Graph(figure=px.histogram(tips, x="tip")),
+                    ],
+                ),
+            ],
+        ),
+    ],
+    controls=[vm.Filter(column="day"), vm.Filter(column="time", selector=vm.Checklist()), vm.Filter(column="size")],
 )
 
 
+dashboard = vm.Dashboard(
+    pages=[page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11, page12, page13, page14,
+           page15, page16, page17, page18, page19, page20, page21, page22, page23],
+    title="Test out Flex/Grid",
+)
 
 
 if __name__ == "__main__":
