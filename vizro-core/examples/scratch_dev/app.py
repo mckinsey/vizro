4b94744970500a51cdbdeafc88f941a3e4ac14da
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2025-03-18 12:41:49 +0100

    [Feat] Dynamic data parameters automatically refresh the relevant dynamic filters (#1051)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,40 +1,61 @@
-"""Dev app to try things out."""
+import time
+
+import pandas as pd
 
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.managers import data_manager
 
 
-# Comment to restart the CI
-df = px.data.iris()
+static_df = pd.DataFrame(
+    {
+        "species": ["artificial_species", "artificial_species", "artificial_species"],
+        "sepal_width": [4, 5, 6],
+        "sepal_length": [4, 5, 6],
+    }
+)
 
 
-# Enable vm.Filter object to be used within the "Page.components"
-vm.Page.add_type("components", vm.Filter)
-vm.Page.add_type("components", vm.Parameter)
+def load_data(number_of_points=150):
+    # Artificial delay to simulate data loading in production
+    print("\nLoading data...\n")
+    time.sleep(1)
+
+    return px.data.iris().head(number_of_points)
+
+
+# A comment to rerun the docs build CI process
+data_manager["dynamic_df"] = load_data
 
 page_1 = vm.Page(
-    title="Page 1",
-    controls=[
-        # DOES NOT work from the main branch
-        vm.Filter(column="species", targets=["graph"]),
-        vm.Parameter(targets=["graph.x"], selector=vm.RadioItems(options=["sepal_length", "sepal_width"])),
-        # WORKS from the main branch
-        vm.Filter(column="species"),
-    ],
+    title="Update dynamic filter from DFP",
     components=[
-        # DOES NOT work from the main branch
-        vm.Filter(column="species", targets=["graph"]),
-        vm.Parameter(targets=["graph.y"], selector=vm.RadioItems(options=["sepal_length", "sepal_width"])),
-        # Graph
-        vm.Graph(id="graph", figure=px.scatter(df, x="sepal_length", y="petal_length", color="species")),
-        # WORKS from the main branch
-        vm.Filter(column="species", targets=["graph"]),
-        vm.Parameter(targets=["graph.color"], selector=vm.RadioItems(options=["species", "petal_width"])),
+        vm.Graph(
+            id="dynamic_graph_1",
+            figure=px.scatter(data_frame="dynamic_df", x="sepal_width", y="sepal_length", color="species"),
+        ),
+        vm.Graph(
+            figure=px.scatter(data_frame=static_df, x="sepal_width", y="sepal_length", color="species"),
+        ),
+    ],
+    controls=[
+        vm.Filter(column="species", selector=vm.RadioItems()),
+        vm.Parameter(
+            targets=["dynamic_graph_1.data_frame.number_of_points"],
+            selector=vm.Slider(
+                min=0,
+                max=150,
+                value=150,
+                title="Number of points",
+                step=10,
+            ),
+        ),
     ],
 )
 
 dashboard = vm.Dashboard(pages=[page_1])
+
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
