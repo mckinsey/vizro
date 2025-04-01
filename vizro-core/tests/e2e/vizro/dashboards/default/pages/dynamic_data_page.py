879518154429c9ddda077b3032e4c3f85e8c3c60
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-04-01 14:32:34 +0200

    data frame parameter test

--- vizro-core/tests/e2e/vizro/dashboards/default/pages/dynamic_data_page.py
+++ vizro-core/tests/e2e/vizro/dashboards/default/pages/dynamic_data_page.py
@@ -11,11 +11,21 @@ def load_iris_data_sample(number_of_points=10):
     return iris
 
 
+def load_iris_data_head(number_of_points=150):
+    # # Artificial delay to simulate data loading in production
+    # print("\nLoading data...\n")
+    # time.sleep(1)
+
+    return px.data.iris().head(number_of_points)
+
+
 data_manager.cache = Cache(config={"CACHE_TYPE": "FileSystemCache", "CACHE_DIR": "cache"})
 data_manager["iris_cached"] = load_iris_data_sample
 data_manager["iris_cached"].timeout = 300
 data_manager["iris_not_cached"] = load_iris_data_sample
 data_manager["iris_not_cached"].timeout = -1  # NOT cached
+data_manager["iris_head"] = load_iris_data_head
+data_manager["iris_head"].timeout = -1  # NOT cached
 
 dynamic_data_page = vm.Page(
     title=cnst.DYNAMIC_DATA_PAGE,
@@ -37,6 +47,37 @@ dynamic_data_page = vm.Page(
         vm.Parameter(
             targets=[f"{cnst.SCATTER_DYNAMIC_ID}.data_frame.number_of_points"],
             selector=vm.Slider(id=cnst.SLIDER_DYNAMIC_DATA_ID, min=10, max=100, step=10, value=10),
+        ),
+    ],
+)
+
+
+dynamic_data_df_parameter_page = vm.Page(
+    title=cnst.DYNAMIC_DATA_DF_PARAMETER_PAGE,
+    components=[
+        vm.Graph(
+            id=cnst.SCATTER_DF_PARAMETER,
+            figure=px.scatter(
+                data_frame="iris_head",
+                x="sepal_width",
+                y="sepal_length",
+                color="species",
+                color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222", "virginica": "#3949ab"},
+            ),
+        ),
+    ],
+    controls=[
+        vm.Filter(column="species", selector=vm.RadioItems(id=cnst.RADIOITEMS_FILTER_DF_PARAMETER)),
+        vm.Parameter(
+            targets=[f"{cnst.SCATTER_DF_PARAMETER}.data_frame.number_of_points"],
+            selector=vm.Slider(
+                id=cnst.SLIDER_DF_PARAMETER,
+                min=0,
+                max=150,
+                value=150,
+                title="Number of points",
+                step=10,
+            ),
         ),
     ],
 )
