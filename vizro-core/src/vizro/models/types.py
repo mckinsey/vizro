23e14cbe81e2781ea7810ed9f5c4998f056ec067
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-04-02 14:44:36 +0200

    Update schema and fix typo

--- vizro-core/src/vizro/models/types.py
+++ vizro-core/src/vizro/models/types.py
@@ -24,7 +24,7 @@ def get_layout_discriminator(layout: Any) -> str:
     if isinstance(layout, dict):
         # If type is supplied then use that (like saying discriminator="type"). Otherwise, it's the legacy case where
         # type is not specified, in which case we want to use vm.Layout, which has type="grid".
-        return layout.get("type", "grid")
+        return layout.get("type", "layout")
 
     # If a model has been specified then this is equivalent to saying discriminator="type". When None is returned,
     # union_tag_not_found error is raised.
@@ -542,7 +542,7 @@ NavSelectorType = Annotated[
 
 
 LayoutType = Annotated[
-    Union[Annotated["Layout", Tag("grid")], Annotated["Flex", Tag("flex")]],
+    Union[Annotated["Layout", Tag("layout")], Annotated["Flex", Tag("flex")]],
     Field(
         discriminator=Discriminator(get_layout_discriminator),
         description="Type of layout to place components on the page.",
