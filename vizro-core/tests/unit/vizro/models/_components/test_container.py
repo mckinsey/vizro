2bfec350fa904ab42ee095797230a40a87c87b5c
Antony Milne <antony.milne@quantumblack.com>
2025-04-03 12:20:26 +0100

    Fix tests

--- vizro-core/tests/unit/vizro/models/_components/test_container.py
+++ vizro-core/tests/unit/vizro/models/_components/test_container.py
@@ -27,14 +27,14 @@ class TestContainerInstantiation:
             components=[vm.Button(), vm.Button()],
             layout=vm.Layout(grid=[[0, 1]]),
             variant=variant,
-            collapse=True,
+            collapsed=True,
         )
         assert container.id == "my-id"
         assert isinstance(container.components[0], vm.Button) and isinstance(container.components[1], vm.Button)
         assert container.layout.grid == [[0, 1]]
         assert container.title == "Title"
         assert container.variant == variant
-        assert container.collapse is True
+        assert container.collapsed is True
 
     def test_mandatory_title_missing(self):
         with pytest.raises(ValidationError, match="Field required"):
@@ -87,18 +87,34 @@ class TestContainerBuildMethod:
         )
 
     @pytest.mark.parametrize(
-        "collapse",
+        "collapsed",
         [True, False],
     )
-    def test_container_with_collapse(self, collapse):
-        result = vm.Container(title="Title", components=[vm.Button()], collapse=collapse, id="test").build()
+    def test_container_with_collapse(self, collapsed):
+        result = vm.Container(title="Title", components=[vm.Button()], collapsed=collapsed, id="test").build()
 
-        # We want to test if html.Span is created in Container.build
+        assert_component_equal(result.children, [html.H3(), dbc.Collapse()], keys_to_strip=STRIP_ALL)
+
+        # We still want to test the exact H3 and dbc.Collapse inside the result
+        expected_title_content = [
+            "Title",
+            html.Span(
+                "keyboard_arrow_down" if collapsed else "keyboard_arrow_up",
+                className="material-symbols-outlined",
+                id="test_icon",
+            ),
+            dbc.Tooltip(
+                id="test_tooltip",
+                children="Show Content" if collapsed else "Hide Content",
+                target="test_icon",
+            ),
+        ]
+
         assert_component_equal(
-            result.children[0].children[1],
-            html.Span("keyboard_arrow_up", className="material-symbols-outlined", id="test_icon"),
+            result.children[0], html.H3(expected_title_content, className="container-title", id="test_title")
         )
-        # We want to test if correct class_name has been applied
         assert_component_equal(
-            result, dbc.Container(class_name="border p-3", fluid=True), keys_to_strip={"children", "id"}
+            result.children[1],
+            dbc.Collapse(id="test_collapse", is_open=not collapsed, className="collapsible-container", key="test"),
+            keys_to_strip={"children"},
         )
