3b48ff3f6c63db1b8542ba9ebdf654c983d17b3d
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-04-04 11:15:11 +0200

    add flex layout examples

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -9,8 +9,8 @@ from vizro import Vizro
 
 iris = px.data.iris()
 
-page = vm.Page(
-    title="Collapsible containers",
+page1 = vm.Page(
+    title="Collapsible containers - grid layout",
     layout=vm.Layout(grid=[[0, 1]]),
     components=[
         vm.Container(
@@ -27,5 +27,23 @@ page = vm.Page(
     ],
 )
 
-dashboard = vm.Dashboard(pages=[page])
+page2 = vm.Page(
+    title="Collapsible containers - flex layout",
+    layout=vm.Flex(direction="column"),
+    components=[
+        vm.Container(
+            title="Initially collapsed container",
+            components=[vm.Graph(figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"))],
+            collapsed=True,
+            variant="filled",
+        ),
+        vm.Container(
+            title="Initially expanded container",
+            components=[vm.Graph(figure=px.box(iris, x="species", y="sepal_length", color="species"))],
+            collapsed=False,
+        ),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[page1, page2])
 Vizro().build(dashboard).run()
