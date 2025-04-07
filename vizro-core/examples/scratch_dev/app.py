6836fc46aae62cc6d3831e0ee05b6e8a3429e512
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-04-07 11:39:50 +0200

    [Tidy] Unset height and width for Card inside Flex (#1095)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,33 +1,77 @@
 """Scratchpad for testing."""
 
 import vizro.models as vm
-import vizro.plotly.express as px
 from vizro import Vizro
+import random
 
-tips = px.data.tips()
 
-page1 = vm.Page(
-    title="Default",
-    components=[
-        vm.Card(text="""# Good morning!"""),
-        vm.Graph(
-            title="Where do we get more tips?",
-            figure=px.bar(tips, y="tip", x="day"),
-        ),
-        vm.Graph(
-            title="Is the average driven by a few outliers?",
-            figure=px.violin(tips, y="tip", x="day", color="day", box=True),
-        ),
-        vm.Graph(
-            title="Which group size is more profitable?",
-            figure=px.density_heatmap(tips, x="day", y="size", z="tip", histfunc="avg", text_auto="$.2f"),
-        ),
-    ],
-    controls=[vm.Filter(column="day")],
+def generate_lorem_ipsum(length=None):
+    lorem_ipsum = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elementum ligula, in pharetra velit.
+            In ultricies est ac mauris vehicula fermentum. Curabitur faucibus elementum lectus, vitae luctus libero fermentum.
+            Name ut ipsum tortor. Praesent ut nulla risus. Praesent in dignissim nulla. In quis blandit ipsum.
+        """
+    words = lorem_ipsum.split()
+    length = random.randint(100, 500) if length is None else length
+    while len(" ".join(words)) < length:
+        words += words  # repeat the words to extend the length
+    return " ".join(words)[:length]
+
+
+page3 = vm.Page(
+    title="Flex - default",
+    layout=vm.Flex(),
+    components=[vm.Card(text=generate_lorem_ipsum()) for i in range(6)],
 )
 
 
-dashboard = vm.Dashboard(pages=[page1])
+page4 = vm.Page(
+    title="Flex - gap",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.Card(text=generate_lorem_ipsum()) for i in range(6)],
+)
+
+page5 = vm.Page(
+    title="Flex - row - unequal content",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Card(text=generate_lorem_ipsum()) for i in range(6)],
+)
+
+page6 = vm.Page(
+    title="Flex - row/wrap - unequal content",
+    layout=vm.Flex(direction="row", wrap=True),
+    components=[vm.Card(text=generate_lorem_ipsum()) for i in range(6)],
+)
+
+page7 = vm.Page(
+    title="Flex - row - unequal content - same width",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Card(text=generate_lorem_ipsum(), extra={"style": {"width": "200px"}}) for i in range(6)],
+)
+
+page8 = vm.Page(
+    title="Flex - row/wrap - unequal content - same width",
+    layout=vm.Flex(direction="row", wrap=True),
+    components=[vm.Card(text=generate_lorem_ipsum(), extra={"style": {"width": "200px"}}) for i in range(6)],
+)
+
+page9 = vm.Page(
+    title="Flex - row - equal content - no width",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Card(text=generate_lorem_ipsum(300)) for i in range(6)],
+)
+
+page10 = vm.Page(
+    title="Flex - row/wrap - equal content - same width",
+    layout=vm.Flex(direction="row", wrap=True),
+    components=[vm.Card(text=generate_lorem_ipsum(300), extra={"style": {"width": "200px"}}) for i in range(6)],
+)
+
+
+dashboard = vm.Dashboard(
+    pages=[page3, page4, page5, page6, page7, page8, page9, page10],
+    title="Test out Card inside Flex",
+)
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
