42082ed8f2070930ba361424deccff7e11b02292
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-04-14 17:12:46 +0200

    [Tidy] Update styling of `Card` (#1112)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,23 +1,44 @@
-"""Scratchpad for testing."""
+"""Test app"""
 
-import vizro.models as vm
+import vizro.plotly.express as px
 from vizro import Vizro
+import vizro.models as vm
 
-page = vm.Page(
-    title="Buttons with different styles",
-    layout=vm.Flex(direction="row"),
+iris = px.data.iris()
+
+page_home = vm.Page(
+    title="Homepage",
+    layout=vm.Grid(grid=[[0, 1]]),
     components=[
-        vm.Button(text="default"),
-        vm.Button(text="filled", variant="filled"),
-        vm.Button(text="outlined", variant="outlined"),
-        vm.Button(text="plain", variant="plain"),
-        vm.Button(extra={"color": "success"}),
-        vm.Button(variant="outlined", extra={"color": "success"}),
+        vm.Card(
+            text="""
+                        ### Paragraphs
+                        Commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit.
+
+                        Fugiat iusto fuga praesentium option, eaque rerum! Provident similique accusantium nemo autem.
+
+                        Obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid.
+
+                        Culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas.
+                   """
+        ),
+        vm.Text(
+            text="""
+                        ### Paragraphs
+                        Commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit.
+
+                        Fugiat iusto fuga praesentium option, eaque rerum! Provident similique accusantium nemo autem.
+
+                        Obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid.
+
+                        Culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas.
+                   """
+        ),
     ],
 )
 
-dashboard = vm.Dashboard(pages=[page])
 
+dashboard = vm.Dashboard(pages=[page_home])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
