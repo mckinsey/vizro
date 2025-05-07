0777c0d96e262463198b65ff5bb66b26257bff65
nadijagraca <108531476+nadijagraca@users.noreply.github.com>
2025-05-07 18:41:36 +0200

    [Feat] Add info icon to components (#1144)
    Co-authored-by: Antony Milne <antony.milne@quantumblack.com>
    Co-authored-by: Antony Milne <49395058+antonymilne@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,80 +1,68 @@
-# Vizro is an open-source toolkit for creating modular data visualization applications.
-# check out https://github.com/mckinsey/vizro for more info about Vizro
-# and checkout https://vizro.readthedocs.io/en/stable/ for documentation.
+# # Vizro is an open-source toolkit for creating modular data visualization applications.
+# # check out https://github.com/mckinsey/vizro for more info about Vizro
+# # and checkout https://vizro.readthedocs.io/en/stable/ for documentation.
 
 import vizro.plotly.express as px
 from vizro import Vizro
 import vizro.models as vm
-from vizro.models.types import capture
+from vizro.tables import dash_ag_grid
 
-
-@capture("action")
-def action_function_output_list(button_number_of_clicks, _controls):
-    return f"Button clicked {button_number_of_clicks} times.", f"Controls: {str(_controls)}"
-
-
-@capture("action")
-def action_function_output_dict(button_number_of_clicks, _controls):
-    return {
-        "card-id-2": f"Controls: {str(_controls)}.",
-        "card-id-1": f"Button clicked {button_number_of_clicks} times.",
-    }
-
-
-@capture("action")
-def legacy_action_function_output_dict(button_number_of_clicks):
-    return {
-        "card-id-2": f"Button clicked {button_number_of_clicks} times.",
-        "card-id-1": f"Button clicked {button_number_of_clicks} times.",
-    }
-
-
-# TODO: Enable all three cases
-action_output_list = vm.Action(
-    function=action_function_output_list("button-id.n_clicks"),
-    outputs=["card-id-1.children", "card-id-2.children"],
-)
-action_output_dict = vm.Action(
-    function=action_function_output_dict("button-id.n_clicks"),
-    outputs={
-        "card-id-1": "card-id-1.children",
-        "card-id-2": "card-id-2.children",
-    },
-)
-action_output_dict_legacy = vm.Action(
-    function=legacy_action_function_output_dict(),
-    inputs=["button-id.n_clicks"],
-    outputs={
-        "card-id-1": "card-id-1.children",
-        "card-id-2": "card-id-2.children",
-    },
-)
-
+df = px.data.iris()
 
 page = vm.Page(
-    title="Title",
-    layout=vm.Flex(),
+    title="Page with lots of extra information",
+    # You could also do this with vm.Tooltip(text=...)
+    description="""
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Name libero tempore, cum soluta nobis est eligendi option cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
+
+
+        # something something
+        * asdfasdf
+
+        [link](www.google.com)
+        """,
     components=[
-        vm.Graph(figure=px.scatter(px.data.iris(), x="sepal_length", y="petal_width", color="species")),
-        vm.Button(
-            id="button-id",
-            text="Click me",
-            actions=[
-                # action_output_list,
-                # action_output_dict,
-                action_output_dict_legacy,
+        vm.Container(
+            title="Container title",
+            description="Test container tooltip",
+            components=[
+                vm.Graph(
+                    id="scatter_chart",
+                    figure=px.scatter(df, x="sepal_length", y="petal_width", color="species"),
+                    description="test graph description",
+                    title="Graph title",
+                ),
+                vm.AgGrid(
+                    figure=dash_ag_grid(data_frame=df), description="test ag grid description", title="AG Grid title"
+                ),
             ],
-        ),
-        vm.Card(
-            id="card-id-1",
-            text="Click button to update me",
-        ),
-        vm.Card(id="card-id-2", text="Click button to update me"),
+            collapsed=False,
+        )
     ],
-    controls=[vm.Filter(column="species", selector=vm.RadioItems())],
+    controls=[
+        vm.Filter(
+            column="species",
+            selector=vm.Dropdown(
+                description="""
+                    Select which species of iris you like.
+
+                    [Click here](www.google.com) to learn more about flowers.""",
+                # You could also do this with vm.Tooltip(text=...)
+            ),
+        ),
+        vm.Filter(
+            column="species",
+            selector=vm.Checklist(description=vm.Tooltip(text="test", icon="help")),
+        ),
+    ],
 )
 
-dashboard = vm.Dashboard(pages=[page])
+dashboard = vm.Dashboard(
+    pages=[page],
+    title="blah blah blah",
+    # Specify a different icon:
+    # description=vm.Tooltip(text="Hello hello", icon="Help"),
+)
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
