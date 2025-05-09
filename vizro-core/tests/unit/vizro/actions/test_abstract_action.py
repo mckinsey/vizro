8541af1c60431fb2e0acd613930e0ba258ae9781
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-05-09 14:39:00 +0200

    Split up unit tests in AbstractAction to capture different errors

--- vizro-core/tests/unit/vizro/actions/test_abstract_action.py
+++ vizro-core/tests/unit/vizro/actions/test_abstract_action.py
@@ -161,28 +161,6 @@ class TestAbstractActionInputs:
         action = action_class(**inputs)
         assert action._transformed_inputs == expected_transformed_inputs
 
-    @pytest.mark.parametrize(
-        "input",
-        [
-            # These raise validation error on instantiation of action_with_one_runtime_arg due to annotation arg_1: str
-            ["component.property"],
-            1,
-            None,
-            # These raise validation error in _transformed_inputs itself but could raise it on instantiation
-            # if we alter the type hint of action_with_one_runtime_arg to use DotSeparateString.
-            "",
-            "component",
-            "component.",
-            ".property",
-            "component..property",
-            "component_property",
-            "component.property.property",
-        ],
-    )
-    def test_runtime_inputs_invalid(self, input):
-        with pytest.raises(ValidationError):
-            action_with_one_runtime_arg(arg_1=input)._transformed_inputs
-
     # TODO: Adjust this test when _controls becomes a public field. Should demonstrate that a runtime arg called
     # controls overrides the inbuilt behavior. This could be done as a new test case in TestAbstractActionInputs
     # like in test_action.TestActionInputs works.
@@ -226,33 +204,106 @@ class TestAbstractActionOutputs:
         action = action_with_mock_outputs()
         assert action._transformed_outputs == expected_transformed_outputs
 
+class TestAbstractActionTransformDependency:
+    @pytest.mark.parametrize(
+        "input",
+        [
+            # These raise validation error on instantiation of action_with_one_runtime_arg due to annotation arg_1: str
+            ["component.property"],
+            1,
+            None,
+        ],
+    )
+    def test_runtime_inputs_invalid_type(self, input):
+        with pytest.raises(ValidationError):
+            action_with_one_runtime_arg(arg_1=input)._transformed_inputs
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            "component",
+            "component_property",
+        ],
+    )
+    def test_runtime_inputs_invalid_model_id(self, input):
+        with pytest.raises(
+            KeyError,
+            match="Component with ID .* not found. Please provide a valid component ID.",
+        ):
+            action_with_one_runtime_arg(arg_1=input)._transformed_inputs
+
+    @pytest.mark.parametrize(
+        "input",
+        [
+            "",
+            "component.",
+            ".property",
+            "component..property",
+            "component.property.property",
+        ],
+    )
+    def test_runtime_inputs_invalid_dot_syntax(self, input):
+        with pytest.raises(
+            ValueError,
+            match="Invalid input format .*. Expected format is '<component-id>.<property>' or '<component-id>'.",
+        ):
+            action_with_one_runtime_arg(arg_1=input)._transformed_inputs
+
     @pytest.mark.parametrize(
         "action_with_mock_outputs",
         [
             "component.property",
             1,
             None,
-            [""],
-            ["component"],
-            ["component."],
-            [".property"],
-            ["component..property"],
-            ["component_property"],
-            ["component.property.property"],
-            {"output_1": ""},
-            {"output_1": "component"},
-            {"output_1": "component."},
-            {"output_1": ".property"},
-            {"output_1": "component..property"},
-            {"output_1": "component_property"},
-            {"output_1": "component.property.property"},
-            {"output_1": "component.property", "output_2": ""},
             {1: "component.property"},
         ],
         indirect=["action_with_mock_outputs"],
     )
-    def test_outputs_invalid(self, action_with_mock_outputs):
+    def test_outputs_invalid_type(self, action_with_mock_outputs):
         with pytest.raises(ValidationError):
+            # An error is raised when accessing _transformed_outputs which is fine because validation is then performed.
+            action_with_mock_outputs()._transformed_outputs
+
+    @pytest.mark.parametrize(
+        "action_with_mock_outputs",
+        [
+            ["component"],
+            ["component_property"],
+            {"output_1": "component"},
+            {"output_1": "component_property"},
+        ],
+        indirect=["action_with_mock_outputs"],
+    )
+    def test_outputs_invalid_model_id(self, action_with_mock_outputs):
+        with pytest.raises(
+            KeyError,
+            match="Component with ID .* not found. Please provide a valid component ID.",
+        ):
+            # An error is raised when accessing _transformed_outputs which is fine because validation is then performed.
+            action_with_mock_outputs()._transformed_outputs
+
+    @pytest.mark.parametrize(
+        "action_with_mock_outputs",
+        [
+            [""],
+            ["component."],
+            [".property"],
+            ["component..property"],
+            ["component.property.property"],
+            {"output_1": ""},
+            {"output_1": "component."},
+            {"output_1": ".property"},
+            {"output_1": "component..property"},
+            {"output_1": "component.property.property"},
+            {"output_1": "component.property", "output_2": ""},
+        ],
+        indirect=["action_with_mock_outputs"],
+    )
+    def test_outputs_invalid_dot_syntax(self, action_with_mock_outputs):
+        with pytest.raises(
+            ValueError,
+            match="Invalid output format .*. Expected format is '<component-id>.<property>' or '<component-id>'.",
+        ):
             # An error is raised when accessing _transformed_outputs which is fine because validation is then performed.
             action_with_mock_outputs()._transformed_outputs
 
