f303ac94d2b58c6dade6c08a9cb71a2ec21fff89
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2025-05-12 10:36:40 +0200

    Add example app for testing

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -2,79 +2,107 @@
 # check out https://github.com/mckinsey/vizro for more info about Vizro
 # and checkout https://vizro.readthedocs.io/en/stable/ for documentation.
 
+"""Example app from the official vizro user tutorial.
+
+See: https://vizro.readthedocs.io/en/stable/pages/tutorials/explore-components/
+"""
+
+import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-import vizro.models as vm
-from vizro.models.types import capture
+from vizro.figures import kpi_card
+from vizro.tables import dash_ag_grid, dash_data_table
+
+tips = px.data.tips()
 
 
-@capture("action")
-def action_function_output_list(button_number_of_clicks, _controls):
-    return f"Button clicked {button_number_of_clicks} times.", f"Controls: {str(_controls)}"
-
-
-@capture("action")
-def action_function_output_dict(button_number_of_clicks, _controls):
-    return {
-        "card-id-2": f"Controls: {str(_controls)}.",
-        "card-id-1": f"Button clicked {button_number_of_clicks} times.",
-    }
-
-
-@capture("action")
-def legacy_action_function_output_dict(button_number_of_clicks):
-    return {
-        "card-id-2": f"Button clicked {button_number_of_clicks} times.",
-        "card-id-1": f"Button clicked {button_number_of_clicks} times.",
-    }
-
-
-# TODO: Enable all three cases
-action_output_list = vm.Action(
-    function=action_function_output_list("button-id.n_clicks"),
-    outputs=["card-id-1.children", "card-id-2.children"],
-)
-action_output_dict = vm.Action(
-    function=action_function_output_dict("button-id.n_clicks"),
-    outputs={
-        "card-id-1": "card-id-1.children",
-        "card-id-2": "card-id-2.children",
-    },
-)
-action_output_dict_legacy = vm.Action(
-    function=legacy_action_function_output_dict(),
-    inputs=["button-id.n_clicks"],
-    outputs={
-        "card-id-1": "card-id-1.children",
-        "card-id-2": "card-id-2.children",
-    },
-)
-
-
-page = vm.Page(
-    title="Title",
-    layout=vm.Flex(),
+page1 = vm.Page(
+    title="Grid - layout - aggrid",
+    layout=vm.Grid(grid=[[0, 1]]),
     components=[
-        vm.Graph(figure=px.scatter(px.data.iris(), x="sepal_length", y="petal_width", color="species")),
-        vm.Button(
-            id="button-id",
-            text="Click me",
-            actions=[
-                # action_output_list,
-                # action_output_dict,
-                action_output_dict_legacy,
-            ],
+        vm.AgGrid(figure=dash_ag_grid(tips)),
+        vm.Graph(
+            figure=px.violin(tips, y="tip", x="day", color="day", box=True),
         ),
-        vm.Card(
-            id="card-id-1",
-            text="Click button to update me",
-        ),
-        vm.Card(id="card-id-2", text="Click button to update me"),
     ],
-    controls=[vm.Filter(column="species", selector=vm.RadioItems())],
 )
 
-dashboard = vm.Dashboard(pages=[page])
+page2 = vm.Page(
+    title="Grid - default - aggrid",
+    components=[
+        vm.AgGrid(figure=dash_ag_grid(tips)),
+        vm.Graph(
+            figure=px.violin(tips, y="tip", x="day", color="day", box=True),
+        ),
+    ],
+)
 
-if __name__ == "__main__":
-    Vizro().build(dashboard).run()
+page3 = vm.Page(
+    title="Flex - default - aggrid",
+    layout=vm.Flex(),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+
+page4 = vm.Page(
+    title="Flex - gap - aggrid",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+page5 = vm.Page(
+    title="Flex - row - aggrid",
+    layout=vm.Flex(direction="row"),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
+)
+
+page6 = vm.Page(
+    title="Flex - default - table",
+    layout=vm.Flex(),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+
+page7 = vm.Page(
+    title="Flex - gap - table",
+    layout=vm.Flex(gap="40px"),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+page8 = vm.Page(
+    title="Flex - row - table",
+    layout=vm.Flex(direction="row"),
+    components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
+)
+
+page9 = vm.Page(
+    title="FlexItem - dimension - table",
+    layout=vm.Flex(direction="row", wrap=True),
+    components=[vm.Table(figure=dash_data_table(tips, style_table={"width": "1000px"})) for i in range(3)],
+)
+
+page10 = vm.Page(
+    title="FlexItem - dimension - aggrid",
+    layout=vm.Flex(direction="row", wrap=True),
+    components=[vm.AgGrid(figure=dash_ag_grid(tips, style={"width": 1000})) for i in range(3)],
+)
+
+
+dashboard = vm.Dashboard(
+    pages=[
+        page1,
+        page2,
+        page3,
+        page4,
+        page5,
+        page6,
+        page7,
+        page8,
+        page9,
+        page10,
+    ],
+    title="Test out Flex/Grid",
+)
+
+
+Vizro().build(dashboard).run()
