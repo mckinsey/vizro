acb0409ac58b083c3b2bad61d947b91bbdd1d5e9
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2025-05-13 15:33:54 +0200

    [Bug] Fix that AgGrid/Table input IDs can't match any Model or input ID (#1165)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -5,79 +5,65 @@
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
+from vizro.tables import dash_ag_grid, dash_data_table
 
-gapminder_2007 = px.data.gapminder().query("year == 2007")
+df = px.data.iris()
 
-
-def tabs(title: str, tab_title: str = ""):
-    return vm.Tabs(
-        title=tab_title,
-        tabs=[
-            vm.Container(
-                title=f"{title} I",
-                components=[
-                    vm.Graph(
-                        figure=px.bar(
-                            gapminder_2007,
-                            title="Graph 1",
-                            x="continent",
-                            y="lifeExp",
-                            color="continent",
-                        ),
-                    ),
-                    vm.Graph(
-                        figure=px.box(
-                            gapminder_2007,
-                            title="Graph 2",
-                            x="continent",
-                            y="lifeExp",
-                            color="continent",
-                        ),
-                    ),
-                ],
-            ),
-            vm.Container(
-                title=f"{title} II",
-                components=[
-                    vm.Graph(
-                        figure=px.scatter(
-                            gapminder_2007,
-                            title="Graph 3",
-                            x="gdpPercap",
-                            y="lifeExp",
-                            size="pop",
-                            color="continent",
-                        ),
-                    ),
-                ],
-            ),
-        ],
-    )
-
-
-page_zero = vm.Page(title="Tabs - Default", components=[tabs("Tab - Single")])
-page_one = vm.Page(title="Tabs - Title", components=[tabs("Tab - Single", "Tab Title")])
-
-page_two = vm.Page(
-    title="Tabs inside Container",
+page1 = vm.Page(
+    title="LAYOUT_FLEX_WRAP_AND_AG_GRID",
     components=[
-        vm.Container(title="Container Title", components=[tabs("Tab Container", "Tab Title")], variant="filled")
+        vm.AgGrid(id=f"ag_grid_outer_id_{i}", figure=dash_ag_grid(df, id=f"ag_grid_inner_id_{i}")) for i in range(3)
     ],
+    controls=[vm.Filter(column="species")],
 )
 
-page_three = vm.Page(
-    title="Tabs inside Container - Collapsed",
+page2 = vm.Page(
+    title="LAYOUT_FLEX_GAP_AND_TABLE",
     components=[
-        vm.Container(
-            title="Container Title Collapsed",
-            components=[tabs("Tab Container Collapsed")],
-            variant="filled",
-            collapsed=True,
+        vm.Table(id=f"table_outer_id_{i}", figure=dash_data_table(df, id=f"table_inner_id_{i}")) for i in range(3)
+    ],
+    controls=[vm.Filter(column="species")],
+)
+
+page3 = vm.Page(
+    title="cross_id",
+    components=[
+        vm.Table(figure=dash_data_table(df, id="qwert")),
+        # TODO: See how configuration below raises an exception.
+        # vm.AgGrid(figure=dash_ag_grid(df, id="qwert")),
+    ],
+    controls=[vm.Filter(column="species")],
+)
+
+page_4_ag_grid_error = vm.Page(
+    title="AG_GRID_ERROR",
+    components=[
+        vm.AgGrid(
+            id="duplicate-ag-grid-id",
+            figure=dash_ag_grid(df, id="unique-ag-grid-id"),
+            # TODO: See how configurations below raise an exception
+            # figure=dash_ag_grid(df, id="duplicate-ag-grid-id"),
+            # figure=dash_ag_grid(df, id="duplicate-table-id")
         )
     ],
+    controls=[vm.Filter(column="species")],
 )
 
-dashboard = vm.Dashboard(pages=[page_zero, page_one, page_two, page_three])
+page_5_table_error = vm.Page(
+    title="TABLE_ERROR",
+    components=[
+        vm.Table(
+            id="duplicate-table-id",
+            figure=dash_data_table(df, id="unique-table-id"),
+            # TODO: See how configurations below raise an exception.
+            # figure=dash_data_table(df, id="duplicate-table-id")
+            # figure=dash_data_table(df, id="duplicate-ag-grid-id"),
+        )
+    ],
+    controls=[vm.Filter(column="species")],
+)
+
+dashboard = vm.Dashboard(pages=[page1, page2, page3, page_4_ag_grid_error, page_5_table_error])
 
 
 if __name__ == "__main__":
