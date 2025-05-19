aa8ed1157bc45666d9c0d4a4a0388ca631e04892
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-05-19 18:19:06 +0200

    failed test

--- vizro-core/tests/e2e/vizro/test_dom_elements/test_charts.py
+++ vizro-core/tests/e2e/vizro/test_dom_elements/test_charts.py
@@ -4,4 +4,4 @@ import e2e.vizro.constants as cnst
 def test_modebar(dash_br):
     """Check that modebar element exist for the chart."""
     dash_br.multiple_click(f"a[href='{cnst.FILTERS_PAGE_PATH}']", 1)
-    dash_br.wait_for_element(".modebar-container")
+    dash_br.wait_for_element(".modebar-containe")
