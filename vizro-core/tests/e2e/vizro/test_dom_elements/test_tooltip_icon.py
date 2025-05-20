38a86ec893d3b1c81e5992fb67858045e3665fd6
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-05-20 11:55:13 +0200

    no yaml dashboard

--- vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
+++ vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
@@ -10,12 +10,12 @@ from e2e.vizro.navigation import (
 @pytest.mark.parametrize(
     "dash_br_driver, icon, tooltip_text",
     [
-        (cnst.DROPDOWN_TOOLTIP_ICON, cnst.DROPDOWN_TOOLTIP_TEXT),
-        (cnst.RADIOITEMS_TOOLTIP_ICON, cnst.RADIOITEMS_TOOLTIP_TEXT),
-        (cnst.CHECKLIST_TOOLTIP_ICON, cnst.CHECKLIST_TOOLTIP_TEXT),
-        (cnst.SLIDER_TOOLTIP_ICON, cnst.SLIDER_TOOLTIP_TEXT),
-        (cnst.RANGESLIDER_TOOLTIP_ICON, cnst.RANGESLIDER_TOOLTIP_TEXT),
-        (cnst.DATEPICKER_TOOLTIP_ICON, cnst.DATEPICKER_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.DROPDOWN_TOOLTIP_ICON, cnst.DROPDOWN_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.RADIOITEMS_TOOLTIP_ICON, cnst.RADIOITEMS_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.CHECKLIST_TOOLTIP_ICON, cnst.CHECKLIST_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.SLIDER_TOOLTIP_ICON, cnst.SLIDER_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.RANGESLIDER_TOOLTIP_ICON, cnst.RANGESLIDER_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.DATEPICKER_TOOLTIP_ICON, cnst.DATEPICKER_TOOLTIP_TEXT),
     ],
     ids=["Dropdown", "RadioItems", "Checklist", "Slider", "RangeSlider", "Datepicker"],
     indirect=["dash_br_driver"],
@@ -38,9 +38,9 @@ def test_controls_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
 @pytest.mark.parametrize(
     "dash_br_driver, icon, tooltip_text",
     [
-        (cnst.PAGE_TOOLTIP_ICON, cnst.PAGE_TOOLTIP_TEXT),
-        (cnst.CONTAINER_TOOLTIP_ICON, cnst.CONTAINER_TOOLTIP_TEXT),
-        (cnst.GRAPH_TOOLTIP_ICON, cnst.GRAPH_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.PAGE_TOOLTIP_ICON, cnst.PAGE_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.CONTAINER_TOOLTIP_ICON, cnst.CONTAINER_TOOLTIP_TEXT),
+        ({"port": cnst.DEFAULT_PORT}, cnst.GRAPH_TOOLTIP_ICON, cnst.GRAPH_TOOLTIP_TEXT),
     ],
     ids=["Page", "Container", "Graph"],
     indirect=["dash_br_driver"],
@@ -59,8 +59,20 @@ def test_components_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
 @pytest.mark.parametrize(
     "dash_br_driver, accordion_name, page_name, icon, tooltip_text",
     [
-        (cnst.AG_GRID_ACCORDION, cnst.TABLE_PAGE, cnst.TABLE_TOOLTIP_ICON, cnst.TABLE_TOOLTIP_TEXT),
-        (cnst.AG_GRID_ACCORDION, cnst.TABLE_AG_GRID_PAGE, cnst.AG_GRID_TOOLTIP_ICON, cnst.AG_GRID_TOOLTIP_TEXT),
+        (
+            {"port": cnst.DEFAULT_PORT},
+            cnst.AG_GRID_ACCORDION,
+            cnst.TABLE_PAGE,
+            cnst.TABLE_TOOLTIP_ICON,
+            cnst.TABLE_TOOLTIP_TEXT,
+        ),
+        (
+            {"port": cnst.DEFAULT_PORT},
+            cnst.AG_GRID_ACCORDION,
+            cnst.TABLE_AG_GRID_PAGE,
+            cnst.AG_GRID_TOOLTIP_ICON,
+            cnst.AG_GRID_TOOLTIP_TEXT,
+        ),
     ],
     ids=[
         "Table",
