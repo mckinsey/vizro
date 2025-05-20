5f01347bdc74763af10648ca662c0d6ff8785ab0
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-05-20 11:48:59 +0200

    no yaml dashboard

--- vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
+++ vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
@@ -8,7 +8,7 @@ from e2e.vizro.navigation import (
 
 
 @pytest.mark.parametrize(
-    "icon, tooltip_text",
+    "dash_br_driver, icon, tooltip_text",
     [
         (cnst.DROPDOWN_TOOLTIP_ICON, cnst.DROPDOWN_TOOLTIP_TEXT),
         (cnst.RADIOITEMS_TOOLTIP_ICON, cnst.RADIOITEMS_TOOLTIP_TEXT),
@@ -18,44 +18,46 @@ from e2e.vizro.navigation import (
         (cnst.DATEPICKER_TOOLTIP_ICON, cnst.DATEPICKER_TOOLTIP_TEXT),
     ],
     ids=["Dropdown", "RadioItems", "Checklist", "Slider", "RangeSlider", "Datepicker"],
+    indirect=["dash_br_driver"],
 )
-def test_controls_tooltip_and_icon(dash_br, icon, tooltip_text):
-    accordion_select(dash_br, accordion_name=cnst.LAYOUT_ACCORDION)
-    page_select(dash_br, page_name=cnst.EXTRAS_PAGE)
+def test_controls_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
+    accordion_select(dash_br_driver, accordion_name=cnst.LAYOUT_ACCORDION)
+    page_select(dash_br_driver, page_name=cnst.EXTRAS_PAGE)
 
     # open datepicker calendar and close it to scroll to the end of the page
-    dash_br.multiple_click("button[class*='DatePickerInput']", 2)
-    dash_br.wait_for_no_elements('div[data-calendar="true"]')
+    dash_br_driver.multiple_click("button[class*='DatePickerInput']", 2)
+    dash_br_driver.wait_for_no_elements('div[data-calendar="true"]')
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
-    "icon, tooltip_text",
+    "dash_br_driver, icon, tooltip_text",
     [
         (cnst.PAGE_TOOLTIP_ICON, cnst.PAGE_TOOLTIP_TEXT),
         (cnst.CONTAINER_TOOLTIP_ICON, cnst.CONTAINER_TOOLTIP_TEXT),
         (cnst.GRAPH_TOOLTIP_ICON, cnst.GRAPH_TOOLTIP_TEXT),
     ],
     ids=["Page", "Container", "Graph"],
+    indirect=["dash_br_driver"],
 )
-def test_components_tooltip_and_icon(dash_br, icon, tooltip_text):
-    accordion_select(dash_br, accordion_name=cnst.LAYOUT_ACCORDION)
-    page_select(dash_br, page_name=cnst.EXTRAS_PAGE)
+def test_components_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
+    accordion_select(dash_br_driver, accordion_name=cnst.LAYOUT_ACCORDION)
+    page_select(dash_br_driver, page_name=cnst.EXTRAS_PAGE)
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
-    "accordion_name, page_name, icon, tooltip_text",
+    "dash_br_driver, accordion_name, page_name, icon, tooltip_text",
     [
         (cnst.AG_GRID_ACCORDION, cnst.TABLE_PAGE, cnst.TABLE_TOOLTIP_ICON, cnst.TABLE_TOOLTIP_TEXT),
         (cnst.AG_GRID_ACCORDION, cnst.TABLE_AG_GRID_PAGE, cnst.AG_GRID_TOOLTIP_ICON, cnst.AG_GRID_TOOLTIP_TEXT),
@@ -64,16 +66,17 @@ def test_components_tooltip_and_icon(dash_br, icon, tooltip_text):
         "Table",
         "AgGrid",
     ],
+    indirect=["dash_br_driver"],
 )
-def test_components_tooltip_and_icon_tables(dash_br, accordion_name, page_name, icon, tooltip_text):
-    accordion_select(dash_br, accordion_name=accordion_name)
-    page_select(dash_br, page_name=page_name, graph_check=False)
+def test_components_tooltip_and_icon_tables(dash_br_driver, accordion_name, page_name, icon, tooltip_text):
+    accordion_select(dash_br_driver, accordion_name=accordion_name)
+    page_select(dash_br_driver, page_name=page_name, graph_check=False)
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
