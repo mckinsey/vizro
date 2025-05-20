609877d74cf65cf84e7a39c693897e61633fe43c
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-05-20 12:05:46 +0200

    screenshot tests report

--- vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
+++ vizro-core/tests/e2e/vizro/test_dom_elements/test_tooltip_icon.py
@@ -8,87 +8,72 @@ from e2e.vizro.navigation import (
 
 
 @pytest.mark.parametrize(
-    "dash_br_driver, icon, tooltip_text",
+    "icon, tooltip_text",
     [
-        ({"port": cnst.DEFAULT_PORT}, cnst.DROPDOWN_TOOLTIP_ICON, cnst.DROPDOWN_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.RADIOITEMS_TOOLTIP_ICON, cnst.RADIOITEMS_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.CHECKLIST_TOOLTIP_ICON, cnst.CHECKLIST_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.SLIDER_TOOLTIP_ICON, cnst.SLIDER_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.RANGESLIDER_TOOLTIP_ICON, cnst.RANGESLIDER_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.DATEPICKER_TOOLTIP_ICON, cnst.DATEPICKER_TOOLTIP_TEXT),
+        (cnst.DROPDOWN_TOOLTIP_ICON, cnst.DROPDOWN_TOOLTIP_TEXT),
+        (cnst.RADIOITEMS_TOOLTIP_ICON, cnst.RADIOITEMS_TOOLTIP_TEXT),
+        (cnst.CHECKLIST_TOOLTIP_ICON, cnst.CHECKLIST_TOOLTIP_TEXT),
+        (cnst.SLIDER_TOOLTIP_ICON, cnst.SLIDER_TOOLTIP_TEXT),
+        (cnst.RANGESLIDER_TOOLTIP_ICON, cnst.RANGESLIDER_TOOLTIP_TEXT),
+        (cnst.DATEPICKER_TOOLTIP_ICON, cnst.DATEPICKER_TOOLTIP_TEXT),
     ],
     ids=["Dropdown", "RadioItems", "Checklist", "Slider", "RangeSlider", "Datepicker"],
-    indirect=["dash_br_driver"],
 )
-def test_controls_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
-    accordion_select(dash_br_driver, accordion_name=cnst.LAYOUT_ACCORDION)
-    page_select(dash_br_driver, page_name=cnst.EXTRAS_PAGE)
+def test_controls_tooltip_and_icon(dash_br, icon, tooltip_text):
+    accordion_select(dash_br, accordion_name=cnst.LAYOUT_ACCORDION)
+    page_select(dash_br, page_name=cnst.EXTRAS_PAGE)
 
     # open datepicker calendar and close it to scroll to the end of the page
-    dash_br_driver.multiple_click("button[class*='DatePickerInput']", 2)
-    dash_br_driver.wait_for_no_elements('div[data-calendar="true"]')
+    dash_br.multiple_click("button[class*='DatePickerInput']", 2)
+    dash_br.wait_for_no_elements('div[data-calendar="true"]')
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
-    "dash_br_driver, icon, tooltip_text",
+    "icon, tooltip_text",
     [
-        ({"port": cnst.DEFAULT_PORT}, cnst.PAGE_TOOLTIP_ICON, cnst.PAGE_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.CONTAINER_TOOLTIP_ICON, cnst.CONTAINER_TOOLTIP_TEXT),
-        ({"port": cnst.DEFAULT_PORT}, cnst.GRAPH_TOOLTIP_ICON, cnst.GRAPH_TOOLTIP_TEXT),
+        (cnst.PAGE_TOOLTIP_ICON, cnst.PAGE_TOOLTIP_TEXT),
+        (cnst.CONTAINER_TOOLTIP_ICON, cnst.CONTAINER_TOOLTIP_TEXT),
+        (cnst.GRAPH_TOOLTIP_ICON, cnst.GRAPH_TOOLTIP_TEXT),
     ],
     ids=["Page", "Container", "Graph"],
-    indirect=["dash_br_driver"],
 )
-def test_components_tooltip_and_icon(dash_br_driver, icon, tooltip_text):
-    accordion_select(dash_br_driver, accordion_name=cnst.LAYOUT_ACCORDION)
-    page_select(dash_br_driver, page_name=cnst.EXTRAS_PAGE)
+def test_components_tooltip_and_icon(dash_br, icon, tooltip_text):
+    accordion_select(dash_br, accordion_name=cnst.LAYOUT_ACCORDION)
+    page_select(dash_br, page_name=cnst.EXTRAS_PAGE)
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
-    "dash_br_driver, accordion_name, page_name, icon, tooltip_text",
+    "accordion_name, page_name, icon, tooltip_text",
     [
-        (
-            {"port": cnst.DEFAULT_PORT},
-            cnst.AG_GRID_ACCORDION,
-            cnst.TABLE_PAGE,
-            cnst.TABLE_TOOLTIP_ICON,
-            cnst.TABLE_TOOLTIP_TEXT,
-        ),
-        (
-            {"port": cnst.DEFAULT_PORT},
-            cnst.AG_GRID_ACCORDION,
-            cnst.TABLE_AG_GRID_PAGE,
-            cnst.AG_GRID_TOOLTIP_ICON,
-            cnst.AG_GRID_TOOLTIP_TEXT,
-        ),
+        (cnst.AG_GRID_ACCORDION, cnst.TABLE_PAGE, cnst.TABLE_TOOLTIP_ICON, cnst.TABLE_TOOLTIP_TEXT),
+        (cnst.AG_GRID_ACCORDION, cnst.TABLE_AG_GRID_PAGE, cnst.AG_GRID_TOOLTIP_ICON, cnst.AG_GRID_TOOLTIP_TEXT),
     ],
     ids=[
         "Table",
         "AgGrid",
     ],
-    indirect=["dash_br_driver"],
 )
-def test_components_tooltip_and_icon_tables(dash_br_driver, accordion_name, page_name, icon, tooltip_text):
-    accordion_select(dash_br_driver, accordion_name=accordion_name)
-    page_select(dash_br_driver, page_name=page_name, graph_check=False)
+def test_components_tooltip_and_icon_tables(dash_br, accordion_name, page_name, icon, tooltip_text):
+    accordion_select(dash_br, accordion_name=accordion_name)
+    page_select(dash_br, page_name=page_name, graph_check=False)
 
     # hover over dropdown icon and wait for the tooltip appear
     hover_over_element_by_xpath_selenium(
-        dash_br_driver, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
+        dash_br, f"//*[@class='material-symbols-outlined tooltip-icon'][text()='{icon}']"
     )
-    dash_br_driver.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
+    dash_br.wait_for_text_to_equal(".tooltip-inner p", tooltip_text)
 
 
 @pytest.mark.parametrize(
