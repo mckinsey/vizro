389c1a7894812eeedec90461d30b41dcbba71425
Vladimir <vladimir_nikolic@external.mckinsey.com>
2025-05-26 13:35:29 +0200

    [Refactor] Remove unused pages and streamline the dashboard setup

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -39,58 +39,9 @@ page = vm.Page(
             ),
         ),
     ],
-    controls=[vm.Filter(column="day"), vm.Filter(column="time", selector=vm.Checklist()), vm.Filter(column="size")],
-)
-
-
-page24 = vm.Page(
-    title="Page with tabs and content- grid",
-    components=[
-        vm.Tabs(
-            tabs=[
-                vm.Container(
-                    title="tab1",
-                    layout=vm.Flex(direction="row"),
-                    components=[
-                        vm.Card(text="This is card inside first tab!"),
-                        vm.Graph(figure=px.histogram(tips, x="tip")),
-                    ],
-                ),
-                vm.Container(
-                    title="tab2",
-                    layout=vm.Flex(direction="row"),
-                    components=[
-                        vm.Card(text="This is card inside second tab!"),
-                        vm.Graph(figure=px.bar(tips, y="tip", x="day")),
-                    ],
-                ),
-            ]
-        ),
-        vm.Card(text="This is card below the tabs!"),
-        vm.Graph(figure=px.violin(tips, y="tip", x="day", color="day", box=True)),
-    ],
-)
-
-
-page25 = vm.Page(
-    title="FlexItem - dimension - graphs",
-    layout=vm.Flex(direction="row", wrap=True),
-    components=[vm.Graph(figure=px.violin(tips, y="tip", x="day", color="day", box=True, width=300)) for i in range(6)],
-)
-
-page26 = vm.Page(
-    title="FlexItem - dimension - tables",
-    layout=vm.Flex(direction="row", wrap=True),
-    components=[vm.Table(figure=dash_data_table(tips, style_table={"width": "1000px"})) for i in range(3)],
-)
-
-page27 = vm.Page(
-    title="FlexItem - dimension - aggrid",
-    layout=vm.Flex(direction="row", wrap=True),
-    components=[vm.AgGrid(figure=dash_ag_grid(tips, style={"width": 1000})) for i in range(3)],
 )
 
 dashboard = vm.Dashboard(pages=[page])
 
-
-Vizro().build(dashboard).run()
+if __name__ == "__main__":
+    Vizro().build(dashboard).run()
