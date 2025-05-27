35ca3e8e1bad0c7c2a1a9223065beb34af2b827b
Antony Milne <antony.milne@quantumblack.com>
2025-05-27 17:34:31 +0100

    Even more minimal example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -5,64 +5,41 @@ from dash import html, dcc, callback, Input, Output, Dash
 
 
 common = [
-    html.H1(id="dashboard_title", children="Dashboard"),
+    html.H1(children="Dashboard"),
     html.Div(dcc.Link("Homepage", href="/")),
     html.Div(dcc.Link("Another page", href="/another-page")),
 ]
 
 
-def _page_build(content):
-    page_build_obj = html.Div(
+def homepage(**kwargs):
+    return html.H2("Homepage")
+
+
+def another_page(radio_items_id_value=1):
+    return html.Div(
         [
-            *common,
-            html.P(datetime.datetime.now()),
-            *content,
+            html.H2("Another page"),
+            dcc.RadioItems(
+                id="radio_items_id",
+                options=[1, 2, 3],
+                value=int(radio_items_id_value),
+                persistence=True,
+                persistence_type="session",
+            ),
         ]
     )
-    return page_build_obj
-
-
-def homepage(**kwargs):
-    return _page_build([
-        html.H2("Homepage"),
-        dbc.Button(
-            id="button_id",
-            children="Go to Another page (and set value=3)",
-            href="/another-page?radio_items_id_value=3",
-        ),
-    ])
-
-
-def another_page(radio_items_id_value=1, **kwargs):
-    return _page_build([
-        html.H2("Another page"),
-        dcc.RadioItems(
-            id="radio_items_id",
-            options=[1, 2, 3],
-            value=int(radio_items_id_value),
-            persistence=True,
-            persistence_type="session",
-        )
-    ])
 
 
 app = Dash(
     use_pages=True,
     pages_folder="",
-    suppress_callback_exceptions=True,
-    # routing_callback_inputs={
-    #     "radio_items_id_value": Input("radio_items_id", "value"),
-    # },
+    # suppress_callback_exceptions=True,
 )
 
 dash.register_page("/", layout=homepage)
 dash.register_page("another-page", layout=another_page)
 
-app.layout = html.Div([
-    dcc.Store("global_store"),
-    dash.page_container,
-    # dcc.RadioItems(id="radio_items_id", options=[], style={"display": "none"})
-])
+app.layout = [*common, dash.page_container]
 
 if __name__ == "__main__":
     app.run(debug=True, dev_tools_hot_reload=False)
