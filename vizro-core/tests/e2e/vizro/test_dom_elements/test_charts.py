0a9fc8b4280cbc125cc81bb13d6fb4ee8b252e6c
Alexey Snigir <35569332+l0uden@users.noreply.github.com>
2025-06-03 09:38:58 +0200

    [QA] Graph extra param test (#1216)

--- vizro-core/tests/e2e/vizro/test_dom_elements/test_charts.py
+++ vizro-core/tests/e2e/vizro/test_dom_elements/test_charts.py
@@ -1,7 +1,15 @@
 import e2e.vizro.constants as cnst
+from e2e.vizro.waiters import graph_load_waiter
 
 
 def test_modebar(dash_br):
     """Check that modebar element exist for the chart."""
     dash_br.multiple_click(f"a[href='{cnst.FILTERS_PAGE_PATH}']", 1)
-    dash_br.wait_for_element(".modebar-container")
+    dash_br.wait_for_element(f"#{cnst.SCATTER_GRAPH_ID} .modebar-container div[id^='modebar']")
+
+
+def test_modebar_false(dash_br):
+    """Check that modebar element disabled for the chart."""
+    dash_br.multiple_click(f"a[href='{cnst.FILTERS_PAGE_PATH}']", 1)
+    graph_load_waiter(dash_br, cnst.BOX_GRAPH_ID)
+    dash_br.wait_for_no_elements(f'div[id="{cnst.BOX_GRAPH_ID}"] .modebar-container div[id^="modebar"]')
