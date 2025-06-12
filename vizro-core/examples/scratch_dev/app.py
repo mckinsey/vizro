8d346c6e31728818f67af4843fd66d2cda823b68
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-06-12 09:21:30 +0200

    Update app-config

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -4,6 +4,7 @@ import vizro.models as vm
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
 from typing import Literal
+from vizro.tables import dash_ag_grid
 
 gapminder_2007 = px.data.gapminder().query("year == 2007")
 
@@ -28,6 +29,7 @@ class CustomDashboard(vm.Dashboard):
                                 vm.Dropdown(options=["A", "B", "C"], title="Region *", multi=False).build(),
                                 vm.Dropdown(options=["A", "B", "C"], title="Status *", multi=False).build(),
                             ],
+                            style={"display": "flex", "flex-direction": "column", "gap": "1rem"},
                         ),
                         dbc.ModalFooter(
                             [
@@ -38,14 +40,13 @@ class CustomDashboard(vm.Dashboard):
                     id="modal",
                     is_open=False,
                 ),
-            ]
+            ],
         )
 
         super_build_obj = super()._make_page_layout(*args, **kwargs)
         # We access the container with id="settings", where the theme switch is placed and add the H4.
         # You can see what's inside the settings.children container here: https://github.com/mckinsey/vizro/blob/main/vizro-core/src/vizro/models/_dashboard.py
         super_build_obj["settings"].children = [
-            dbc.Button("↓ Export", color="secondary"),
             modal_with_button,
             super_build_obj["settings"].children,
         ]
@@ -55,16 +56,9 @@ class CustomDashboard(vm.Dashboard):
 page_1 = vm.Page(
     title="Page with button",
     components=[
-        vm.Graph(
-            title="Graph 1",
-            figure=px.bar(
-                gapminder_2007,
-                x="continent",
-                y="lifeExp",
-                color="continent",
-            ),
-        )
+        vm.AgGrid(figure=dash_ag_grid(gapminder_2007)),
     ],
+    controls=[vm.Filter(column="continent"), vm.Filter(column="country")],
 )
 
 dashboard = CustomDashboard(
