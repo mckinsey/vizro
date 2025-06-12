fbf10b8a4ddadc92ea79a77119f51e161b3f174e
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-06-12 11:55:20 +0200

    Add div selector

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -4,7 +4,7 @@ import vizro.models as vm
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
 from typing import Literal
-from vizro.tables import dash_ag_grid
+from vizro.tables import dash_ag_grid, dash_data_table
 from vizro.figures import kpi_card_reference, kpi_card
 import pandas as pd
 
@@ -63,7 +63,7 @@ class CustomDashboard(vm.Dashboard):
 page_1 = vm.Page(
     title="Page with modal",
     components=[
-        vm.AgGrid(figure=dash_ag_grid(gapminder_2007)),
+        vm.Table(figure=dash_data_table(gapminder_2007)),
     ],
     controls=[vm.Filter(column="continent"), vm.Filter(column="country")],
 )
