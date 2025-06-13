dddac0cecb163e9a62a7e04f2493ace69505d03e
lingyielia <lingyi_zhang@mckinsey.com>
2025-06-13 13:51:04 -0400

    tidy

--- vizro-ai/src/vizro_ai/components/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Vizro-AI components module.
-
-This module contains reusable processors for chat components.
-"""
-
-from vizro_ai.models._processors import ChatProcessor, EchoProcessor, OpenAIProcessor
-
-__all__ = ["ChatProcessor", "EchoProcessor", "OpenAIProcessor"] 
