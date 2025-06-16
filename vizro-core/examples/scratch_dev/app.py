2f0950f59d890d3f3e8806634dc5aa310d719cd8
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-06-16 16:29:52 +0200

    Add scratch dev app to test

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -2,52 +2,64 @@ from vizro import Vizro
 import vizro.plotly.express as px
 import vizro.models as vm
 import pandas as pd
-from vizro.tables import dash_ag_grid
+from vizro.tables import dash_ag_grid, dash_data_table
 
 gapminder_2007 = px.data.gapminder().query("year == 2007")
 
-# Create a list of fake long words
-long_words = [
-    "Client Discussion",
-    "Client Discussion Engagement",
-    "Completed Engagement",
-    "Confirmed Engagement in Progress",
-    "Inactive/on hold",
-]
-
-# Create the DataFrame
-df = pd.DataFrame({"id": range(1, 6), "category": ["A", "B", "A", "C", "B"], "long_words": long_words})
-
-
 page_1 = vm.Page(
-    title="Test page",
+    title="Grid AgGird",
+    layout=vm.Grid(grid=[[0, 1]]),
     components=[
-        vm.Container(
-            components=[vm.AgGrid(figure=dash_ag_grid(df))],
-            variant="outlined",
-            controls=[
-                vm.Filter(column="long_words"),
-                vm.Filter(
-                    column="category",
-                    selector=vm.Dropdown(
-                        options=[
-                            {"label": "a", "value": "A"},
-                            {"label": "b", "value": "B"},
-                            {"label": "c", "value": "C"},
-                        ]
-                    ),
-                ),
-                vm.Filter(column="id"),
-            ],
-        ),
+        vm.AgGrid(figure=dash_ag_grid(gapminder_2007)),
+        vm.Graph(figure=px.scatter(gapminder_2007, x="continent", y="lifeExp")),
     ],
-    controls=[
-        vm.Filter(column="long_words"),
+    controls=[vm.Filter(column="country")],
+)
+
+page_2 = vm.Page(
+    title="Grid Table",
+    layout=vm.Grid(grid=[[0, 1]]),
+    components=[
+        vm.Table(figure=dash_data_table(gapminder_2007)),
+        vm.Graph(figure=px.scatter(gapminder_2007, x="continent", y="lifeExp")),
+    ],
+    controls=[vm.Filter(column="country")],
+)
+
+page_3 = vm.Page(
+    title="Flex AgGird",
+    layout=vm.Flex(direction="row"),
+    components=[
+        vm.AgGrid(figure=dash_ag_grid(gapminder_2007)),
+        vm.Graph(figure=px.scatter(gapminder_2007, x="continent", y="lifeExp")),
+    ],
+    controls=[vm.Filter(column="country")],
+)
+
+page_4 = vm.Page(
+    title="Flex Table",
+    layout=vm.Flex(direction="row"),
+    components=[
+        vm.Table(figure=dash_data_table(gapminder_2007)),
+        vm.Graph(figure=px.scatter(gapminder_2007, x="continent", y="lifeExp")),
+    ],
+    controls=[vm.Filter(column="country")],
+)
+
+page_5 = vm.Page(
+    title="Page",
+    layout=vm.Flex(),
+    components=[
+        vm.Card(text="""BLABLA"""),
+        vm.Container(
+            variant="outlined",
+            title="Container with AgGrid",
+            components=[vm.AgGrid(figure=dash_ag_grid(gapminder_2007))],
+        ),
     ],
 )
 
-dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1])
+dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1, page_2, page_3, page_4, page_5])
 
 if __name__ == "__main__":
-    app = Vizro().build(dashboard)
-    app.run()
+    Vizro().build(dashboard).run()
