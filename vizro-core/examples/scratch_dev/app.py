7d87a2f5b7d4a726dd17bfc37cc1dbbb091ffae8
Vladimir <vladimir_nikolic@external.mckinsey.com>
2025-06-17 16:21:15 +0200

    [Add] Include second page in the dashboard with filters for long words, category, and id

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -46,7 +46,35 @@ page_1 = vm.Page(
     ],
 )
 
-dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1])
+
+page_2 = vm.Page(
+    title="Test page 2",
+    components=[
+        vm.Container(
+            components=[vm.AgGrid(figure=dash_ag_grid(df))],
+            variant="outlined",
+            controls=[
+                vm.Filter(column="long_words"),
+                vm.Filter(
+                    column="category",
+                    selector=vm.Dropdown(
+                        options=[
+                            {"label": "a", "value": "A"},
+                            {"label": "b", "value": "B"},
+                            {"label": "c", "value": "C"},
+                        ]
+                    ),
+                ),
+                vm.Filter(column="id"),
+            ],
+        ),
+    ],
+)
+
+dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1, page_2], 
+                          navigation=vm.Navigation(
+        pages={"Group A": ["Test page"], "Group B": ["Test page 2"]}, nav_selector=vm.NavBar()
+    ),)
 
 if __name__ == "__main__":
     app = Vizro().build(dashboard)
