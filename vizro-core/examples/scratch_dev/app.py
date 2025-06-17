9c13d7094edc8a93b0a203851cfef189ea31b803
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-06-17 14:26:19 +0200

    slider title fix

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -2,20 +2,33 @@ import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
 
-gapminder = px.data.gapminder()
+iris = px.data.iris()
 
 page = vm.Page(
     title="Containers with controls",
     components=[
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab with iris data",
+                    components=[vm.Graph(figure=px.bar(iris, x="species", y="sepal_length"))],
+                    controls=[
+                        vm.Filter(column="petal_length", selector=vm.Slider()),
+                        vm.Filter(column="sepal_length", selector=vm.RangeSlider()),
+                    ],
+                ),
+            ]
+        ),
         vm.Container(
-            title="Container with gapminder data",
-            components=[vm.Graph(id="bar_chart", figure=px.bar(gapminder, x="country", y="gdpPercap"))],
+            title="Container with iris data",
+            components=[vm.Graph(figure=px.bar(iris, x="species", y="sepal_length"))],
             controls=[
-                vm.Filter(column="continent", selector=vm.RadioItems()),
-                vm.Filter(column="year", selector=vm.RangeSlider(title="This is a longer title and it goes and goes")),
+                vm.Filter(column="petal_length", selector=vm.Slider()),
+                vm.Filter(column="sepal_length", selector=vm.RangeSlider()),
             ],
         ),
     ],
+    controls=[vm.Filter(column="sepal_length", selector=vm.RangeSlider())],
 )
 
 dashboard = vm.Dashboard(pages=[page])
