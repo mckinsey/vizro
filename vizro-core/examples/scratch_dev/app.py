dd7b814b70c6103808e89b239876baaabeb0b8ba
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-06-17 13:36:12 +0200

    scratch dev app

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -11,43 +11,53 @@ df = px.data.gapminder()
 
 first_page = vm.Page(
     title="First Page",
-    layout=vm.Layout(grid=[[0, 0], [1, 1], [1, 1], [1, 1]]),
+    # layout=vm.Layout(grid=[[0, 0], [1, 1], [1, 1], [1, 1]]),
     components=[
-        vm.Card(
-            text="""
-                # First dashboard page
-                This pages shows the inclusion of markdown text in a page and how components
-                can be structured using Layout.
-            """,
-        ),
-        vm.AgGrid(
-            figure=dash_ag_grid(data_frame=df, dashGridOptions={"pagination": True}),
-            title="Gapminder Data Insights",
-            header="""#### An Interactive Exploration of Global Health, Wealth, and Population""",
-            footer="""SOURCE: **Plotly gapminder data set, 2024**""",
-        ),
+        vm.Container(
+            title="Container with controls",
+            layout=vm.Layout(grid=[[0, 0], [1, 1], [1, 1], [1, 1]]),
+            variant="outlined",
+            components=[
+                vm.Card(
+                    text="""
+                        # First dashboard page
+                        This pages shows the inclusion of markdown text in a page and how components
+                        can be structured using Layout.
+                    """,
+                ),
+                vm.AgGrid(
+                    figure=dash_ag_grid(data_frame=df, dashGridOptions={"pagination": True}),
+                    title="Gapminder Data Insights",
+                    header="""#### An Interactive Exploration of Global Health, Wealth, and Population""",
+                    footer="""SOURCE: **Plotly gapminder data set, 2024**""",
+                ),
+            ],
+            controls=[
+                vm.Filter(column="continent", selector=vm.Checklist(title="Continent (checklist)"))
+            ]
+        )
     ],
     controls=[
-        vm.Filter(column="continent"),
+        # vm.Filter(column="continent"),
         vm.Filter(column="continent", selector=vm.Checklist()),
-        vm.Filter(column="country"),
-        vm.Filter(column="country", selector=vm.Checklist()),
-        vm.Filter(column="country", selector=vm.Dropdown(multi=False)),
-        vm.Filter(
-            column="continent",
-            selector=vm.Dropdown(
-                options=[
-                    {"label": "EUROPE", "value": "Europe"},
-                    {"label": "AFRICA", "value": "Africa"},
-                    {"label": "ASIA", "value": "Asia"},
-                    # {"label": "AMERICAS", "value": "Americas"},
-                    # {"label": "OCEANIA", "value": "Oceania"},
-                ],
-                # value=["Europe", "Africa", "Asia", "Americas", "Oceania"],
-                value="Asia",
-                multi=False,
-            ),
-        ),
+        # vm.Filter(column="country"),
+        # vm.Filter(column="country", selector=vm.Checklist()),
+        # vm.Filter(column="country", selector=vm.Dropdown(multi=False)),
+        # vm.Filter(
+        #     column="continent",
+        #     selector=vm.Dropdown(
+        #         options=[
+        #             {"label": "EUROPE", "value": "Europe"},
+        #             {"label": "AFRICA", "value": "Africa"},
+        #             {"label": "ASIA", "value": "Asia"},
+        #             # {"label": "AMERICAS", "value": "Americas"},
+        #             # {"label": "OCEANIA", "value": "Oceania"},
+        #         ],
+        #         # value=["Europe", "Africa", "Asia", "Americas", "Oceania"],
+        #         value="Asia",
+        #         multi=False,
+        #     ),
+        # ),
     ],
 )
 
