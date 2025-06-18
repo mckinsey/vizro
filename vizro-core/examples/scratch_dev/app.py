56409651b48b5afcd91e2076e1ce40943ade291d
vladimir-mck <106236933+vladimir-mck@users.noreply.github.com>
2025-06-18 12:17:24 +0200

    [Fix] Use `min-height: 100%` to fix Plotly graph rendering in tab panel (#1239)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: petar-qb <petar_pejovic@external.mckinsey.com>

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,59 +1,176 @@
-import numpy as np
-
-from vizro import Vizro
 import vizro.models as vm
-import pandas as pd
-from vizro.tables import dash_ag_grid
-import string
+from typing import Literal
+from vizro.managers import data_manager
+from vizro import Vizro
+
+import vizro.plotly.express as px
 
 
-N = len(string.ascii_letters)
-df = pd.DataFrame(
-    {
-        "category 1": np.random.choice(list("abc"), N),
-        "category 2": np.random.choice(list("ABC"), N),
-        "long_words": [" ".join(list(string.ascii_letters[: n + 1])) for n in range(N)],
-        "numbers": np.random.randint(N, size=N),
-    }
-)
+def load_data_function(number_of_data_points: int = 50):
+    return px.data.iris().head(number_of_data_points)
 
 
-def dynamic_data(n=10):
-    return df.loc[:n]
+data_manager["data_key"] = load_data_function
 
 
-def make_controls():
-    return [
-        vm.Filter(column="long_words"),
-        vm.Filter(
-            column="category 1",
-            selector=vm.Checklist(),
-        ),
-        vm.Filter(column="category 2", selector=vm.RadioItems()),
-        vm.Filter(column="numbers"),
-    ]
+gapminder_2007 = px.data.gapminder().query("year == 2007")
+tips = px.data.tips()
+df = px.data.iris()
 
 
-page_1 = vm.Page(
-    title="Test page",
+class CustomParameter(vm.Parameter):
+    """Custom Parameter used to overcome Vizro exception when targeting figures outside its vm.Container."""
+
+    type: Literal["custom_parameter"] = "custom_parameter"
+
+    def pre_build(self):
+        self._check_numerical_and_temporal_selectors_values()
+        self._check_categorical_selectors_options()
+        self._set_selector_title()
+        self._set_actions()
+
+
+# TODO-Comment: The following line is needed to enable using CustomParameter in the Container components.
+vm.Container.add_type("components", CustomParameter)
+
+page1 = vm.Page(
+    title="Tabs",
+    description="""Single page app description.""",
+    layout=vm.Flex(),
     components=[
         vm.Container(
-            components=[vm.AgGrid(id="grid", figure=dash_ag_grid(dynamic_data))],
-            controls=make_controls(),
+            title="Global parameters and filters",
+            description="Set global parameters and filters that apply to all tabs.",
+            collapsed=False,
+            components=[
+                CustomParameter(
+                    targets=[
+                        "page_1_graph_1.data_frame.number_of_data_points",
+                        "page_2_graph_1.data_frame.number_of_data_points",
+                    ],
+                    selector=vm.Slider(min=1, max=150, value=50),
+                ),
+            ],
+            controls=[],
         ),
-    ],
-    controls=[
-        vm.Parameter(
-            targets=["grid.data_frame.n"],
-            selector=vm.Slider(
-                title="Change me to see bug", min=0, max=N, step=1, marks={0: "0", N // 2: str(N // 2), N: str(N)}
-            ),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Page 1 title",
+                    components=[
+                        vm.Graph(
+                            id="page_1_graph_1",
+                            figure=px.scatter("data_key", x="sepal_length", y="petal_width", color="species"),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Page 2 title",
+                    components=[
+                        vm.Graph(
+                            id="page_2_graph_1",
+                            figure=px.scatter(
+                                "data_key", x="sepal_length", y="petal_width", color="species", height=600
+                            ),
+                        ),
+                    ],
+                ),
+            ]
         ),
-        *make_controls(),
     ],
 )
 
-dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1])
+
+page2 = vm.Page(
+    title="Tabs v2",
+    layout=vm.Flex(),
+    components=[
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab1",
+                    components=[
+                        vm.Graph(
+                            title="Graph 1",
+                            figure=px.bar(
+                                gapminder_2007,
+                                x="continent",
+                                y="lifeExp",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab2",
+                    components=[
+                        vm.Graph(
+                            title="Graph 2",
+                            figure=px.scatter(
+                                gapminder_2007,
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab3",
+                    layout=vm.Flex(),
+                    components=[vm.Card(text="First card!"), vm.Card(text="Second card!"), vm.Card(text="Third card!")],
+                ),
+            ]
+        )
+    ],
+)
+
+page3 = vm.Page(
+    title="Flex - default - graphs",
+    layout=vm.Flex(),
+    components=[vm.Graph(figure=px.violin(tips, y="tip", x="day", color="day", box=True)) for i in range(6)],
+)
+
+
+page4 = vm.Page(
+    title="Tabs v3",
+    components=[
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Tab1",
+                    layout=vm.Flex(direction="row"),
+                    components=[
+                        vm.Graph(
+                            title="Graph 1",
+                            figure=px.bar(gapminder_2007, x="continent", y="lifeExp", color="continent", height=400),
+                        ),
+                        vm.Card(text="Card 1"),
+                    ],
+                ),
+                vm.Container(
+                    title="Tab2",
+                    components=[
+                        vm.Graph(
+                            title="Graph 2",
+                            figure=px.scatter(
+                                gapminder_2007,
+                                x="gdpPercap",
+                                y="lifeExp",
+                                size="pop",
+                                color="continent",
+                            ),
+                        ),
+                    ],
+                ),
+            ]
+        )
+    ],
+)
+
+
+dashboard = vm.Dashboard(title="Test dashboard", pages=[page1, page2, page3, page4])
 
 if __name__ == "__main__":
     app = Vizro().build(dashboard)
