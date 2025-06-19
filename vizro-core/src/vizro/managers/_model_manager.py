e3711004119aaca85ee2681ab5d2c9e2116285c9
Antony Milne <antony.milne@quantumblack.com>
2025-06-19 09:41:22 +0100

    Turn into TypedTree

--- vizro-core/src/vizro/managers/_model_manager.py
+++ vizro-core/src/vizro/managers/_model_manager.py
@@ -7,7 +7,7 @@ import uuid
 from collections.abc import Collection, Generator, Iterable, Mapping
 from typing import TYPE_CHECKING, Optional, TypeVar, Union, cast, Any
 
-from nutree import Tree
+from nutree import Tree, TypedTree
 
 from vizro.managers._managers_utils import _state_modifier
 
@@ -38,10 +38,8 @@ class ModelManager:
     def __init__(self):
         self.__models: dict[ModelID, VizroBaseModel] = {}
         # AM rough notes: need to handle CapturedCallable in future too? Which doesn't have .id
-        # AM rough notes: forward_attrs=True looks nice for simplifying syntax. TypedTree also maybe useful so you
-        # can set kind=<field_name> if we want field name there (only alternative would be node for each field name
-        # also).
-        self.__dashboard_tree: Tree = Tree(calc_data_id=lambda tree, data: data.id)
+        # AM rough notes: forward_attrs=True looks nice for simplifying syntax.
+        self.__dashboard_tree = TypedTree(calc_data_id=lambda tree, data: data.id)
         self._frozen_state = False
 
     # AM rough notes: probably better to just instantiate model manager in Vizro and pass dashboard in init rather
@@ -51,7 +49,7 @@ class ModelManager:
         d = self.__dashboard_tree
         # AM rough notes: importantly the nodes are just pointers to real objects so not duplicated in memory.
         # assert d.first_child().data.pages[0] is d["Test page"].data
-        d.print(title=False, repr=lambda node: f"{node.data.__class__.__name__}(id={node.data.id})")
+        d.print(title=False, repr=lambda node: f"{node.kind}: {node.data.__class__.__name__}(id={node.data.id})")
         # repr = {node.data!r} is maybe the default and shows all fields too
         d.to_mermaid_flowchart(
             "graph.md",
@@ -82,6 +80,7 @@ class ModelManager:
         # But there's unexpected behaviour/maybe a bug in nutree where you set Tree(calc_data_id) upfront.
         # Probably easy to fix.
         # AM rough notes: problem here probably caused by pre-build order in filters.
+        print(model_id)
         return self.__dashboard_tree.find_first(data_id=model_id).data
         # Do we need to return deepcopy(self.__models[model_id]) to avoid adjusting element by accident?
         # return self.__models[model_id]
@@ -133,11 +132,10 @@ class ModelManager:
             for single_model in model:
                 yield from self.__get_model_children(single_model)
 
-    def __populate_tree(self, model: Model, parent=None):
-        """Iterates through children of `model`."""
+    def __populate_tree(self, model: Model, parent=None, field_name=None):
+        """Iterates through children of `model` with depth-first pre-order traversal."""
         # AM rough notes: this is basically copied and pasted from __get_model_children and then modified to populate
         # dashboard tree instead of yielding.
-        # Still depth-first pre-order.
         from vizro.models import VizroBaseModel
 
         if isinstance(model, VizroBaseModel):
@@ -145,19 +143,19 @@ class ModelManager:
                 # AM rough notes:
                 # Populate root node of dashboard. Maybe dashboard should be actual tree root instead
                 # (so Tree("dashboard")) but note there's no way to attach data to it then.
-                self.__dashboard_tree.add(model)
+                self.__dashboard_tree.add(model, kind="dashboard")
             else:
-                self.__dashboard_tree[parent].add(model)
+                self.__dashboard_tree[parent].add(model, kind=field_name)
 
             for model_field in model.__class__.model_fields:
-                self.__populate_tree(getattr(model, model_field), model)
+                self.__populate_tree(getattr(model, model_field), model, model_field)
         elif isinstance(model, Mapping):
             # We don't look through keys because Vizro models aren't hashable.
             for child_model in model.values():
-                self.__populate_tree(child_model, parent)
+                self.__populate_tree(child_model, parent, field_name)
         elif isinstance(model, Collection) and not isinstance(model, str):
             for child_model in model:
-                self.__populate_tree(child_model, parent)
+                self.__populate_tree(child_model, parent, field_name)
 
     def _get_model_page(self, model: Model) -> Page:  # type: ignore[return]
         """Gets the page containing `model`."""
@@ -195,9 +193,6 @@ Keep get_models etc. centralised into MM for now but then maybe move to Dashboar
 
 Might be useful to have property in every model to enable you to get to its Node in the tree or just a reference to the 
 whole dashboard tree? Basically this inserts model manager into every model so no need for global. 
-
-How to encode field name? Could do as new node in tree (e.g. components is node) or as kind in nutree. Or maybe no 
-need to encode it at all?
 
 nutree supports custom tree traversal orders so we could write our own, but how would we define it in a better way than 
 priority = 1000 etc.?
