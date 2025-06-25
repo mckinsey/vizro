d0ec66e8a48d56d4cdd3928574b299de06cab314
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-06-25 09:39:01 +0200

    Fix header alignment

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -3,57 +3,30 @@ import numpy as np
 from vizro import Vizro
 import vizro.models as vm
 import pandas as pd
-from vizro.tables import dash_ag_grid
-import string
+import vizro.plotly.express as px
+
+iris = px.data.iris()
 
 
-N = len(string.ascii_letters)
-df = pd.DataFrame(
-    {
-        "category 1": np.random.choice(list("abc"), N),
-        "category 2": np.random.choice(list("ABC"), N),
-        "long_words": [" ".join(list(string.ascii_letters[: n + 1])) for n in range(N)],
-        "numbers": np.random.randint(N, size=N),
-    }
-)
-
-
-def dynamic_data(n=10):
-    return df.loc[:n]
-
-
-def make_controls():
-    return [
-        vm.Filter(column="long_words"),
-        vm.Filter(
-            column="category 1",
-            selector=vm.Checklist(),
-        ),
-        vm.Filter(column="category 2", selector=vm.RadioItems()),
-        vm.Filter(column="numbers"),
-    ]
-
-
-page_1 = vm.Page(
-    title="Test page",
+graphs = vm.Page(
+    title="Graphs",
     components=[
-        vm.Container(
-            components=[vm.AgGrid(id="grid", figure=dash_ag_grid(dynamic_data))],
-            controls=make_controls(),
+        vm.Graph(
+            figure=px.scatter(iris, x="sepal_width", y="sepal_length", color="species"),
+            title="Relationships between Sepal Width and Sepal Length",
+            header="""
+                Each point in the scatter plot represents one of the 150 iris flowers, with colors indicating their
+                types. The Setosa type is easily identifiable by its short and wide sepals.
+
+                However, there is still overlap between the Versicolor and Virginica types when considering only sepal
+                width and length.
+                """,
+            footer="""SOURCE: **Plotly iris data set, 2024**""",
         ),
-    ],
-    controls=[
-        vm.Parameter(
-            targets=["grid.data_frame.n"],
-            selector=vm.Slider(
-                title="Change me to see bug", min=0, max=N, step=1, marks={0: "0", N // 2: str(N // 2), N: str(N)}
-            ),
-        ),
-        *make_controls(),
     ],
 )
 
-dashboard = vm.Dashboard(title="Test dashboard", pages=[page_1])
+dashboard = vm.Dashboard(pages=[graphs])
 
 if __name__ == "__main__":
     app = Vizro().build(dashboard)
