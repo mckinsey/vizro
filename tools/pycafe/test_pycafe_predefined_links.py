50de70fed5ac0aef158a46c582110ddc2f29edb2
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2025-06-27 17:00:09 +0200

    failure test

--- tools/pycafe/test_pycafe_predefined_links.py
+++ tools/pycafe/test_pycafe_predefined_links.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     links = sys.argv[1:]
 
     # Test the link
-    for link in links:
-        success = test_pycafe_link(url=link, wait_for_locator="#dashboard-container", wait_for_text=False)
+    success = [
+        test_pycafe_link(url=link, wait_for_locator="#dashboard-container", wait_for_text=False) for link in links
+    ]
 
-        # Exit with appropriate status code
-        sys.exit(0 if success else 1)
+    # Exit with appropriate status code
+    sys.exit(1 if False in success else 0)
