2da9eb1ec922bffe787608fc2538a04cd6e68ccc
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-07-03 15:07:55 +0200

    change accordion title

--- vizro-core/src/vizro/_constants.py
+++ vizro-core/src/vizro/_constants.py
@@ -9,6 +9,6 @@ EMPTY_SPACE_CONST = -1
 ON_PAGE_LOAD_ACTION_PREFIX = "__on_page_load_action"
 FILTER_ACTION_PREFIX = "__filter_action"
 PARAMETER_ACTION_PREFIX = "__parameter_action"
-ACCORDION_DEFAULT_TITLE = "SELECT PAGE"
+ACCORDION_DEFAULT_TITLE = "Select Page"
 VIZRO_ASSETS_PATH = Path(__file__).with_name("static")
 GAP_DEFAULT = "24px"
