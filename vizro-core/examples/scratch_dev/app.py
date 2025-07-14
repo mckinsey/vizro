a4a000c830dbd367ad9b72541711e783ea60e0cd
Vladimir <vladimir_nikolic@external.mckinsey.com>
2025-07-14 11:55:29 +0200

    Refactor app.py to improve filter component structure and remove commented-out code

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -6,12 +6,13 @@ import vizro.plotly.express as px
 from vizro import Vizro
 import vizro.models as vm
 from vizro.actions import filter_interaction, export_data
-from vizro.tables import dash_ag_grid, dash_data_table
+from vizro.tables import dash_data_table
 
 tips = px.data.tips()
 gapminder = px.data.gapminder()
 iris = px.data.iris()
-iris["date_column"] = pd.date_range(
+iris2 = px.data.iris()
+iris2["date_column"] = pd.date_range(
     start=pd.to_datetime("2024-01-01"), periods=len(iris), freq="D"
 )
 
@@ -119,46 +120,46 @@ page1 = vm.Page(
 page2 = vm.Page(
     title="FILTERS_PAGE",
     components=[
-        # vm.Tabs(
-        #     tabs=[
-        vm.Container(
-            id="FILTERS_TAB_CONTAINER",
-            title="FILTERS_TAB_CONTAINER",
-            components=[
+        vm.Tabs(
+            tabs=[
                 vm.Container(
-                    id="FILTERS_COMPONENTS_CONTAINER",
-                    title="FILTERS_COMPONENTS_CONTAINER",
-                    layout=vm.Grid(grid=[[0, 1], [0, 1], [0, 2]]),
+                    id="FILTERS_TAB_CONTAINER",
+                    title="FILTERS_TAB_CONTAINER",
                     components=[
-                        vm.Graph(
-                            id="SCATTER_GRAPH_ID",
-                            figure=px.scatter(
-                                iris,
-                                x="sepal_length",
-                                y="petal_width",
-                                color="sepal_width",
-                                height=450,
-                            ),
-                        ),
-                        vm.Graph(
-                            title="Where do we get more tips?",
-                            figure=px.bar(tips, y="tip", x="day"),
-                        ),
-                        vm.Graph(
-                            id="BOX_GRAPH_ID_2",
-                            figure=px.box(
-                                iris,
-                                x="sepal_length",
-                                y="petal_width",
-                                color="sepal_width",
-                            ),
-                        ),
+                        vm.Container(
+                            id="FILTERS_COMPONENTS_CONTAINER",
+                            title="FILTERS_COMPONENTS_CONTAINER",
+                            layout=vm.Grid(grid=[[0, 1], [0, 1], [0, 2]]),
+                            components=[
+                                vm.Graph(
+                                    id="SCATTER_GRAPH_ID",
+                                    figure=px.scatter(
+                                        iris,
+                                        x="sepal_length",
+                                        y="petal_width",
+                                        color="sepal_width",
+                                        height=450,
+                                    ),
+                                ),
+                                vm.Graph(
+                                    title="Where do we get more tips?",
+                                    figure=px.bar(tips, y="tip", x="day"),
+                                ),
+                                vm.Graph(
+                                    id="BOX_GRAPH_ID_2",
+                                    figure=px.box(
+                                        iris,
+                                        x="sepal_length",
+                                        y="petal_width",
+                                        color="sepal_width",
+                                    ),
+                                ),
+                            ],
+                        )
                     ],
-                )
-            ],
+                ),
+            ]
         ),
-        #     ]
-        # ),
         vm.Graph(
             id="BOX_GRAPH_ID",
             figure=px.box(
@@ -169,41 +170,7 @@ page2 = vm.Page(
             ),
         ),
     ],
-    controls=[
-        vm.Filter(
-            column="species",
-            targets=["SCATTER_GRAPH_ID", "BOX_GRAPH_ID"],
-            selector=vm.Dropdown(id="DROPDOWN_FILTER_FILTERS_PAGE"),
-        ),
-        vm.Filter(
-            column="species",
-            targets=["SCATTER_GRAPH_ID", "BOX_GRAPH_ID"],
-            selector=vm.RadioItems(
-                id="RADIO_ITEMS_FILTER_FILTERS_PAGE",
-                options=["setosa", "versicolor", "virginica"],
-            ),
-        ),
-        vm.Filter(
-            column="species",
-            targets=["SCATTER_GRAPH_ID"],
-            selector=vm.Checklist(
-                id="CHECK_LIST_FILTER_FILTERS_PAGE",
-                options=["setosa", "versicolor", "virginica"],
-            ),
-        ),
-        vm.Filter(
-            column="petal_width",
-            targets=["SCATTER_GRAPH_ID"],
-            selector=vm.Slider(id="SLIDER_FILTER_FILTERS_PAGE", step=0.5),
-        ),
-        vm.Filter(
-            column="sepal_length",
-            targets=["SCATTER_GRAPH_ID", "BOX_GRAPH_ID"],
-            selector=vm.RangeSlider(id="RANGE_SLIDER_FILTER_FILTERS_PAGE", step=1.0),
-        ),
-    ],
 )
-
 
 page3 = vm.Page(
     title="EXTRAS_PAGE",
@@ -217,7 +184,7 @@ page3 = vm.Page(
             components=[
                 vm.Graph(
                     figure=px.line(
-                        iris,
+                        iris2,
                         x="sepal_length",
                         y="petal_width",
                         color="sepal_width",
@@ -303,46 +270,6 @@ page3 = vm.Page(
         ),
     ],
 )
-
-
-# page11 = vm.Page(
-#     title="Flex - default - aggrid",
-#     layout=vm.Flex(),
-#     components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
-# )
-
-
-# page12 = vm.Page(
-#     title="Flex - gap - aggrid",
-#     layout=vm.Grid(grid=[[0]]),
-#     components=[vm.AgGrid(figure=dash_ag_grid(tips))],
-# )
-
-# page13 = vm.Page(
-#     title="Flex - row - aggrid",
-#     layout=vm.Flex(direction="row"),
-#     components=[vm.AgGrid(figure=dash_ag_grid(tips)) for i in range(3)],
-# )
-
-# page14 = vm.Page(
-#     title="Flex - default - table",
-#     layout=vm.Flex(),
-#     components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
-# )
-
-
-# page15 = vm.Page(
-#     title="Flex - gap - table",
-#     layout=vm.Flex(gap="40px"),
-#     components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
-# )
-
-# page16 = vm.Page(
-#     title="Flex - row - table",
-#     layout=vm.Flex(direction="row"),
-#     components=[vm.Table(figure=dash_data_table(tips)) for i in range(3)],
-# )
-
 dashboard = vm.Dashboard(
     title="Vizro dashboard for integration testing",
     pages=[page, page1, page2, page3],
