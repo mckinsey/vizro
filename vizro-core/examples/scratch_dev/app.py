282ad21f0e7e3aaebe3a9d535f9a7201358ba4a1
petar-qb <petar_pejovic@external.mckinsey.com>
2025-08-08 13:58:57 +0200

    Fix two bugs with url params

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -28,42 +28,41 @@ def my_custom_action(t: int):
     sleep(t)
 
 
-#
-
 page_1 = vm.Page(
-    title="My first dashboard",
+    title="My first dashboard - [1 guard]",
     components=[
-        # vm.Graph(figure=px.scatter("dynamic_df_gapminder", x="gdpPercap", y="lifeExp", size="pop", color="continent")),
-        vm.Graph(figure=px.histogram(df_gapminder, x="lifeExp", color="continent", barmode="group")),
+        vm.Graph(id="page_1_graph", figure=px.histogram(df_gapminder, x="lifeExp", color="continent", barmode="group")),
     ],
     controls=[
-        vm.Filter(column="continent", id="f", show_in_url=True),
+        vm.Filter(id="page_1_filter", column="continent"),
     ],
 )
 
 # TEST NEW ACTIONS SYNTAX
-# page_2 = vm.Page(
-#     title="Simple custom action",
-#     components=[
-#         vm.Graph(figure=px.scatter(df_gapminder, x="gdpPercap", y="lifeExp", size="pop", color="continent")),
-#         vm.Button(
-#             text="Export data",
-#             actions=[
-#                 export_data(id="a1"),
-#                 vm.Action(id="a2", function=my_custom_action(t=2)),
-#                 export_data(file_format="xlsx", id="a3"),
-#             ],
-#         ),
-#     ],
-#     controls=[
-#         vm.Filter(column="continent", selector=vm.RadioItems()),
-#     ],
-# )
+page_2 = vm.Page(
+    title="Export data -> custom sleep action -> export data - [1 guard]",
+    components=[
+        vm.Graph(id="page_2_graph", figure=px.scatter(df_gapminder, x="gdpPercap", y="lifeExp", size="pop", color="continent")),
+        vm.Button(
+            id="page_2_button",
+            text="Export data",
+            actions=[
+                export_data(id="a1"),
+                vm.Action(id="a2", function=my_custom_action(t=2)),
+                export_data(file_format="xlsx", id="a3"),
+            ],
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_2_filter", column="continent", selector=vm.RadioItems()),
+    ],
+)
 
 page_3 = vm.Page(
-    title="Filter interaction graph",
+    title="Filter interaction graph - [1 guard]",
     components=[
         vm.Graph(
+            id="page_3_graph",
             figure=px.box(
                 df_gapminder,
                 x="continent",
@@ -71,10 +70,10 @@ page_3 = vm.Page(
                 color="continent",
                 custom_data=["continent"],
             ),
-            actions=[filter_interaction(targets=["scatter_relation_2007"])],
+            actions=[filter_interaction(id="page_3_action", targets=["page_3_graph_2"])],
         ),
         vm.Graph(
-            id="scatter_relation_2007",
+            id="page_3_graph_2",
             figure=px.scatter(
                 df_gapminder,
                 x="gdpPercap",
@@ -85,20 +84,20 @@ page_3 = vm.Page(
         ),
     ],
     controls=[
-        vm.Filter(column="continent", show_in_url=True, id="f2"),
+        vm.Filter(id="page_3_filter", column="continent"),
     ],
 )
 
 page_4 = vm.Page(
-    title="Filter interaction grid",
+    title="Filter interaction grid - [2 guards]",
     components=[
-        vm.Table(
-            id="grid",
-            figure=dash_data_table(data_frame=df_gapminder),
-            actions=[vm.Action(function=filter_interaction(targets=["scatter_relation_2007b"]))],
+        vm.AgGrid(
+            id="page_4_grid",
+            figure=dash_ag_grid(data_frame=df_gapminder),
+            actions=[vm.Action(function=filter_interaction(targets=["page_4_graph"]))],
         ),
         vm.Graph(
-            id="scatter_relation_2007b",
+            id="page_4_graph",
             figure=px.scatter(
                 df_gapminder,
                 x="gdpPercap",
@@ -109,42 +108,142 @@ page_4 = vm.Page(
         ),
     ],
     controls=[
-        vm.Filter(column="continent"),
+        vm.Filter(id="page_4_filter", column="continent"),
     ],
 )
-#
-# page_5 = vm.Page(
-#     title="Dynamic filter",
-#     components=[
-#         vm.Graph(
-#             figure=px.scatter("dynamic_df_gapminder", x="gdpPercap", y="lifeExp", size="pop", color="continent"),
-#         ),
-#     ],
-#     controls=[
-#         vm.Filter(id="filter", column="continent", selector=vm.RadioItems(id="selector")),
-#     ],
-# )
-#
-# page_6 = vm.Page(
-#     title="DataFrame Parameter",
-#     components=[
-#         vm.Graph(
-#             id="page_6_graph", figure=px.box("dynamic_df_gapminder_arg", x="continent", y="lifeExp", color="continent")
-#         ),
-#     ],
-#     controls=[
-#         vm.Filter(id="filter", column="continent", selector=vm.Dropdown(id="filter_selector")),
-#         vm.Parameter(
-#             targets=["page_6_graph.data_frame.continent"],
-#             selector=vm.RadioItems(options=list(set(df_gapminder["continent"])), value="Europe", id="parameter"),
-#         ),
-#     ],
-# )
 
-dashboard = vm.Dashboard(pages=[page_1])
+page_5 = vm.Page(
+    title="Dynamic filter - [2 guards]",
+    components=[
+        vm.Graph(
+            id="page_5_graph",
+            figure=px.scatter("dynamic_df_gapminder", x="gdpPercap", y="lifeExp", size="pop", color="continent"),
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_4_dynamic_filter", column="continent", selector=vm.RadioItems(id="selector")),
+    ],
+)
+
+page_6 = vm.Page(
+    title="DataFrame Parameter - [2 guards]",
+    components=[
+        vm.Graph(
+            id="page_6_graph", figure=px.box("dynamic_df_gapminder_arg", x="continent", y="lifeExp", color="continent")
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_6_filter", column="continent", selector=vm.Dropdown(id="page_6_filter_selector")),
+        vm.Parameter(
+            id="page_6_dfp_parameter",
+            targets=["page_6_graph.data_frame.continent"],
+            selector=vm.RadioItems(
+                title="DFP - [1 guard]",
+                options=list(set(df_gapminder["continent"])), value="Europe", id="page_6_dfp_selector"
+            ),
+        ),
+    ],
+)
+
+page_7 = vm.Page(
+    title="URL parameter filters - [2 guards on refresh]",
+    components=[
+        vm.Graph(
+            id="page_7_graph",
+            figure=px.scatter(df_gapminder, x="gdpPercap", y="lifeExp", size="pop", color="continent"),
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_7_filter", column="continent", show_in_url=True),
+    ]
+)
+
+
+"""
+Test cases:
+1. navigate to page_8
+2. refresh the page_8 (this made issues as filters in this process were changed by sync_url clientside callback)
+3. change page_8_filter_1 and page_8_filter_2 values and check everything is ok
+4. copy the part of the URL that contains page_8_filter_1 but not page_8_filter_2. Open it in the new browser tab.
+5. check that page_8_filter_1 is set to the value from the URL and page_8_filter_2 is set to the default value.
+6. check that both filters are shown in the URL.
+7. change page_8_filter_1 and see how the filter-action is triggered.
+8. change page_8_filter_2 and see how the filter-action is triggered. (this was the problem if 
+    guardian is not changed from the sync_url clientside callback)
+"""
+page_8 = vm.Page(
+    title="Multi URL parameter filters - [3 guards or refresh]",
+    components=[
+        vm.Graph(
+            id="page_8_graph",
+            figure=px.scatter(df_gapminder, x="gdpPercap", y="lifeExp", size="pop", color="continent"),
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_8_filter_1", column="continent", show_in_url=True, selector=vm.RadioItems()),
+        vm.Filter(id="page_8_filter_2", column="continent", show_in_url=True, selector=vm.RadioItems()),
+    ]
+)
+
+
+page_9 = vm.Page(
+    title="DataFrame Parameter and URL Filter- [2 guards]",
+    components=[
+        vm.Graph(
+            id="page_9_graph", figure=px.box("dynamic_df_gapminder_arg", x="continent", y="lifeExp", color="continent")
+        ),
+    ],
+    controls=[
+        vm.Filter(id="page_9_filter", column="continent", selector=vm.Dropdown(id="page_9_filter_selector"), show_in_url=True),
+        vm.Parameter(
+            id="page_9_dfp_parameter",
+            targets=["page_9_graph.data_frame.continent"],
+            selector=vm.RadioItems(
+                title="DFP - [1 guard]",
+                options=list(set(df_gapminder["continent"])), value="Europe", id="page_9_dfp_selector"
+            ),
+        ),
+    ],
+)
+
+# TODO: Solve bug when DFP changes and after that filter changes. Correct number of guards on DFP.
+page_10 = vm.Page(
+    title="DFP + Dynamic filter + URL + filter interaction - [5 guards on refresh]",
+    components=[
+        vm.AgGrid(
+            id="page_10_grid",
+            figure=dash_ag_grid(data_frame="dynamic_df_gapminder_arg"),
+            actions=[vm.Action(function=filter_interaction(id="page_10_filter_interaction", targets=["page_10_graph"]))],
+        ),
+        vm.Graph(
+            id="page_10_graph",
+            figure=px.scatter("dynamic_df_gapminder_arg", x="gdpPercap", y="lifeExp", size="pop", color="continent"),
+        ),
+    ],
+    controls=[
+        # TODO: Try with different selectors
+        vm.Filter(
+            id="page_10_filter",
+            column="continent",
+            selector=vm.Dropdown(id="page_10_filter_selector"),
+            show_in_url=True
+        ),
+        vm.Parameter(
+            id="page_10_dfp_parameter",
+            targets=[
+                "page_10_grid.data_frame.continent",
+                "page_10_graph.data_frame.continent",
+            ],
+            selector=vm.RadioItems(
+                title="DFP - [3 guards]",
+                options=list(set(df_gapminder["continent"])), value="Europe", id="page_10_dfp_selector"
+            ),
+            show_in_url=True
+        ),
+    ]
+)
+
+dashboard = vm.Dashboard(pages=[page_1, page_2, page_3, page_4, page_5, page_6, page_7, page_8, page_9, page_10])
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run(
-        debug=True,
-        use_reloader=False,
-    )
+    Vizro().build(dashboard).run(debug=True)
