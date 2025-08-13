4746f7ccf6bb57a670fd33ae4ee2671acd7469ec
Antony Milne <antony.milne@quantumblack.com>
2025-08-13 10:31:48 +0100

    Checkpoint

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -6,42 +6,75 @@ from datetime import datetime
 
 
 @capture("action")
-def update_text(use_24_hour_clock, date_format):
+def update_time_text(use_24_hour_clock):  # (1)!
     time_format = "%H:%M:%S" if use_24_hour_clock else "%I:%M:%S %p"
-    date_format = "%d/%m/%y" if date_format == "DD/MM/YY" else "%m/%d/%y"
     now = datetime.now()
     time = now.strftime(time_format)
+    return f"The time is {time}"
+
+
+@capture("action")
+def update_date_text(date_format):  # (1)!
+    date_format = "%d/%m/%y" if date_format == "DD/MM/YY" else "%m/%d/%y"
+    now = datetime.now()
     date = now.strftime(date_format)
-    return [f"The time is {time}", f"The date is {date}"]
+    return f"The date is {date}"  # (2)!
 
 
-vm.Container.add_type("components", vm.Switch)  # (1)!
+@capture("action")
+def update_time_date_formats(location):
+    if location == "Berlin":
+        return True, "DD/MM/YY"
+    return False, "MM/DD/YY"
+
+
+vm.Container.add_type("components", vm.Switch)
 vm.Container.add_type("components", vm.RadioItems)
+vm.Container.add_type("components", vm.Dropdown)
 
 page = vm.Page(
     title="My first custom action",
     layout=vm.Flex(),
     components=[
-        vm.Container(  # (2)!
+        vm.Container(
             layout=vm.Flex(direction="row"),
             variant="outlined",
             components=[
-                vm.Switch(id="clock_switch", title="24-hour clock", value=True),  # (3)!
-                vm.RadioItems(id="date_selector", options=["DD/MM/YY", "MM/DD/YY"]),
-                vm.Button(
+                vm.Dropdown(
+                    id="location_dropdown",
+                    title="Location",
+                    options=["Berlin", "Washington, D.C"],
+                    multi=False,
                     actions=[
                         vm.Action(
-                            function=update_text(use_24_hour_clock="clock_switch", date_format="date_selector"),
-                            outputs=["time_text", "date_text"],
-                        ),
-                    ]
+                            function=update_time_date_formats("location_dropdown.value"),
+                            outputs=["clock_switch", "date_selector"],
+                        )
+                    ],
+                ),
+                vm.Switch(
+                    id="clock_switch",
+                    title="24-hour clock",
+                    value=True,
+                    actions=[
+                        vm.Action(function=update_time_text(use_24_hour_clock="clock_switch"), outputs=["time_text"])
+                    ],
+                ),
+                vm.RadioItems(
+                    id="date_selector",
+                    options=["DD/MM/YY", "MM/DD/YY"],
+                    actions=[vm.Action(function=update_date_text(date_format="date_selector"), outputs=["date_text"])],
                 ),
             ],
         ),
-        vm.Text(id="time_text", text="Click the button"),
-        vm.Text(id="date_text", text="Click the button"),
+        vm.Text(id="time_text", text="Click the switch"),
+        vm.Text(id="date_text", text="Click the radio item"),
     ],
 )
+
+# HERE: Do API request for weather in location as explicit chain. Probably this before implicit chain.
+# Put time into timezone as final challenge?
+# Note datetime shows system time (?) or UTC rather than fixed timezone.
 
 dashboard = vm.Dashboard(pages=[page])
 Vizro().build(dashboard).run()
