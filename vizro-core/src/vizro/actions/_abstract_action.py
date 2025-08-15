bebc1cb4387ffc996c117ae29f3382e0baadc9d0
petar-qb <petar_pejovic@external.mckinsey.com>
2025-08-15 11:20:43 +0200

    Minor

--- vizro-core/src/vizro/actions/_abstract_action.py
+++ vizro-core/src/vizro/actions/_abstract_action.py
@@ -4,7 +4,6 @@ import abc
 import inspect
 from typing import Union
 
-from dash.development.base_component import Component
 from pydantic import TypeAdapter
 
 from vizro.models._action._action import _BaseAction
