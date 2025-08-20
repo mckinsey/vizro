07a58afc789af7c8aed2fa97055c518d797332af
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-08-20 10:13:00 +0200

    Make using vizro-bootstrap configurable

--- vizro-core/examples/dev/app.py
+++ vizro-core/examples/dev/app.py
@@ -1203,7 +1203,7 @@ if __name__ == "__main__":
     # DBC-THEMES: BOOTSTRAP, MINTY, LUMEN, CYBORG, DARKLY, LITERA, SLATE, SOLAR, SUPERHERO, UNITED, YETI
     # GOOD: BOOTSTRAP, MINTY, LITERA, UNITED, LUMEN
     # NOT SO GOOD: SOLAR, CYBORG, DARKLY, SLATE, SUPERHERO (all dark themes only don't look good on light)
-    app = Vizro(external_stylesheets=[dbc.themes.DARKLY]).build(dashboard)
+    app = Vizro(use_vizro_bootstrap=False, external_stylesheets=[dbc.themes.DARKLY]).build(dashboard)
     app.dash.layout.children.append(
         dbc.NavLink(
             ["Made with ", html.Img(src=get_asset_url("logo.svg"), id="banner", alt="Vizro logo"), "vizro"],
