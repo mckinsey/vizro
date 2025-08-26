bd9c55d5195493305e42ca5105761f2bcf3047b4
Antony Milne <antony.milne@quantumblack.com>
2025-08-26 13:35:33 +0100

    Example drill down and through

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,36 +1,85 @@
-from datetime import datetime
+import base64
+import json
 
 import vizro.models as vm
 from vizro import Vizro
 from vizro.models.types import capture
 
+import vizro.plotly.express as px
+from vizro.tables import dash_ag_grid
+
+df = px.data.gapminder().query("year == 2007")
+
 
 @capture("action")
-def update_text(use_24_hour_clock):
-    time_format = "%H:%M:%S %Z" if use_24_hour_clock else "%I:%M:%S %p %Z"
-    switch_title = "24-hour clock" if use_24_hour_clock else "12-hour clock"
-    time = datetime.now().strftime(time_format)
-    return f"The time is {time}", switch_title
+def drill_down(data):
+    # In future this action will be built-in.
+    # In fact, hopefully soon you won't need to do this data["points"][0] stuff even for custom action.
+    [continent] = data["points"][0]["customdata"]
+    # In future this would just be trigger["customdata"]
+    return continent
 
 
-vm.Page.add_type("components", vm.Switch)  # (1)!
+def encode_to_base64(value):
+    json_bytes = json.dumps(value, separators=(",", ":")).encode("utf-8")
+    b64_bytes = base64.urlsafe_b64encode(json_bytes)
+    return f"b64_{b64_bytes.decode('utf-8').rstrip('=')}"
+
+
+@capture("action")
+def drill_through(data):
+    # In future this action will be built-in.
+    # In fact, hopefully soon you won't need to do this data["points"][0] stuff even for custom action.
+    [continent] = data["points"][0]["customdata"]
+    # In future this would just be trigger["customdata"]
+    return "/target_page", f"?target_filter2={encode_to_base64(continent)}"
+
 
 page = vm.Page(
-    title="Action attached to button",
-    layout=vm.Flex(),
+    title="Action triggered by graph",
     components=[
-        vm.Switch(
-            id="clock_switch",
-            title="24-hour clock",
-            value=True,
-            actions=vm.Action(
-                function=update_text(use_24_hour_clock="clock_switch"),
-                outputs=["time_text", "clock_switch.title"],
-            ),
+        vm.Container(
+            title="Source graph",
+            components=[
+                vm.Graph(
+                    id="source_graph",
+                    figure=px.scatter(df, x="pop", y="lifeExp", log_x=True, color="continent", custom_data="continent"),
+                    actions=vm.Action(function=drill_down("source_graph.clickData"), outputs="target_filter"),
+                    # In future you won't need to label source_graph and do source_graph.clickData. You'd use special
+                    # argument trigger instead.
+                ),
+            ],
         ),
-        vm.Text(id="time_text", text="Toggle the switch"),
+        vm.Container(
+            title="Target table",
+            controls=[vm.Filter(id="target_filter", column="continent")],
+            components=[vm.AgGrid(figure=dash_ag_grid(df))],
+        ),
     ],
 )
 
-dashboard = vm.Dashboard(pages=[page])
-Vizro().build(dashboard).run()
+page_2 = vm.Page(
+    title="Cross-page action triggered by graph source",
+    components=[
+        vm.Graph(
+            id="source_graph2",
+            figure=px.scatter(df, x="pop", y="lifeExp", log_x=True, color="continent", custom_data="continent"),
+            actions=vm.Action(
+                function=drill_through("source_graph2.clickData"), outputs=["vizro_url.pathname", "vizro_url.search"]
+            ),
+            # In future you won't need to label source_graph and do source_graph.clickData. You'd use special
+            # argument trigger instead.
+        ),
+    ],
+)
+
+
+page_3 = vm.Page(
+    id="target_page",
+    title="Cross-page action triggered by graph target",
+    components=[vm.AgGrid(figure=dash_ag_grid(df))],
+    controls=[vm.Filter(id="target_filter2", column="continent", show_in_url=True)],
+)
+
+dashboard = vm.Dashboard(pages=[page, page_2, page_3])
+Vizro().build(dashboard).run(debug=True)
