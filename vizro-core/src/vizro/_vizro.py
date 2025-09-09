44bcb92ca4f1db2740c2eebd913f4cd915573b08
Jo Stichbury <jo_stichbury@mckinsey.com>
2025-09-09 18:25:07 +0100

    Update _vizro.py

--- vizro-core/src/vizro/_vizro.py
+++ vizro-core/src/vizro/_vizro.py
@@ -31,7 +31,7 @@ class Vizro:
     """The main class of the `vizro` package."""
 
     def __init__(self, **kwargs):
-        """Initializes Dash app, stored in `self.dash`, passing parameters through to `Dash.__init__`."""
+        """Initializes Dash app, stored in `self.dash`."""
         # **kwargs is passed through to `Dash.__init__`, e.g. `assets_folder`, `url_base_pathname`.
         # See [Dash documentation](https://dash.plotly.com/reference#dash.dash) for possible arguments.
         # Set suppress_callback_exceptions=True for the following reasons:
