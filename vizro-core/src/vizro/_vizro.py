498925adf648804afc7d4137eaae93cce040b322
Jo Stichbury <jo_stichbury@mckinsey.com>
2025-09-09 15:03:48 +0100

    Update _vizro.py

--- vizro-core/src/vizro/_vizro.py
+++ vizro-core/src/vizro/_vizro.py
@@ -34,7 +34,7 @@ class Vizro:
         """Initializes Dash app, stored in `self.dash`.
 
         Args:
-            kwargs:Passed through to `Dash.__init__`, e.g. `assets_folder`, `url_base_pathname`. See
+            kwargs : Passed through to `Dash.__init__`, e.g. `assets_folder`, `url_base_pathname`. See
                 [Dash documentation](https://dash.plotly.com/reference#dash.dash) for possible arguments.
 
         """
@@ -126,8 +126,8 @@ class Vizro:
         """Runs the dashboard.
 
         Args:
-            args: Passed through to `dash.run`.
-            kwargs: Passed through to `dash.run`.
+            args : Passed through to `dash.run`.
+            kwargs : Passed through to `dash.run`.
 
         """
         data_manager._frozen_state = True
