c361400915193184adb3ebe375f2c191817c8e7b
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-09-15 09:23:03 +0200

    Tidy

--- vizro-core/src/vizro/models/_dashboard.py
+++ vizro-core/src/vizro/models/_dashboard.py
@@ -233,19 +233,9 @@ class Dashboard(VizroBaseModel):
         path_to_logo_dark = get_asset_url(logo_dark_img) if logo_dark_img else None
         path_to_logo_light = get_asset_url(logo_light_img) if logo_light_img else None
 
-        logo = html.A(
-            html.Img(id="logo", src=path_to_logo, hidden=not path_to_logo), href="/", style={"height": "100%"}
-        )
-        logo_dark = html.A(
-            html.Img(id="logo-dark", src=path_to_logo_dark, hidden=not path_to_logo_dark),
-            href="/",
-            style={"height": "100%"},
-        )
-        logo_light = html.A(
-            html.Img(id="logo-light", src=path_to_logo_light, hidden=not path_to_logo_light),
-            href="/",
-            style={"height": "100%"},
-        )
+        logo = html.A(html.Img(id="logo", src=path_to_logo, hidden=not path_to_logo), href="/")
+        logo_dark = html.A(html.Img(id="logo-dark",src=path_to_logo_dark, hidden=not path_to_logo_dark), href="/")
+        logo_light = html.A(html.Img(id="logo-light",src=path_to_logo_light, hidden=not path_to_logo_light), href="/")
 
         return logo, logo_dark, logo_light
 
