afd86f84b02203ebe9a6bc31ba4f48af38553e6e
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-10-01 18:40:48 +0200

    Lint

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -15,7 +15,7 @@ SELECTED_COUNTRIES = [
     "Vietnam",
     "Cambodia",
     "Myanmar",
-    "NONE"
+    "NONE",
 ]
 
 gapminder = px.data.gapminder().query("country.isin(@SELECTED_COUNTRIES)")
@@ -24,25 +24,16 @@ gapminder = px.data.gapminder().query("country.isin(@SELECTED_COUNTRIES)")
 @capture("graph")
 def bump_chart(data_frame, highlight_country=None):
     data_with_rank = data_frame.copy()
-    data_with_rank['rank'] = data_frame.groupby('year')['lifeExp'].rank(
-        method='dense', ascending=False
-    )
-    
-    fig = px.line(
-        data_with_rank,
-        x="year",
-        y="rank",
-        color="country",
-        markers=True,
-    )
-    
+    data_with_rank["rank"] = data_frame.groupby("year")["lifeExp"].rank(method="dense", ascending=False)
+
+    fig = px.line(data_with_rank, x="year", y="rank", color="country", markers=True)
     fig.update_layout(
         legend_title="",
         xaxis_title="",
         yaxis=dict(autorange="reversed"),
         yaxis_title="Rank (1 = Highest lifeExp)",
     )
-    
+
     if highlight_country:
         for trace in fig.data:
             if trace.name == highlight_country:
@@ -51,17 +42,13 @@ def bump_chart(data_frame, highlight_country=None):
             else:
                 trace.opacity = 0.3
                 trace.line.width = 2
-    
+
     return fig
 
 
 @capture("graph")
 def bar_chart(data_frame):
-    fig = px.bar(
-        data_frame[data_frame["year"] == 2007],
-        y="country", 
-        x="lifeExp",
-    )
+    fig = px.bar(data_frame[data_frame["year"] == 2007], y="country", x="lifeExp")
     fig.update_layout(yaxis_title="", xaxis_title="lifeExp (2007)")
     return fig
 
@@ -71,7 +58,7 @@ page = vm.Page(
     components=[
         vm.Graph(
             figure=bar_chart(data_frame=gapminder),
-            header="💡 Click on any bar to highlight that country's trace in the bump chart",
+            header="💡 Click any bar to highlight that country in the bump chart",
             actions=[va.set_control(control="highlight_parameter", value="y")],
         ),
         vm.Graph(id="bump_chart", figure=bump_chart(data_frame=gapminder)),
@@ -83,7 +70,7 @@ page = vm.Page(
             selector=vm.Dropdown(multi=False, options=SELECTED_COUNTRIES, value="NONE"),
             visible=False,
         ),
-    ]
+    ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
