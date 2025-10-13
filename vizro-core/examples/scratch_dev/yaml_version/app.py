0900d7b7ed072159219f0d80182629db4fa2233a
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-10-13 12:15:39 +0200

    Fix formatting of yaml example

--- vizro-core/examples/scratch_dev/yaml_version/app.py
+++ vizro-core/examples/scratch_dev/yaml_version/app.py
@@ -9,28 +9,34 @@ from vizro.managers import data_manager
 from vizro.models import Dashboard
 from vizro.models.types import capture
 
-data_manager["iris"] = px.data.iris()
-data_manager["gapminder"] = px.data.gapminder().query("continent == 'Europe' and year == 2007")
-data_manager["tips"] = px.data.tips()
+selected_countries = [
+    "Singapore",
+    "Malaysia",
+    "Thailand",
+    "Indonesia",
+    "Philippines",
+    "Vietnam",
+    "Cambodia",
+    "Myanmar",
+]
+
+gapminder = px.data.gapminder().query("country.isin(@selected_countries)")
+
+data_manager["gapminder"] = gapminder
+data_manager["gapminder_2007"] = gapminder.query("year == 2007")
 
 
 @capture("graph")
-def scatter_with_highlight(data_frame, highlight_country=None):
-    country_is_highlighted = data_frame["country"] == highlight_country
-    fig = px.scatter(
+def bar_with_highlight(data_frame, highlight_country=None):  # (1)!
+    country_is_highlighted = data_frame["country"] == highlight_country  # (2)!
+    fig = px.bar(
         data_frame,
-        x="gdpPercap",
-        y="lifeExp",
-        size="pop",
-        size_max=60,
-        opacity=0.3,
+        x="lifeExp",
+        y="country",
+        labels={"lifeExp": "lifeExp in 2007"},
         color=country_is_highlighted,
-        category_orders={"color": [False, True]},
+        category_orders={"country": sorted(data_frame["country"])},  # (3)!
     )
-
-    if highlight_country is not None:
-        fig.update_traces(selector=1, marker={"line_width": 2, "opacity": 1})
-
     fig.update_layout(showlegend=False)
     return fig
 
