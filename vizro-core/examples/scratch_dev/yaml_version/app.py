16968eaa4cc347b9640ffa17cd01990777920cc7
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-10-13 12:32:22 +0200

    Revert yaml examples in scratch dev for tests to pass

--- vizro-core/examples/scratch_dev/yaml_version/app.py
+++ vizro-core/examples/scratch_dev/yaml_version/app.py
@@ -24,22 +24,8 @@ gapminder = px.data.gapminder().query("country.isin(@selected_countries)")
 
 data_manager["gapminder"] = gapminder
 data_manager["gapminder_2007"] = gapminder.query("year == 2007")
-
-
-
-@capture("graph")
-def bar_with_highlight(data_frame, highlight_country=None):  # (1)!
-    country_is_highlighted = data_frame["country"] == highlight_country  # (2)!
-    fig = px.bar(
-        data_frame,
-        x="lifeExp",
-        y="country",
-        labels={"lifeExp": "lifeExp in 2007"},
-        color=country_is_highlighted,
-        category_orders={"country": sorted(data_frame["country"])},  # (3)!
-    )
-    fig.update_layout(showlegend=False)
-    return fig
+data_manager["tips"] = px.data.tips()
+data_manager["iris"] = px.data.iris()
 
 
 dashboard = yaml.safe_load(Path("dashboard.yaml").read_text(encoding="utf-8"))
