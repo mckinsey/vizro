582054a167d80792677ab85f3713f215ad8ab77f
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-10-20 17:47:46 +0200

    add builtin components

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,165 +1,36 @@
-import dash_bootstrap_components as dbc
 import pandas as pd
 
 import vizro.plotly.express as px
 import vizro.models as vm
+import vizro.actions as va
 from vizro import Vizro
-from vizro.figures import kpi_card, kpi_card_reference
-from vizro.actions import set_control
+from dash import dcc
 from vizro.models.types import capture
 
 
 df = px.data.iris()
-df_kpi = pd.DataFrame({"Actual": [100, 200, 700], "Reference": [100, 300, 500], "Category": ["A", "B", "C"]})
-
-
-example_cards = [
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with value"),
-    kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with aggregation", agg_func="median"),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with formatting",
-        value_format="${value:.2f}",
-    ),
-    kpi_card(
-        data_frame=df_kpi,
-        value_column="Actual",
-        title="KPI with icon",
-        icon="Shopping Cart",
-    ),
-]
-
-example_reference_cards = [
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference (pos)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        agg_func="median",
-        title="KPI reference (neg)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference with formatting",
-        value_format="{value:.2f}€",
-        reference_format="{delta:+.2f}€ vs. last year ({reference:.2f}€)",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference with icon",
-        icon="Shopping Cart",
-    ),
-    kpi_card_reference(
-        data_frame=df_kpi,
-        value_column="Actual",
-        reference_column="Reference",
-        title="KPI reference (reverse color)",
-        reverse_color=True,
-    ),
-]
-
-page_1 = vm.Page(
-    title="KPI cards + 1 Nav card",
-    layout=vm.Flex(direction="row", wrap=True),
-    components=[vm.Figure(figure=figure) for figure in example_cards + example_reference_cards]
-    + [
-        vm.Card(
-            text="This is a nav card. Click to go to page 2.",
-            href="/page-2",
-        )
-    ],
-)
 
 
 @capture("action")
-def custom_action(_trigger):
-    return f"The card is clicked: {_trigger} times."
+def download_data():
+    return dcc.send_data_frame(df.to_csv, "mydf.csv")
 
 
-page_2 = vm.Page(
-    title="KPI cards trigger custom action",
-    path="/page-2",
-    layout=vm.Flex(direction="row", wrap=True),
+@capture("graph")
+def custom_bar(data_frame, custom_data):
+    return px.bar(data_frame, x="species", y="sepal_width", custom_data=custom_data)
+
+
+page_1 = vm.Page(
+    title="Vizro download",
     components=[
-        vm.Figure(
-            figure=kpi_card(data_frame=df_kpi, value_column="Actual", title="KPI with value"),
-            actions=vm.Action(function=custom_action(), outputs="text_output"),
-        ),
-        vm.Text(id="text_output", text="Click a card to see the action output here."),
+        vm.Graph(figure=custom_bar(df, custom_data="species")),
+        vm.Button(text="Download data", actions=vm.Action(function=download_data(), outputs=["vizro_download"])),
     ],
 )
 
 
-page_3 = vm.Page(
-    title="KPI cards trigger set_control",
-    path="/page-3",
-    layout=vm.Flex(direction="row", wrap=True),
-    components=[
-        vm.Figure(
-            figure=kpi_card(data_frame=df_kpi[df_kpi["Category"] == "A"], value_column="Actual", title="Actual for A"),
-            actions=set_control(control="filter-id-1", value="A"),
-        ),
-        vm.Figure(
-            figure=kpi_card(data_frame=df_kpi[df_kpi["Category"] == "B"], value_column="Actual", title="Actual for B"),
-            actions=set_control(control="filter-id-1", value="B"),
-        ),
-        vm.Figure(
-            figure=kpi_card(data_frame=df_kpi[df_kpi["Category"] == "C"], value_column="Actual", title="Actual for C"),
-            actions=set_control(control="filter-id-1", value="C"),
-        ),
-        vm.Graph(
-            id="graph-1",
-            figure=px.bar(
-                df_kpi,
-                x="Category",
-                y="Actual",
-                color="Category",
-                color_discrete_map={"A": "#00b4ff", "B": "#ff9222", "C": "#3949ab"},
-            ),
-        ),
-    ],
-    controls=[vm.Filter(id="filter-id-1", column="Category", targets=["graph-1"])],
-)
-
-
-@capture("figure")
-def button_as_figure(data_frame):
-    return dbc.Button(
-        children=f"Graph below shows {len(data_frame)} rows. Click to trigger two actions",
-        color="primary",
-        class_name="m-2",
-    )
-
-
-page_4 = vm.Page(
-    title="Custom button as a figure",
-    path="/page-4",
-    components=[
-        vm.Figure(
-            id="figure-2",
-            figure=button_as_figure(df),
-            actions=[
-                set_control(control="filter-id-2", value="setosa"),
-                vm.Action(function=custom_action(), outputs="text-2"),
-            ],
-        ),
-        vm.Text(id="text-2", text="Click the button to see the action output here."),
-        vm.Graph(id="graph-2", figure=px.scatter(df, x="sepal_width", y="sepal_length", color="species")),
-    ],
-    controls=[vm.Filter(id="filter-id-2", column="species", targets=["graph-2", "figure-2"])],
-)
-
-dashboard = vm.Dashboard(pages=[page_1, page_2, page_3, page_4])
+dashboard = vm.Dashboard(pages=[page_1])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
