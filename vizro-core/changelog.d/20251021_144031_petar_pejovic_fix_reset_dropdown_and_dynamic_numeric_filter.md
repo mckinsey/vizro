8996ba8b263d1609a74fb3941959d19bc8d20fd3
petar-qb <petar_pejovic@external.mckinsey.com>
2025-10-21 14:51:51 +0200

    Fix 2271 and 1936

---
+++ vizro-core/changelog.d/20251021_144031_petar_pejovic_fix_reset_dropdown_and_dynamic_numeric_filter.md
@@ -0,0 +1,49 @@
+<!--
+A new scriv changelog fragment.
+
+Uncomment the section that is right (remove the HTML comment wrapper).
+-->
+
+<!--
+### Highlights ✨
+
+- A bullet item for the Highlights ✨ category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
+<!--
+### Removed
+
+- A bullet item for the Removed category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
+<!--
+### Added
+
+- A bullet item for the Added category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
+<!--
+### Changed
+
+- A bullet item for the Changed category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
+<!--
+### Deprecated
+
+- A bullet item for the Deprecated category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
+### Fixed
+
+- Fix multi-select `Dropdown` filter to work when its value is defined as a single object instead of a list. ([#2278](https://github.com/mckinsey/vizro/pull/2278))
+
+- Fix dynamic numeric filters to handle year-like numbers correctly. ([#2278](https://github.com/mckinsey/vizro/pull/2278))
+
+
+<!--
+### Security
+
+- A bullet item for the Security category with a link to the relevant PR at the end of your entry, e.g. Enable feature XXX. ([#1](https://github.com/mckinsey/vizro/pull/1))
+
+-->
--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,194 +1,69 @@
-import pandas as pd
-import vizro.plotly.express as px
-import vizro.models as vm
 from vizro import Vizro
-from vizro.actions import set_control
-from vizro.models.types import capture
+import vizro.models as vm
+import vizro.plotly.express as px
 from vizro.managers import data_manager
+from vizro.tables import dash_ag_grid
 
-df = px.data.iris()
-
-df["date_column"] = pd.date_range(start=pd.to_datetime("2024-01-01"), periods=len(df), freq="D")
-df["is_setosa"] = df["species"] == "setosa"
-
-data_manager["dynamic_df"] = lambda: df
+gapminder = px.data.gapminder()
+data_manager["df_dynamic"] = lambda: gapminder
 
 
 page_1 = vm.Page(
-    title="Buttons trigger set_control",
+    title="[BUG] #2271",
     layout=vm.Flex(),
     components=[
-        vm.Button(
-            text="Set setosa",
-            actions=set_control(control="filter-id-1", value="setosa"),
-        ),
-        vm.Button(
-            text="Set versicolor",
-            actions=set_control(control="filter-id-1", value="versicolor"),
-        ),
-        vm.Button(
-            text="Set virginica",
-            actions=set_control(control="filter-id-1", value="virginica"),
-        ),
-        vm.Graph(
-            id="graph-1",
-            figure=px.scatter(
-                df,
-                x="sepal_width",
-                y="sepal_length",
-                color="species",
-                color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222", "virginica": "#3949ab"},
-            ),
+        vm.Text(text="Reset Dropdown(multi=True) control does not work when defined with non-list value"),
+        vm.Text(text="https://github.com/McK-Internal/vizro-internal/issues/2271"),
+        vm.Tabs(
+            tabs=[
+                vm.Container(
+                    title="Dropdown [bug]",
+                    controls=[
+                        vm.Filter(column="year", selector=vm.Dropdown(value=2007)),
+                        vm.Filter(column="continent", selector=vm.Dropdown(value="Europe")),
+                    ],
+                    components=[
+                        vm.Graph(figure=px.scatter(gapminder, x="gdpPercap", y="lifeExp", color="continent")),
+                    ],
+                ),
+                vm.Container(
+                    title="Checklist [works]",
+                    controls=[
+                        vm.Filter(column="year", selector=vm.Checklist(value=[2007])),
+                    ],
+                    components=[
+                        vm.Graph(figure=px.scatter(gapminder, x="gdpPercap", y="lifeExp", color="continent")),
+                    ],
+                ),
+            ]
         ),
     ],
-    controls=[vm.Filter(id="filter-id-1", column="species", targets=["graph-1"])],
 )
+
 
 page_2 = vm.Page(
-    title="Button resets the certain control",
+    title="[BUG] #1936",
+    layout=vm.Flex(),
     components=[
-        vm.Button(
-            text="Reset species filter",
-            actions=set_control(control="filter-id-2", value="setosa"),
+        vm.Text(
+            text="""
+            Dynamic filters don't work with numeric columns which values can be translated to date objects by ISO 8601.
+            For example, the 'year' column in the Gapminder dataset. It contains years as integers (1952, 1957...)
+        """
         ),
-        vm.Graph(
-            id="graph-2",
-            figure=px.scatter(
-                df,
-                x="sepal_width",
-                y="sepal_length",
-                color="species",
-                color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222", "virginica": "#3949ab"},
-            ),
-        ),
-    ],
-    controls=[
-        vm.Filter(id="filter-id-2", column="species", selector=vm.RadioItems()),
-        vm.Filter(id="filter-id-3", column="sepal_length"),
-    ],
-)
-
-
-# ==== Page 3: Button sets all types of selectors ====
-
-SET_SELECTORS_VALUES = [
-    "versicolor",
-    "versicolor",
-    ["versicolor"],
-    ["versicolor"],
-    "petal_length",
-]
-
-
-def _make_all_selectors(prefix_id: str, visible=True, show_in_url=False):
-    return [
-        vm.Filter(
-            id=f"{prefix_id}_filter_1",
-            column="species",
-            targets=[f"{prefix_id}_graph"],
-            selector=vm.RadioItems(id=f"{prefix_id}_filter_1_selector"),
-            visible=visible,
-            show_in_url=show_in_url,
-        ),
-        vm.Filter(
-            id=f"{prefix_id}_filter_2",
-            column="species",
-            targets=[f"{prefix_id}_graph"],
-            selector=vm.Dropdown(id=f"{prefix_id}_filter_2_selector", multi=False),
-            visible=visible,
-            show_in_url=show_in_url,
-        ),
-        vm.Filter(
-            id=f"{prefix_id}_filter_3",
-            column="species",
-            targets=[f"{prefix_id}_graph"],
-            selector=vm.Checklist(id=f"{prefix_id}_filter_3_selector"),
-            visible=visible,
-            show_in_url=show_in_url,
-        ),
-        vm.Filter(
-            id=f"{prefix_id}_filter_4",
-            column="species",
-            targets=[f"{prefix_id}_graph"],
-            selector=vm.Dropdown(id=f"{prefix_id}_filter_4_selector"),
-            visible=visible,
-            show_in_url=show_in_url,
-        ),
-        vm.Parameter(
-            id=f"{prefix_id}_filter_5",
-            targets=[f"{prefix_id}_graph.x"],
-            selector=vm.RadioItems(
-                id=f"{prefix_id}_filter_10_selector",
-                title="I'M A PARAMETER",
-                options=["sepal_width", "sepal_length", "petal_width", "petal_length"],
-            ),
-            visible=visible,
-            show_in_url=show_in_url,
-        ),
-    ]
-
-
-def _make_page_components(page_id: str, visible=True, show_in_url=False):
-    return [
-        vm.Button(
-            text="Set controls",
-            # This was the solution before set_control source from Button was available
-            # actions=vm.Action(
-            #     function=set_all_page_controls(),
-            #     outputs=[
-            #         *[f"{page_id}_static_filter_{i}" for i in range(1, 11)],
-            #         *[f"{page_id}_dynamic_filter_{i}" for i in range(1, 11)],
-            #     ],
-            # ),
-            actions=[
-                set_control(control=f"{page_id}_static_filter_{i}", value=SET_SELECTORS_VALUES[i - 1])
-                for i in range(1, 6)
-            ]
-            + [
-                set_control(control=f"{page_id}_dynamic_filter_{i}", value=SET_SELECTORS_VALUES[i - 1])
-                for i in range(1, 6)
-            ],
-        ),
-        vm.Graph(
-            id=f"{page_id}_static_graph",
-            title="Static Graph",
-            figure=px.scatter(
-                df,
-                x="sepal_width",
-                y="sepal_length",
-                color="species",
-                color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222", "virginica": "#3949ab"},
-            ),
-        ),
+        vm.Text(text="https://github.com/McK-Internal/vizro-internal/issues/1936"),
         vm.Container(
-            title="Dynamic selectors",
-            components=[
-                vm.Graph(
-                    id=f"{page_id}_dynamic_graph",
-                    title="Dynamic Graph",
-                    figure=px.scatter(
-                        "dynamic_df",
-                        x="sepal_width",
-                        y="sepal_length",
-                        color="species",
-                        color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222", "virginica": "#3949ab"},
-                    ),
-                ),
+            controls=[
+                vm.Filter(column="year", selector=vm.Slider()),
             ],
-            controls=_make_all_selectors(prefix_id=f"{page_id}_dynamic", visible=visible, show_in_url=show_in_url),
+            components=[
+                vm.Graph(figure=px.scatter("df_dynamic", x="gdpPercap", y="lifeExp", color="continent")),
+            ],
         ),
-    ]
-
-
-page_3 = vm.Page(
-    title="Button sets different selectors",
-    layout=vm.Grid(grid=[[0], [1], [1], [1], [1], [2], [2], [2], [2], [2], [2]]),
-    components=_make_page_components(page_id="p1"),
-    controls=_make_all_selectors(prefix_id="p1_static"),
+    ],
 )
 
-
-dashboard = vm.Dashboard(pages=[page_1, page_2, page_3])
+dashboard = vm.Dashboard(pages=[page_1, page_2])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
