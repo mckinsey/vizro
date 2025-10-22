aabc607256b896cc43f3feeb0b0bf6f8c286d73c
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2025-10-22 17:08:52 +0200

    [Docs] Adding yaml example for CapturedCallables to `custom-actions.md` (#1456)

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,69 +1,32 @@
-from vizro import Vizro
+from datetime import datetime
+
 import vizro.models as vm
-import vizro.plotly.express as px
-from vizro.managers import data_manager
-from vizro.tables import dash_ag_grid
-
-gapminder = px.data.gapminder()
-data_manager["df_dynamic"] = lambda: gapminder
+from vizro import Vizro
+from vizro.models.types import capture
 
 
-page_1 = vm.Page(
-    title="[BUG] #2271",
+@capture("action")
+def current_time_text():
+    """Return the current time as a formatted string."""
+    time = datetime.now()
+    return f"The time is {time}"
+
+
+page = vm.Page(
+    title="Action triggered by button",
     layout=vm.Flex(),
     components=[
-        vm.Text(text="Reset Dropdown(multi=True) control does not work when defined with non-list value"),
-        vm.Text(text="https://github.com/McK-Internal/vizro-internal/issues/2271"),
-        vm.Tabs(
-            tabs=[
-                vm.Container(
-                    title="Dropdown [bug]",
-                    controls=[
-                        vm.Filter(column="year", selector=vm.Dropdown(value=2007)),
-                        vm.Filter(column="continent", selector=vm.Dropdown(value="Europe")),
-                    ],
-                    components=[
-                        vm.Graph(figure=px.scatter(gapminder, x="gdpPercap", y="lifeExp", color="continent")),
-                    ],
-                ),
-                vm.Container(
-                    title="Checklist [works]",
-                    controls=[
-                        vm.Filter(column="year", selector=vm.Checklist(value=[2007])),
-                    ],
-                    components=[
-                        vm.Graph(figure=px.scatter(gapminder, x="gdpPercap", y="lifeExp", color="continent")),
-                    ],
-                ),
-            ]
+        vm.Button(
+            actions=vm.Action(
+                function=current_time_text(),
+                outputs="time_text",
+            )
         ),
+        vm.Text(id="time_text", text="Click the button"),
     ],
 )
 
-
-page_2 = vm.Page(
-    title="[BUG] #1936",
-    layout=vm.Flex(),
-    components=[
-        vm.Text(
-            text="""
-            Dynamic filters don't work with numeric columns which values can be translated to date objects by ISO 8601.
-            For example, the 'year' column in the Gapminder dataset. It contains years as integers (1952, 1957...)
-        """
-        ),
-        vm.Text(text="https://github.com/McK-Internal/vizro-internal/issues/1936"),
-        vm.Container(
-            controls=[
-                vm.Filter(column="year", selector=vm.Slider()),
-            ],
-            components=[
-                vm.Graph(figure=px.scatter("df_dynamic", x="gdpPercap", y="lifeExp", color="continent")),
-            ],
-        ),
-    ],
-)
-
-dashboard = vm.Dashboard(pages=[page_1, page_2])
+dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
--- vizro-core/examples/scratch_dev/yaml_version/app.py
+++ vizro-core/examples/scratch_dev/yaml_version/app.py
@@ -1,19 +1,27 @@
 """Example to show dashboard configuration specified as a YAML file."""
 
+from datetime import datetime
 from pathlib import Path
 
-import vizro.plotly.express as px
+import vizro.models as vm
 import yaml
 from vizro import Vizro
-from vizro.managers import data_manager
-from vizro.models import Dashboard
+from vizro.models.types import capture
 
-data_manager["iris"] = px.data.iris()
-data_manager["gapminder"] = px.data.gapminder()
-data_manager["tips"] = px.data.tips()
+
+@capture("action")
+def current_time_text(use_24_hour_clock):
+    """Return the current time as a formatted string."""
+    time_format = "%H:%M:%S" if use_24_hour_clock else "%I:%M:%S %p"
+    time = datetime.now().strftime(time_format)
+    return f"The time is {time}"
+
+
+# This line doesn't affect the example, so it raises the ValidationError exception.
+vm.Page.add_type("components", vm.Switch)
 
 dashboard = yaml.safe_load(Path("dashboard.yaml").read_text(encoding="utf-8"))
-dashboard = Dashboard(**dashboard)
+dashboard = vm.Dashboard(**dashboard)
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run(debug=True)
