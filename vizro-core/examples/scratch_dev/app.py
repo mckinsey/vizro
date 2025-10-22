5a4eecc5fc9173406a89b1fedb4050f2913395fa
Maximilian Schulz <83698606+maxschulz-COL@users.noreply.github.com>
2025-10-22 18:25:59 +0200

    [Docs] Adding yaml example for CapturedCallables (Max's Batch) (#1454)
    Co-authored-by: Jo Stichbury <jo_stichbury@mckinsey.com>
    Co-authored-by: Petar Pejovic <108530920+petar-qb@users.noreply.github.com>

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,32 +1,55 @@
-from datetime import datetime
-
 import vizro.models as vm
+import vizro.plotly.express as px
+from dash_ag_grid import AgGrid
 from vizro import Vizro
 from vizro.models.types import capture
 
+df = px.data.gapminder().query("year == 2007")
 
-@capture("action")
-def current_time_text():
-    """Return the current time as a formatted string."""
-    time = datetime.now()
-    return f"The time is {time}"
+
+@capture("ag_grid")
+def my_custom_aggrid(chosen_columns: list[str], data_frame=None):
+    """Custom Dash AgGrid."""
+    defaults = {
+        "className": "ag-theme-quartz-dark ag-theme-vizro",
+        "defaultColDef": {
+            "resizable": True,
+            "sortable": True,
+            "filter": True,
+            "filterParams": {
+                "buttons": ["apply", "reset"],
+                "closeOnApply": True,
+            },
+            "flex": 1,
+            "minWidth": 70,
+        },
+        # "style": {"height": "100%"},
+    }
+    return AgGrid(
+        columnDefs=[{"field": col} for col in chosen_columns], rowData=data_frame.to_dict("records"), **defaults
+    )
 
 
 page = vm.Page(
-    title="Action triggered by button",
-    layout=vm.Flex(),
+    title="Example of a custom Dash AgGrid",
     components=[
-        vm.Button(
-            actions=vm.Action(
-                function=current_time_text(),
-                outputs="time_text",
-            )
+        vm.AgGrid(
+            id="custom_ag_grid",
+            title="Custom Dash AgGrid",
+            figure=my_custom_aggrid(
+                data_frame=df, chosen_columns=["country", "continent", "lifeExp", "pop", "gdpPercap"]
+            ),
         ),
         vm.Text(id="time_text", text="Click the button"),
+    ],
+    controls=[
+        vm.Parameter(
+            targets=["custom_ag_grid.chosen_columns"],
+            selector=vm.Dropdown(title="Choose columns", options=df.columns.to_list()),
+        )
     ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
-
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
--- vizro-core/examples/scratch_dev/yaml_version/app.py
+++ vizro-core/examples/scratch_dev/yaml_version/app.py
@@ -1,27 +1,61 @@
 """Example to show dashboard configuration specified as a YAML file."""
 
-from datetime import datetime
 from pathlib import Path
 
 import vizro.models as vm
+import vizro.plotly.express as px
 import yaml
+from dash import dash_table
+from dash_ag_grid import AgGrid
 from vizro import Vizro
+from vizro.managers import data_manager
 from vizro.models.types import capture
 
-
-@capture("action")
-def current_time_text(use_24_hour_clock):
-    """Return the current time as a formatted string."""
-    time_format = "%H:%M:%S" if use_24_hour_clock else "%I:%M:%S %p"
-    time = datetime.now().strftime(time_format)
-    return f"The time is {time}"
+df = px.data.gapminder().query("year == 2007")
+data_manager["gapminder_2007"] = df
 
 
-# This line doesn't affect the example, so it raises the ValidationError exception.
-vm.Page.add_type("components", vm.Switch)
+@capture("table")
+def my_custom_table(chosen_columns: list[str], data_frame=None):
+    """Custom Dash DataTable."""
+    columns = [{"name": i, "id": i} for i in chosen_columns]
+    defaults = {
+        "style_as_list_view": True,
+        "style_data": {"border_bottom": "1px solid var(--bs-border-color)", "height": "40px"},
+        "style_header": {
+            "border_bottom": "1px solid var(--bs-border-color-translucent)",
+            "border_top": "None",
+            "height": "32px",
+        },
+    }
+    return dash_table.DataTable(data=data_frame.to_dict("records"), columns=columns, **defaults)
+
+
+@capture("ag_grid")
+def my_custom_aggrid(chosen_columns: list[str], data_frame=None):
+    """Custom Dash AgGrid."""
+    defaults = {
+        "className": "ag-theme-quartz-dark ag-theme-vizro",
+        "defaultColDef": {
+            "resizable": True,
+            "sortable": True,
+            "filter": True,
+            "filterParams": {
+                "buttons": ["apply", "reset"],
+                "closeOnApply": True,
+            },
+            "flex": 1,
+            "minWidth": 70,
+        },
+    }
+    return AgGrid(
+        columnDefs=[{"field": col} for col in chosen_columns], rowData=data_frame.to_dict("records"), **defaults
+    )
+
 
 dashboard = yaml.safe_load(Path("dashboard.yaml").read_text(encoding="utf-8"))
 dashboard = vm.Dashboard(**dashboard)
+
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run(debug=True)
