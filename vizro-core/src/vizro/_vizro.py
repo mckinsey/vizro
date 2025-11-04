cace5cb31a60ec763b449347981316df775666c6
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2025-11-04 09:08:33 +0100

    Add fix to remove console error

--- vizro-core/src/vizro/_vizro.py
+++ vizro-core/src/vizro/_vizro.py
@@ -70,7 +70,10 @@ class Vizro:
         for path in sorted(
             VIZRO_ASSETS_PATH.rglob("*.*"), key=lambda file: (file.name != "vizro-bootstrap.min.css", file)
         ):
-            if path.suffix == ".css" and (path.name != "vizro-bootstrap.min.css" or use_vizro_bootstrap):
+            if path.suffix == ".css":
+                if path.name == "vizro-bootstrap.min.css" and not use_vizro_bootstrap:
+                    # Skip vizro-bootstrap.min.css if external Bootstrap CSS is provided
+                    continue
                 self.dash.css.append_css(_make_resource_spec(path))
             elif path.suffix == ".js":
                 self.dash.scripts.append_script(_make_resource_spec(path))
