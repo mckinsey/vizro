7e58b9902f1c8b0deda5fb947a5d7ed30526dbfc
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-11-18 14:38:42 +0100

    fix flex bug

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,20 +1,27 @@
 import vizro.plotly.express as px
-import vizro.models as vm
 from vizro import Vizro
+import vizro.models as vm
 
 df = px.data.iris()
 
-page_1 = vm.Page(
-    title="BUG theme switch doesn't work with Flex layout",
-    layout=vm.Flex(),
+page = vm.Page(
+    title="Vizro on PyCafe",
+    layout=vm.Flex(direction="row"),
     components=[
-        vm.Graph(figure=px.scatter(data_frame=df, x="sepal_width", y="sepal_length")),
-        vm.Graph(figure=px.scatter(data_frame=df.head(10), x="sepal_width", y="sepal_length")),
-        vm.Card(text="test"),
+        vm.Graph(
+            id="scatter_chart",
+            title="title one",
+            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species"),
+        ),
+        vm.Graph(id="hist_chart", title="title onetwothree", figure=px.histogram(df, x="sepal_width", color="species")),
+        vm.Graph(id="scatter_chart2", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species")),
+        vm.Graph(id="hist_chart2", figure=px.histogram(df, x="sepal_width", color="species")),
+        vm.Card(text="Placeholder text"),
     ],
+    controls=[vm.Filter(column="species"), vm.Filter(column="petal_length"), vm.Filter(column="sepal_width")],
 )
 
-dashboard = vm.Dashboard(pages=[page_1])
+dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
