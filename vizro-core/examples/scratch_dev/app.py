101dc2baad2fff8f24998fa637b9cb9bf9071387
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2025-12-02 16:43:54 +0100

    change scratch app

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -7,17 +7,17 @@ df = px.data.iris()
 
 page = vm.Page(
     title="Vizro on PyCafe",
-    layout=vm.Flex(direction="row", wrap=True),
+    layout=vm.Flex(direction="row", wrap=False),
     components=[
         vm.Graph(
             id="scatter_chart",
             title="title one",
-            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300),
+            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300, height=300),
         ),
         vm.Graph(
             id="hist_chart",
             title="title onetwothree",
-            figure=px.histogram(df, x="sepal_width", color="species", width=300),
+            figure=px.histogram(df, x="sepal_width", color="species"),
         ),
         vm.Graph(
             id="scatter_chart2", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300)
