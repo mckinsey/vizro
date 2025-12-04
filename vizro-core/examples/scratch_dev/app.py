bb4133eb4f65cb3c02dacdf4f26f2378efa644ca
petar-qb <petar_pejovic@external.mckinsey.com>
2025-12-04 11:06:52 +0100

    Change scratch app and comment out the custom.css used for flex

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -10,9 +10,12 @@ tips = px.data.tips()
 
 filtered_gapminder = gapminder[(gapminder["continent"] == "Europe") & (gapminder["year"] == 2007)]
 
-# WORKS FINE - CAN BE ADDED TO STATIC CSS
-page0 = vm.Page(
-    title="Sticky headers - grid ",
+page_grid_0 = vm.Page(
+    title="Grid",
+    layout=vm.Grid(
+        grid=[[0, 1], [2, 2]],
+        row_min_height="1000px",
+    ),
     components=[
         vm.Container(
             title="",
@@ -23,51 +26,89 @@ page0 = vm.Page(
         vm.Graph(figure=px.scatter(iris, x="sepal_width", y="petal_length"), title="Title"),
         vm.AgGrid(figure=dash_ag_grid(data_frame=iris)),
     ],
-    layout=vm.Grid(grid=[[0, 1], [2, 2]]),
 )
 
-# WORKS FINE WITH CUSTOM CSS - CAN BE ADDED AS DOCS EXAMPLES WITH CAVEAT
-page01 = vm.Page(
-    title="Sticky headers - flex",
-    layout=vm.Flex(),
+
+page_grid_1 = vm.Page(
+    title="Grid - in column",
+    layout=vm.Grid(
+        grid=[[0], [1], [2], [3], [4]],
+        row_min_height="1000px",
+    ),
     components=[
-        vm.AgGrid(figure=dash_ag_grid(data_frame=gapminder), id="my-grid"),
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
+        vm.AgGrid(figure=dash_ag_grid(tips)),
+        vm.Button(text="Button"),
         vm.Button(text="Button"),
     ],
 )
 
 
-# DOES NOT WORK AS TOO MANY AGGRIDS ARE ON ONE SCREEN WITH HEADERS THAT NEED TO BE
-# POSITIONED DIFFERENTLY, SO MENTION THIS IN THE DOCS AS CAVEAT
-page11 = vm.Page(
-    title="Flex - default - aggrid",
+page_grid_2 = vm.Page(
+    title="Grid - in row",
+    layout=vm.Grid(
+        grid=[[0, 1, 2, 3, 4]],
+        col_min_width="1000px",
+    ),
+    components=[
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
+        vm.AgGrid(figure=dash_ag_grid(tips)),
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
+    ],
+)
+
+page_flex_0 = vm.Page(
+    title="Flex",
     layout=vm.Flex(),
     components=[
-        vm.AgGrid(figure=dash_ag_grid(tips)),
-        vm.AgGrid(figure=dash_ag_grid(iris)),
-        vm.AgGrid(figure=dash_ag_grid(gapminder)),
+        vm.Container(
+            title="",
+            components=[
+                vm.AgGrid(figure=dash_ag_grid(data_frame=filtered_gapminder)),
+            ],
+        ),
+        vm.Graph(figure=px.scatter(iris, x="sepal_width", y="petal_length"), title="Title"),
+        vm.AgGrid(figure=dash_ag_grid(data_frame=iris)),
     ],
 )
 
 
-# THIS WORKS AGAIN, AS ALL HEADERS ARE ON THE SAME LEVEL - ADD TO DOCS
-page13 = vm.Page(
-    title="Flex - row - aggrid",
+page_flex_1 = vm.Page(
+    title="Flex - in column",
+    layout=vm.Flex(direction="column"),
+    components=[
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
+        vm.AgGrid(figure=dash_ag_grid(tips)),
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
+    ],
+)
+
+page_flex_2 = vm.Page(
+    title="Flex - in row",
     layout=vm.Flex(direction="row"),
     components=[
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
         vm.AgGrid(figure=dash_ag_grid(tips)),
-        vm.AgGrid(figure=dash_ag_grid(iris)),
-        vm.AgGrid(figure=dash_ag_grid(gapminder)),
+        vm.Button(text="Button"),
+        vm.Button(text="Button"),
     ],
 )
 
 
 dashboard = vm.Dashboard(
     pages=[
-        page0,
-        page01,
-        page11,
-        page13,
+        page_grid_0,
+        page_grid_1,
+        page_grid_2,
+        page_flex_0,
+        page_flex_1,
+        page_flex_2,
     ],
     title="Test out Flex/Grid",
 )
