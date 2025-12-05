fe15e0265d8f7b699848740bffa44fa1362a8047
Antony Milne <49395058+antonymilne@users.noreply.github.com>
2025-12-05 14:44:03 +0000

    Refactor app.py to use ControlGroup and update layout

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,55 +1,97 @@
+from typing import Literal
+
+from dash import html
+
+import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-import vizro.models as vm
-from vizro.tables import dash_ag_grid
+from vizro.models.types import ControlType
+
+df_gapminder = px.data.gapminder()
+
+
+class ControlGroup(vm.VizroBaseModel):
+    """Container to group controls."""
 
     type: Literal["control_group"] = "control_group"
     title: str
     controls: list[ControlType] = []
 
+    def build(self):
+        return html.Div(
+            [html.H4(self.title), html.Hr()] + [control.build() for control in self.controls],
+            className="control_group_container",
+        )
+
+
+vm.Page.add_type("controls", ControlGroup)
+vm.Page.add_type("components", ControlGroup)
+vm.Page.add_type("components", vm.Filter)
+
+vm.Container.add_type("controls", ControlGroup)
+vm.Container.add_type("components", ControlGroup)
+vm.Container.add_type("components", vm.Filter)
+
 page = vm.Page(
-    title="Vizro on PyCafe",
-    layout=vm.Flex(direction="row", wrap=False),
+    layout=vm.Flex(),
+    title="Page:",
     components=[
-        vm.Graph(
-            id="scatter_chart",
-            title="title one",
-            figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300, height=300),
+        # vm.Filter(id="page_components", column="country"),  # -> EXCEPTION
+        # ControlGroup(  # -> EXCEPTION
+        #     title="page_components_group",
+        #     controls=[vm.Filter(id="page_components_group", column="continent")],
+        # ),
+        vm.Container(
+            title="Container:",
+            components=[
+                # vm.Filter(id="container_components", column="country"),  # -> EXCEPTION
+                # ControlGroup(  # -> EXCEPTION
+                #     title="container_components_group",
+                #     controls=[vm.Filter(id="container_components_group", column="continent")],
+                # ),
+                vm.Container(
+                    components=[
+                        vm.Container(
+                            title="Nested Container:",
+                            components=[
+                                # vm.Filter(id="nested_container_components", column="country"),  # -> EXCEPTION
+                                # ControlGroup(  # -> EXCEPTION
+                                #     title="nested_container_components_group",
+                                #     controls=[vm.Filter(id="nested_container_components_group", column="continent")],
+                                # ),
+                                vm.Graph(figure=px.scatter(df_gapminder, x="gdpPercap", y="lifeExp", size="pop")),
+                            ],
+                            controls=[
+                                vm.Filter(id="nested_container_controls", column="country"),
+                                ControlGroup(
+                                    title="nested_container_controls_group",
+                                    controls=[vm.Filter(id="nested_container_controls_group", column="continent")],
+                                ),
+                            ],
+                        )
+                    ]
+                )
+            ],
+            controls=[
+                vm.Filter(id="container_controls", column="country"),
+                ControlGroup(
+                    title="container_controls_group",
+                    controls=[vm.Filter(id="container_controls_group", column="continent")],
+                ),
+            ],
         ),
-        vm.Graph(
-            id="hist_chart",
-            title="title onetwothree",
-            figure=px.histogram(df, x="sepal_width", color="species"),
-        ),
-        vm.Graph(
-            id="scatter_chart2", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300)
-        ),
-        vm.Graph(id="hist_chart2", figure=px.histogram(df, x="sepal_width", color="species", width=300)),
-        vm.Graph(
-            id="hist_chart1",
-            title="title onetwothree",
-            figure=px.histogram(df, x="sepal_width", color="species", width=300),
-        ),
-        vm.Graph(
-            id="scatter_chart12", figure=px.scatter(df, x="sepal_length", y="petal_width", color="species", width=300)
-        ),
-        vm.Graph(id="hist_chart12", figure=px.histogram(df, x="sepal_width", color="species", width=300)),
     ],
-    controls=[vm.Filter(column="species"), vm.Filter(column="petal_length"), vm.Filter(column="sepal_width")],
+    controls=[
+        vm.Filter(id="page_controls", column="continent"),
+        ControlGroup(
+            title="page_controls_group",
+            controls=[
+                vm.Filter(id="page_controls_group", column="country"),
+            ],
+        ),
+    ],
 )
 
-page_two = vm.Page(
-    title="Tables",
-    # layout=vm.Flex(direction="row", wrap=True),
-    components=[
-        vm.AgGrid(figure=dash_ag_grid(df), title="Table 1"),
-        vm.AgGrid(figure=dash_ag_grid(df), title="Table 2 fd"),
-        vm.AgGrid(figure=dash_ag_grid(df), title="Table 3 fdffdd"),
-    ],
-    controls=[vm.Filter(column="species"), vm.Filter(column="petal_length"), vm.Filter(column="sepal_width")],
-)
-
-dashboard = vm.Dashboard(pages=[page, page_two])
-
+dashboard = vm.Dashboard(pages=[page])
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
