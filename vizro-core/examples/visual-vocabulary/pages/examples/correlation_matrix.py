604920aa4b3bde8692792f72064f0648a100bf54
datascienceio <prachee.choudhury.07@gmail.com>
2025-12-09 18:17:33 +0530

    Add files via upload

---
+++ vizro-core/examples/visual-vocabulary/pages/examples/correlation_matrix.py
@@ -0,0 +1,19 @@
+import vizro.plotly.express as px
+from vizro.models.types import capture
+
+# Note iris data
+iris = px.data.iris().select_dtypes("number")
+
+
+# Chart definition
+@capture("graph")
+def correlation_heatmap(data_frame):
+    return px.imshow(
+        data_frame.corr(),
+        text_auto=True,
+        aspect="auto",
+    )
+
+
+# definition
+fig = correlation_heatmap(data_frame="iris")
