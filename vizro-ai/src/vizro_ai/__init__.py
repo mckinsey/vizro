93d4e61a12e5caf980bd5fefbd6d5b2747832d66
Maximilian Schulz <83698606+maxschulz-COL@users.noreply.github.com>
2026-01-05 15:29:36 +0100

    [Release] Release of VizroAI 0.3.8 (#1530)

--- vizro-ai/src/vizro_ai/__init__.py
+++ vizro-ai/src/vizro_ai/__init__.py
@@ -9,7 +9,7 @@ from ._vizro_ai import VizroAI
 
 __all__ = ["VizroAI"]
 
-__version__ = "0.3.8.dev0"
+__version__ = "0.3.8"
 
 # TODO: I think this collides with the VIZRO_LOG_LEVEL setting, as basicConfig can only be set once
 logging.basicConfig(level=os.getenv("VIZRO_AI_LOG_LEVEL", "INFO"))
