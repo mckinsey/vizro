ee8d5628d145848084c18b248a0c276a471c6644
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2026-01-22 17:38:52 +0100

    Style active icons

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -7,13 +7,35 @@ import vizro.actions as va
 from vizro import Vizro
 from vizro.models.types import capture
 from vizro.tables import dash_ag_grid
-
+import pandas as pd
 
 df = px.data.iris()
 
+data = {
+    "user_id": range(1, 10),
+    "name": ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Ian"],
+    "age": [10, 20, 30, 15, 25, 35, 25, 35, 45],
+    "signup_date": [
+        "2023-01-01",
+        "2023-01-02",
+        "2023-01-03",
+        "2023-01-04",
+        "2023-01-05",
+        "2023-01-06",
+        "2023-01-07",
+        "2023-01-08",
+        "2023-01-09",
+    ],
+    "active": [True, False, True, False, True, False, True, False, True],
+}
+df = pd.DataFrame(data)
+
 page = vm.Page(
-    title="Iris",
+    title="Data",
     components=[vm.AgGrid(figure=dash_ag_grid(df))],
+    controls=[
+        vm.Filter(column="name", selector=vm.Checklist()),
+    ]
 )
 
 dashboard = vm.Dashboard(pages=[page])
