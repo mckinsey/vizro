bfe3f7412c3b26e65aadb8de42d5adecc682856a
Vizro Team <145135826+vizro-svc@users.noreply.github.com>
2026-01-26 19:29:39 +0000

    [Release] Bump vizro-ai to 0.4.1.dev0 [skip ci]

--- vizro-ai/src/vizro_ai/__init__.py
+++ vizro-ai/src/vizro_ai/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-__version__ = "0.4.0"
+__version__ = "0.4.1.dev0"
 
 # TODO: I think this collides with the VIZRO_LOG_LEVEL setting, as basicConfig can only be set once
 logging.basicConfig(level=os.getenv("VIZRO_AI_LOG_LEVEL", "INFO"))
