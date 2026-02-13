c72f9d8c639ef73021ff28829a5d4614deb9383e
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2026-02-13 15:24:31 +0100

    Remove transparency in charts and bring back export button

--- vizro-core/src/vizro/_themes/_templates.py
+++ vizro-core/src/vizro/_themes/_templates.py
@@ -122,16 +122,9 @@ def create_template_common() -> go.layout.Template:
 
 dashboard_overrides = go.layout.Template(
     layout=go.Layout(
-        geo_bgcolor=colors.transparent,
-        geo_lakecolor=colors.transparent,
-        geo_landcolor=colors.transparent,
         margin_b=16,
         margin_l=24,
         margin_t=24,
-        paper_bgcolor=colors.transparent,
-        plot_bgcolor=colors.transparent,
-        polar_bgcolor=colors.transparent,
-        ternary_bgcolor=colors.transparent,
         title_pad_l=0,
         title_pad_r=0,
         xaxis_title_font_size=14,
