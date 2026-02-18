a048a4f26aa556fda5d485152552dee344b19ba4
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2026-02-18 13:39:11 +0100

    Remove transparent bg colors

--- vizro-core/src/vizro/themes/_templates.py
+++ vizro-core/src/vizro/themes/_templates.py
@@ -4,16 +4,9 @@ from vizro.themes import colors
 
 dashboard_overrides = go.layout.Template(
     layout=go.Layout(
-        geo_bgcolor=colors.transparent,
-        geo_lakecolor=colors.transparent,
-        geo_landcolor=colors.transparent,
         margin_b=16,
         margin_l=24,
         margin_t=24,
-        paper_bgcolor=colors.transparent,
-        plot_bgcolor=colors.transparent,
-        polar_bgcolor=colors.transparent,
-        ternary_bgcolor=colors.transparent,
         title_pad_l=0,
         title_pad_r=0,
     )
