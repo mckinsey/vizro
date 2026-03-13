c61239acd8d62a7be921adb045042087f32e7709
Antony Milne <antony.milne@quantumblack.com>
2026-03-13 00:19:59 +0000

    Update css

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -2,7 +2,6 @@
 
 import vizro.models as vm
 import vizro.plotly.express as px
-
 from vizro import Vizro
 
 df = px.data.iris()
@@ -35,7 +34,33 @@ dashboard = vm.Dashboard(pages=[page])
 Vizro().build(dashboard).run()
 
 ```"""
-        )
+        ),
+        vm.Text(
+            text="""
+
+Block code snippet:
+```python
+
+import vizro.plotly.express as px
+from vizro import Vizro
+import vizro.models as vm
+
+df = px.data.iris()
+
+page = vm.Page(
+    title="My first dashboard",
+    components=[
+        vm.Graph(figure=px.histogram(df, x="sepal_width", color="species"))],
+    controls=[
+        vm.Filter(column="species"),
+    ],
+)
+
+dashboard = vm.Dashboard(pages=[page])
+Vizro().build(dashboard).run()
+
+```"""
+        ),
     ],
 )
 
