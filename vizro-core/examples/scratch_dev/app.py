4529992ecacdded848a807e24b8a34cc785933ba
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2026-03-16 17:41:29 +0100

    initial implementation

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -5,39 +5,89 @@ import vizro.plotly.express as px
 
 from vizro import Vizro
 
-df = px.data.iris()
+iris = px.data.iris()
 
 
 page = vm.Page(
     title="Page Two",
     components=[
-        vm.Graph(figure=px.histogram(df, x="sepal_length")),
+        vm.Graph(
+            id="scatter_chart",
+            figure=px.scatter(iris, title="My scatter chart", x="sepal_length", y="petal_width", color="species"),
+        )
     ],
     controls=[
         vm.ControlGroup(
-            title="Filters",
+            title="Control group title",
             controls=[
                 vm.Filter(column="species", selector=vm.Dropdown(variant="filled")),
-                vm.Filter(column="sepal_length", selector=vm.Slider())
+                vm.Filter(column="sepal_length", selector=vm.Slider()),
             ],
-            variant="outlined",
-            collapsed="True"
         ),
         vm.ControlGroup(
-            title="Filters 2",
+            id="ctl2",
+            title="Parameters",
             controls=[
-                vm.Filter(column="species", selector=vm.Dropdown(variant="filled")),
-                vm.Filter(column="sepal_length", selector=vm.Slider())
+                vm.Parameter(
+                    targets=["scatter_chart.title"],
+                    selector=vm.Dropdown(
+                        title="Choose title",
+                        options=["My scatter chart", "A better title!", "Another title..."],
+                        multi=False,
+                    ),
+                ),
             ],
-            variant="outlined",
-            collapsed="True"
+        ),
+        vm.Filter(column="sepal_length", selector=vm.Slider()),
+    ],
+)
+
+page_2 = vm.Page(
+    title="ControlGroup in Container",
+    components=[
+        vm.Container(
+            title="Bar chart container",
+            components=[
+                vm.Graph(
+                    id="bar_chart",
+                    figure=px.bar(
+                        iris,
+                        x="sepal_width",
+                        y="sepal_length",
+                        color="species",
+                        color_discrete_map={"setosa": "#00b4ff", "versicolor": "#ff9222"},
+                    ),
+                ),
+            ],
+            controls=[
+                vm.ControlGroup(
+                    title="Control group in container",
+                    controls=[
+                        vm.Parameter(
+                            targets=["bar_chart.color_discrete_map.virginica"],
+                            selector=vm.Dropdown(
+                                options=["#ff5267", "#3949ab"],
+                                multi=False,
+                                value="#3949ab",
+                            ),
+                        ),
+                        vm.Filter(column="species", selector=vm.Dropdown(variant="filled")),
+                    ],
+                ),
+                vm.ControlGroup(
+                    title="Control group in container",
+                    controls=[
+                        vm.Filter(column="sepal_length"),
+                    ],
+                ),
+            ],
         )
     ],
 )
 
 
 dashboard = vm.Dashboard(
-    pages=[page],
+    pages=[page, page_2],
     title="QB",
 )
 
