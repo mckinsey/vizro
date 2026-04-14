bb431a22fb6d42b72412efa08bba76b697a5d369
Antony Milne <49395058+antonymilne@users.noreply.github.com>
2026-04-14 07:11:29 +0100

    [Feat] Cascader model and hierarchical filters (#1683)
    Co-authored-by: petar-qb <petar_pejovic@external.mckinsey.com>

--- vizro-core/src/vizro/models/_components/form/__init__.py
+++ vizro-core/src/vizro/models/_components/form/__init__.py
@@ -1,4 +1,5 @@
 from vizro.models._components.button import Button
+from vizro.models._components.form.cascader import Cascader
 from vizro.models._components.form.checklist import Checklist
 from vizro.models._components.form.date_picker import DatePicker
 from vizro.models._components.form.dropdown import Dropdown
@@ -7,4 +8,4 @@ from vizro.models._components.form.range_slider import RangeSlider
 from vizro.models._components.form.slider import Slider
 from vizro.models._components.form.switch import Switch
 
-__all__ = ["Button", "Checklist", "DatePicker", "Dropdown", "RadioItems", "RangeSlider", "Slider", "Switch"]
+__all__ = ["Button", "Cascader", "Checklist", "DatePicker", "Dropdown", "RadioItems", "RangeSlider", "Slider", "Switch"]
