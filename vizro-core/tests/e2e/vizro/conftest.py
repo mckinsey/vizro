6db5a08fb791dd372ddc3cb535c7f8b9c334bb02
Alexey Snigir <alexey_snigir@external.mckinsey.com>
2026-04-30 15:04:33 +0200

    test

--- vizro-core/tests/e2e/vizro/conftest.py
+++ vizro-core/tests/e2e/vizro/conftest.py
@@ -17,6 +17,7 @@ def pytest_setup_options():
         return options
 
 
+# This fixture is used in all tests by default and provides the driver from the dash library.
 @pytest.fixture(autouse=True)
 def dash_br_driver(dash_br, request):
     """Built-in driver from the dash library."""
