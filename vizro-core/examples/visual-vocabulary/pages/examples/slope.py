3ed225f92d9717646b38475f882855e52582628d
Li Nguyen <90609403+huong-li-nguyen@users.noreply.github.com>
2026-05-04 14:20:16 +0200

    Update vizro-core/examples/visual-vocabulary/pages/examples/slope.py

--- vizro-core/examples/visual-vocabulary/pages/examples/slope.py
+++ vizro-core/examples/visual-vocabulary/pages/examples/slope.py
@@ -1,26 +1,11 @@
-import pandas as pd
 import vizro.plotly.express as px
 
-gapminder_2007 = px.data.gapminder().query("year == 2007")
-gapminder_1952 = px.data.gapminder().query("year == 1952")
+# Need to have a string name here, otherwise it displays the other years
+X = {1952: "Y1952", 2007: "Y2007"}
+COUNTRIES = "Japan", "Norway", "Korea, Rep.", "United States", "China", "Brazil", "India"
 
-data = pd.merge(
-    gapminder_1952[["country", "lifeExp"]].rename(columns={"lifeExp": "1952"}),
-    gapminder_2007[["country", "lifeExp"]].rename(columns={"lifeExp": "2007"}),
-    on="country",
-)
-data = data[data["1952"] > 30].sort_values("2007")
-data["change"] = data["2007"] - data["1952"]
-data = data.nlargest(10, "change")
+gm = px.data.gapminder()
+df = gm.loc[gm["year"].isin(X) & gm["country"].isin(COUNTRIES), ["country", "year", "lifeExp"]]
+df["x"] = df["year"].map(X)
 
-data_long = data.melt(id_vars=["country"], var_name="year", value_name="life_exp")
-
-fig = px.line(
-    data_long,
-    x="year",
-    y="life_exp",
-    color="country",
-    markers=True,
-    title="Life expectancy: 1952 vs 2007",
-)
-fig.update_yaxes(title="Life expectancy (years)")
+fig = px.line(df, x="x", y="lifeExp", color="country", markers=True)
