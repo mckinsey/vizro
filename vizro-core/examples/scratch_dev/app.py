cba6a8efb9b13713b29c0c41113b51299cd46882
Petar Pejovic <108530920+petar-qb@users.noreply.github.com>
2026-06-03 18:31:23 +0200

    [Docs] Remove self-highlighting from docs (#1747)
    Co-authored-by: Antony Milne <49395058+antonymilne@users.noreply.github.com>

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,27 +1,52 @@
 """Simple Vizro dashboard with a graph inside a container."""
 
+import vizro.plotly.express as px
 import vizro.models as vm
+import vizro.actions as va
+from vizro.models.types import capture
 from vizro import Vizro
+from vizro.tables import dash_ag_grid
+
+selected_countries = [
+    "Singapore",
+    "Malaysia",
+    "Thailand",
+    "Indonesia",
+    "Philippines",
+    "Vietnam",
+    "Cambodia",
+    "Myanmar",
+]
+
+gapminder = px.data.gapminder().query("country.isin(@selected_countries)")
+
+gapminder_2007 = gapminder[gapminder["year"] == 2007]
 
 page = vm.Page(
-    title="Debug",
+    title="Cross-filter a graph and table",
     components=[
-        vm.Tabs(
-            tabs=[
-                vm.Container(
-                    title="A",
-                    components=[vm.Text(text="AAA")],
-                ),
-                vm.Container(
-                    title="B",
-                    components=[vm.Text(text="BBB")],
-                ),
+        vm.Graph(
+            id="bar_chart",
+            figure=px.bar(
+                gapminder_2007,
+                x="lifeExp",
+                y="country",
+                labels={"lifeExp": "lifeExp in 2007"},
+                category_orders={"country": sorted(gapminder_2007["country"]), "color": [False, True]},
+            ),
+            header="💡 Click on a bar to highlight the selected country and filter the table below",
+            actions=[
+                va.set_control(control="country_filter", value="y"),
             ],
         ),
+        vm.AgGrid(id="gapminder_table", figure=dash_ag_grid(data_frame=gapminder)),
+    ],
+    controls=[
+        vm.Filter(id="country_filter", column="country", targets=["gapminder_table"], visible=False),
     ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run(debug=True)
+    Vizro().build(dashboard).run()
