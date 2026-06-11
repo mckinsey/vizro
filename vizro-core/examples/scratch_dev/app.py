1653e89cf66eaca01ce6ec1c22a0c77a012b6c97
petar-qb <petar_pejovic@external.mckinsey.com>
2026-06-11 10:41:37 +0200

    Fix AgGrid to work without the dashGridOptions specified

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,48 +1,49 @@
-"""Simple Vizro dashboard with a graph inside a container."""
-
-import vizro.plotly.express as px
 import vizro.models as vm
-import vizro.actions as va
-from vizro.models.types import capture
+import vizro.plotly.express as px
+from dash_ag_grid import AgGrid
 from vizro import Vizro
-from vizro.tables import dash_ag_grid
+from vizro.models.types import capture
 
-selected_countries = [
-    "Singapore",
-    "Malaysia",
-    "Thailand",
-    "Indonesia",
-    "Philippines",
-    "Vietnam",
-    "Cambodia",
-    "Myanmar",
-]
+df = px.data.gapminder().query("year == 2007")
 
-gapminder = px.data.gapminder().query("country.isin(@selected_countries)")
 
-gapminder_2007 = gapminder[gapminder["year"] == 2007]
+@capture("ag_grid")
+def my_custom_aggrid(chosen_columns: list[str], data_frame=None):
+    defaults = {
+        "className": "ag-theme-vizro",
+        "defaultColDef": {
+            "resizable": True,
+            "sortable": True,
+            "filter": True,
+            "filterParams": {
+                "buttons": ["apply", "reset"],
+                "closeOnApply": True,
+            },
+            "flex": 1,
+            "minWidth": 70,
+        },
+    }
+    return AgGrid(
+        columnDefs=[{"field": col} for col in chosen_columns], rowData=data_frame.to_dict("records"), **defaults
+    )
+
 
 page = vm.Page(
-    title="Cross-filter a graph and table",
+    title="Example of a custom Dash AgGrid",
     components=[
-        vm.Graph(
-            id="bar_chart",
-            figure=px.bar(
-                gapminder_2007,
-                x="lifeExp",
-                y="country",
-                labels={"lifeExp": "lifeExp in 2007"},
-                category_orders={"country": sorted(gapminder_2007["country"]), "color": [False, True]},
+        vm.AgGrid(
+            id="custom_ag_grid",
+            title="Custom Dash AgGrid",
+            figure=my_custom_aggrid(
+                data_frame=df, chosen_columns=["country", "continent", "lifeExp", "pop", "gdpPercap"]
             ),
-            header="💡 Click on a bar to highlight the selected country and filter the table below",
-            actions=[
-                va.set_control(control="country_filter", value="y"),
-            ],
         ),
-        vm.AgGrid(id="gapminder_table", figure=dash_ag_grid(data_frame=gapminder)),
     ],
     controls=[
-        vm.Filter(id="country_filter", column="country", targets=["gapminder_table"], visible=False),
+        vm.Parameter(
+            targets=["custom_ag_grid.chosen_columns"],
+            selector=vm.Dropdown(title="Choose columns", options=df.columns.to_list()),
+        )
     ],
 )
 
