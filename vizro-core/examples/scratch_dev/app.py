bfb5bc6896837cf849eb1dff725f14864f0b404f
petar-qb <petar_pejovic@external.mckinsey.com>
2026-06-17 10:40:58 +0200

    Explore CI bug - 7

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -254,12 +254,12 @@ page_5 = vm.Page(
 
 dashboard = vm.Dashboard(
     pages=[
-        page_0,
-        # page_1,
-        # page_2,
-        # page_3,
-        # page_4,
-        # page_5,
+        # page_0,
+        page_1,
+        page_2,
+        page_3,
+        page_4,
+        page_5,
     ],
 )
 
