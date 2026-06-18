80149e576579066e50b8ff75e4dc2dcfe43962b2
nadijagraca <nadija_ratkusic_graca@external.mckinsey.com>
2026-06-18 18:23:49 +0200

    update app example

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,6 +1,7 @@
 """Scratch demo app."""
 
 import vizro.models as vm
+import vizro.actions as va
 import vizro.plotly.express as px
 
 from vizro import Vizro
@@ -11,7 +12,11 @@ iris = px.data.iris()
 page_1 = vm.Page(
     title="Action Logs 1",
     components=[
-        vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+        vm.Graph(id="graph_1", figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
+        vm.Button(
+            text="Download data",
+            actions=[va.export_data(targets=["graph_1"])]
+        )
     ],
     controls=[
         vm.Filter(column="species"),
