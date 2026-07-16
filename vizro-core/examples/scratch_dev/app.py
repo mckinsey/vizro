30fa985b5b5b57145e47b7acfadf3507cd033448
petar-qb <petar_pejovic@external.mckinsey.com>
2026-07-16 20:06:35 +0200

    Support the latest dash-ag-grid==34.3.0

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,39 +1,13 @@
-"""Scratch demo app."""
-
 import vizro.models as vm
-import vizro.actions as va
 import vizro.plotly.express as px
-
 from vizro import Vizro
+from vizro.tables import dash_ag_grid
 
-iris = px.data.iris()
-
-
-page_1 = vm.Page(
-    title="Action Logs 1",
-    components=[
-        vm.Graph(id="graph_1", figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
-        vm.Button(text="Download data", actions=[va.export_data(targets=["graph_1"])]),
-    ],
-    controls=[
-        vm.Filter(column="species"),
-    ],
+page = vm.Page(
+    title="Default Dash AG Grid", components=[vm.AgGrid(figure=dash_ag_grid(data_frame=px.data.gapminder()))]
 )
 
-page_2 = vm.Page(
-    title="Action Logs 2",
-    components=[
-        vm.Graph(id="graph_2", figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species")),
-    ],
-    controls=[
-        vm.Filter(column="species"),
-    ],
-)
-
-
-dashboard = vm.Dashboard(
-    pages=[page_1, page_2],
-)
+dashboard = vm.Dashboard(pages=[page])
 
 if __name__ == "__main__":
-    Vizro().build(dashboard).run(debug=True)
+    Vizro().build(dashboard).run()
