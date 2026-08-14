e5d96bdd98bae25b2631c5fdab96cd0db0c5ebf4
nadijagraca <108531476+nadijagraca@users.noreply.github.com>
2026-08-14 15:53:39 +0200

    [Tidy] Update background colors (#1830)
    Co-authored-by: Alexey Snigir <alexey_snigir@external.mckinsey.com>

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -265,12 +265,50 @@ page_5 = vm.Page(
     ],
 )
 
+page_6 = vm.Page(
+    title="Simple filled container",
+    components=[
+        vm.Container(
+            title="Filled",
+            components=[
+                vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species"), id="graph_8"),
+            ],
+            variant="filled",
+        )
+    ],
+)
+
+page_7 = vm.Page(
+    title="Simple outlined container",
+    components=[
+        vm.Container(
+            title="Outlined",
+            components=[
+                vm.Graph(figure=px.scatter(iris, x="sepal_length", y="petal_width", color="species"), id="graph_9"),
+            ],
+            variant="outlined",
+        )
+    ],
+)
+
 
 dashboard = vm.Dashboard(
     title="Vizro",
-    pages=[page_1, page_2, page_3, page_4, page_5],
-    # pages=[page_1],
-    # navigation=vm.Navigation(nav_selector=vm.NavBar()),
+    pages=[page_1, page_2, page_6, page_7, page_3, page_4, page_5],
+    navigation=vm.Navigation(
+        nav_selector=vm.NavBar(
+            items=[
+                vm.NavLink(
+                    pages=["Regular page", "Plain containers", "Simple filled container", "Simple outlined container"],
+                    label="First icon",
+                ),
+                vm.NavLink(
+                    pages=["Filled containers", "Containers nested mixed", "Containers nested filled"],
+                    label="Second icon",
+                ),
+            ]
+        )
+    ),
 )
 
 if __name__ == "__main__":
